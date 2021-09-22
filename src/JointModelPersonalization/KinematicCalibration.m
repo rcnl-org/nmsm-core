5fe4df1488338c40e9405ddb08c3908991a96ca6
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-22 15:58:24 -0500

    added skeleton KinematicCalibration fn

---
+++ src/JointModelPersonalization/KinematicCalibration.m
@@ -0,0 +1,10 @@
+% Kinematic Calibration takes a model and applies a calibration.
+
+% Copyright RCNL *change*
+
+% (Model, Array, Array, struct) -> (Model)
+% Creates calibrated model from joint structure and marker plate structure
+function outputModel = KinematicCalibration(inputModel, jointArray, ...
+    markerPlateArray, params)
+
+end
