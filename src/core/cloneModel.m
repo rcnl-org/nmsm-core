140c894316cb3397df4cbd705b0a927bf6a91162
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-23 12:25:04 -0500

    added core folder and cloneModel skeleton

---
+++ src/core/cloneModel.m
@@ -0,0 +1,13 @@
+% Clones a model to allow for modification of the new model without
+% changing any values in the original model. This function is important to
+% ensure functions that return a new model do not modify the original
+% model.
+
+% copyright RCNL *change later*
+
+% (Model) -> (Model)
+% Duplicate model as a new object separate from the original model
+function outputModel = cloneModel(inputModel)
+
+end
+
