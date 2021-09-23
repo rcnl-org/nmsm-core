a1c6fddb0206f04608671f074b7c676da0c8e031
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-23 10:55:24 -0500

    updated JointModelPersonalization with initial implementation

--- src/JointModelPersonalization/JointModelPersonalization.m
+++ src/JointModelPersonalization/JointModelPersonalization.m
@@ -5,6 +5,16 @@
 
 % (struct, struct) -> struct
 % Runs the Joint Model Personalization algorithm
-function outputs = JointModelPersonalization(inputs, params)
-    
+function outputModel = JointModelPersonalization(inputs, params)
+%prepare optimizations
+optimizations = prepareOptimizations(inputs, params);
+
+outputModel = cloneModel(inputs.Model); %copy model
+for i=1:length(optimizations) %iterate optimizations
+    % retreive optimization values for this specific optimization
+    [jointArray, markerPlateArray, optimizerParams] = ...
+        findOptimizationValues(optimizations(i), inputs, params);
+    outputModel = KinematicCalibration(outputModel, jointArray, ...
+        markerPlateArray, optimizerParams);
+end
 end
