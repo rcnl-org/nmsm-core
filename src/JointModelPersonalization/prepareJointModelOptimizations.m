1ce744e35d0080e764a2a91911db5e4e854b1e44
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-23 19:02:28 -0500

    made skeleton for prepareJointModelOptimization

---
+++ src/JointModelPersonalization/prepareJointModelOptimizations.m
@@ -0,0 +1,11 @@
+% This function prepares the input parameters from Joint Model
+% Personalization as an array of optimizations to be passed for kinematic
+% calibration.
+
+% Copyright RCNL *to be changed*
+
+% (struct, struct) -> (Array)
+% make array of optimizations to complete from inputs and params
+function optimizations = prepareJointModelOptimizations(inputs, params)
+
+end
