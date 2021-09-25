b26578726cde0f161eb1be23d8845a50d6abd504
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-24 23:19:56 -0500

    added simple error definition

--- src/JointModelPersonalization/KinematicCalibration/markerError.m
+++ src/JointModelPersonalization/KinematicCalibration/markerError.m
@@ -1,14 +1,12 @@
 % This function compares the error between two sets of markers by comparing
-% them element-wise and using squared error algorithm
+% them element-wise
 
 % Copyright RCNL *change later*
 
-% (
-
-function [outputArg1,outputArg2] = markerError(inputArg1,inputArg2)
-%COMPUTEMARKERERROR Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% (Model, struct) -> (float)
+% calculates the error between experimental marker pos and model marker pos
+function error = markerError(model, params)
+modelMarkerPositions = findModelMarkerPositions(model);
+error = params.experimentalMarkerPositions - modelMarkerPositions;
 end
 
