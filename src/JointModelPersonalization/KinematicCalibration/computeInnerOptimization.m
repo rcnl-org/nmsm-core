a664b9d4fb582a8b3e12d06d004d1ad3a0d4041e
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-24 22:54:09 -0500

    computeInnerOptimization skeleton

---
+++ src/JointModelPersonalization/KinematicCalibration/computeInnerOptimization.m
@@ -0,0 +1,12 @@
+% This function computes the inner optimization as defined in the Joint
+% Model Personalization module by preparing and running the IK algorithm
+% and returning a model with the new values
+
+% Copyright RCNL *change later*
+
+% (Model, struct) -> (Model)
+% Returns new model with inverse kinematic optimized marker positions
+function outputModel = computeInnerOptimization(model,params)
+
+end
+
