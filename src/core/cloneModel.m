b4919aecf397279f6ad76475319251769c043ad0
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-25 11:31:14 -0500

    added cloneModel implementation

--- src/core/cloneModel.m
+++ src/core/cloneModel.m
@@ -1,13 +1,14 @@
 % Clones a model to allow for modification of the new model without
 % changing any values in the original model. This function is important to
 % ensure functions that return a new model do not modify the original
-% model.
+% model. Keywords include: copy, deep copy, clone
 
 % copyright RCNL *change later*
 
 % (Model) -> (Model)
 % Duplicate model as a new object separate from the original model
 function outputModel = cloneModel(inputModel)
-
+    import org.opensim.modeling.Model;
+    outputModel = inputModel.clone();
 end
 
