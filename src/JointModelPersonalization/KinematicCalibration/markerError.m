7dc6c147a5a2f1de37bd7c157ef311abbba4f68a
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-24 22:48:59 -0500

    created markerError file skeleton

---
+++ src/JointModelPersonalization/KinematicCalibration/markerError.m
@@ -0,0 +1,14 @@
+% This function compares the error between two sets of markers by comparing
+% them element-wise and using squared error algorithm
+
+% Copyright RCNL *change later*
+
+% (
+
+function [outputArg1,outputArg2] = markerError(inputArg1,inputArg2)
+%COMPUTEMARKERERROR Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
