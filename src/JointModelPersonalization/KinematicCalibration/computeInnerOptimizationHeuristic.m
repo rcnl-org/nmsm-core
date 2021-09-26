7835cf0d4294bbc6549682015cac7f6fa0f8a85f
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-25 20:40:14 -0500

    changed function names

--- src/JointModelPersonalization/KinematicCalibration/computeInnerOptimizationHeuristic.m
+++ src/JointModelPersonalization/KinematicCalibration/computeInnerOptimizationHeuristic.m
@@ -6,8 +6,8 @@
 % (Model, struct) -> (number)
 % Computes the heuristic value of the output of the inner optimization
 function output = computeInnerOptimizationHeuristic(model, params)
-params = prepareInnerOptimizationParams(model, params);% Prepare IK algo
-model = computeInnerOptimization(model, params); % Run IK algorithm
+params = prepareIKOptionsForKinematicCalibration(model, params);
+model = computeInverseKinematics(model, params); % Run IK algorithm
 output = markerError(model, params);  % Calculate marker error
 end
 
