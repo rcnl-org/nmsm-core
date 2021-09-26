aa07d72ce14793d96a48e26799c943e4e73af454
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-25 19:20:20 -0500

    refactored joints and marker plates to separate fn
    Former-commit-id: 14e6c2ded2edb8c330a840ce32d3f98d5c2d5c8b

--- src/JointModelPersonalization/findJointModelArrays.m
+++
@@ -1,9 +0,0 @@
-% This function processes the optimization plus the inputs and params of
-% Joint Model Personalization to produce the joint and marker plate arrays.
-
-% (struct, struct, struct) -> (Array, Array, struct)
-function [jointArray, markerPlateArray] = ...
-    findJointModelArrays(optimization,inputs, params)
-
-end
-
