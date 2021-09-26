45f0674ff5af3621e5d69c0075e8fccaef6ebf74
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-25 20:40:14 -0500

    changed function names
    Former-commit-id: 7835cf0d4294bbc6549682015cac7f6fa0f8a85f

---
+++ src/JointModelPersonalization/KinematicCalibration/prepareOptimizerOptions.m
@@ -0,0 +1,11 @@
+% This function takes the input values and returns the parameters for the
+% outer optimization to be used in the kinematic calibration.
+
+% Copyright RCNL *change later*
+
+% (struct) -> (struct)
+% Prepare params for outer optimizer for Kinematic Calibration
+function output = prepareOptimizerOptions(params)
+
+end
+
