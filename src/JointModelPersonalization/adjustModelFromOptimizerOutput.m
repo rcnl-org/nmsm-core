328a51e20d2985bd0f7dce4fd8f36fbc1c58bcd2
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-25 19:47:44 -0500

    added skeleton adjustModelFromOptimizerOutput

---
+++ src/JointModelPersonalization/adjustModelFromOptimizerOutput.m
@@ -0,0 +1,12 @@
+% The optimizer returns the optimized values after completion, these values
+% should be used to change the input model to reflect the optimized state.
+
+% Copyright RNCL *change later*
+
+% (Model, struct, struct) -> (Model)
+% Adjust the model based on the output of the optimizer
+function [outputArg1,outputArg2] = adjustModelFromOptimizerOutput( ...
+    inputModel, optimizerOutput, params)
+
+end
+
