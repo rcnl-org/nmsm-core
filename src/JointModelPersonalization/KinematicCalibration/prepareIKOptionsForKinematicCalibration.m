cabfe290f2cc0b2243e76edc9e0845d463c24dfc
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-25 20:43:13 -0500

    built skeleton prepareIKOptions

---
+++ src/JointModelPersonalization/KinematicCalibration/prepareIKOptionsForKinematicCalibration.m
@@ -0,0 +1,11 @@
+% This function takes the model and params and returns a struct with the
+% params needed by computeInverseKinematics
+
+% Copyright RCNL *change later*
+
+% (Model, struct) -> (struct)
+% Return params for computeInverseKinematics given params from KinCal
+function output = prepareIKOptionsForKinematicCalibration(model, params)
+
+end
+
