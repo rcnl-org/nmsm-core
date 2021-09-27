efa2aba7a5eedac625b9b5a4ea9a120cd2e65fd1
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-27 00:09:06 -0500

    implemented adjustMarkerWeights
    Former-commit-id: 9b0bb70e4db7fc002dd13257c98adf33d9e3b9ad

---
+++ src/core/InverseKinematicsSolver/adjustMarkerWeights.m
@@ -0,0 +1,27 @@
+% This function changes the marker weights of the SetMarkerWeights object
+% based on the struct given as input. The struct format is:
+% field name=name of marker, field value = new weight (number)
+% No change is applied if the weight is not a number
+
+% Copyright RCNL *change later*
+
+% (SetMarkerWeights, struct) -> (SetMarkerWeights)
+% Changes the weight of specified markers in the SetMarkerWeights
+function newMarkerWeightSet = adjustMarkerWeights(markerWeightSet, ...
+    markersToAdjust)
+import org.opensim.modeling.*
+newMarkerWeightSet = markerWeightSet.clone();
+namesMarkersToAdjust = string(fieldnames(markersToAdjust));
+for i=1:length(namesMarkersToAdjust)
+    for j=0:newMarkerWeightSet.getSize()-1
+        if(namesMarkersToAdjust(i) == newMarkerWeightSet.get(j).getName())
+            newWeight = markersToAdjust.(namesMarkersToAdjust(i));
+            if isnumeric(newWeight)
+                newMarkerWeightSet.get(j).setWeight(newWeight);
+                break
+            end
+        end
+    end
+end
+end
+
