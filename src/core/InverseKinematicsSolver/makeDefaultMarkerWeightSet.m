4b940b82d376375756d0167832bb5a953928318d
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-26 23:24:11 -0500

    added makeDefaultMarkerWeightSet

---
+++ src/core/InverseKinematicsSolver/makeDefaultMarkerWeightSet.m
@@ -0,0 +1,16 @@
+% The default marker weight set includes all markers from the model and
+% all markers have a weight of 1. From there, the markers can be modified
+% for use.
+
+% Copyright RCNL *change later*
+
+% (Model) -> (SetMarkerWeight)
+% Makes a default SetMarkerWeight with all markers in model with weight 1.
+function markerWeightSet = makeDefaultMarkerWeightSet(model)
+import org.opensim.modeling.*
+markerWeightSet = SetMarkerWeight();
+for i=0:model.getMarkerSet().getSize()-1
+    markerWeightSet.set(i, MarkerWeight(model.getMarkerSet.get(i), 1.0));
+end
+end
+
