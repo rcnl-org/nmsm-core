9a27c62b4c30efb553a2f4c0bbfccf7f2264364e
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-09-29 16:22:33 -0500

    Update makeInverseKinematicsSolver.m

--- src/core/InverseKinematicsSolver/makeInverseKinematicsSolver.m
+++ src/core/InverseKinematicsSolver/makeInverseKinematicsSolver.m
@@ -1,7 +1,16 @@
+% This function makes an InverseKinematicsSolver object from input Model,
+% markerfilename and parameters. The params are a struct and have the
+% following format:
+% params:
+%     excludedMarkers - (1D array strings of marker names)
+%     markerWeights - struct(marker name, number weight)
+%     accuracy - numeric
+%     startTime - numeric (default: 0)
 
+% Copyright RCNL *change later*
 
-% (Model, struct) -> (InverseKinematicsSolver)
-
+% (Model, string, struct) -> (InverseKinematicsSolver)
+% makes an InverseKinematicsSolver from input values
 function ikSolver = makeInverseKinematicsSolver(model, markerFileName, ...
     params)
 import org.opensim.modeling.*
@@ -9,8 +18,8 @@ markerReferences = makeMarkersReference(model, markerFileName, params);
 ikSolver = InverseKinematicsSolver(model, markerReferences, ...
     SimTKArrayCoordinateReference());
 ikSolver = applyParametersToIKSolver(ikSolver, params);
-state=model.initSystem();%initialize system
-state.setTime(startTime);%set simulation start time
-ikSolver.assemble(state);%assemble
+state = model.initSystem();
+state.setTime(valueOrZero(params, 'startTime'));
+ikSolver.assemble(state);
 end
 
