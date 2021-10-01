bbf90b2f6728a9242d209278c13a9726d785e06f
MSShourijeh <33348612+MSShourijeh@users.noreply.github.com>
2021-10-01 02:44:33 -0500

    Update JointModelPersonalization.m
    minor changes made.

--- src/JointModelPersonalization/JointModelPersonalization.m
+++ src/JointModelPersonalization/JointModelPersonalization.m
@@ -1,5 +1,5 @@
 % Joint Model Personalization uses motion tracking data to personalize the
-% joint centers of the model.
+% joint locations and orientations of the model.
 
 % Copyright RCNL *change to actual license in future*
 
@@ -7,7 +7,7 @@
 % Runs the Joint Model Personalization algorithm
 function outputModel = JointModelPersonalization(inputs, params)
 optimizations = prepareJointModelOptimizations(inputs, params);
-outputModel = cloneModel(inputs.Model); %copy model
+outputModel = cloneModel(inputs.model); %copy model
 for i=1:length(optimizations) %iterate optimizations
     % retrieve optimization values for this specific optimization
     joints = findJointsForJointModel(optimizations(i), inputs, params);
