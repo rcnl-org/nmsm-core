d3a37f13496717ba8860267278d22c7bf9e255ce
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-10-03 20:50:27 -0500

    fixed argument inputs

--- src/JointModelPersonalization/KinematicCalibration/computeInverseKinematicsSquaredError.m
+++ src/JointModelPersonalization/KinematicCalibration/computeInverseKinematicsSquaredError.m
@@ -7,18 +7,19 @@
 % Computes the sum of the squared error of the markers through all frames
 function error = computeInverseKinematicsSquaredError(model, ikSolver, ...
     markersReference, params)
-[state, numFrames, frequency] = prepareFrameIterations(model, ...
-    markersReference);
+import org.opensim.modeling.*
+[state, numFrames, frequency] = prepareFrameIterations(model, ikSolver, ...
+    markersReference, params);
 error = 0;
-for i=1:numFrames-1
-    state.setTime(state.getTime() + frequency)
+for i=1:numFrames
     ikSolver.track(state);
     error = error + calculateFrameSquaredError(ikSolver);
+    state.setTime(state.getTime() + 1/frequency)
 end
 end
 
 function [state, numFrames, frequency] = prepareFrameIterations(model, ...
-    markersReference)
+    ikSolver, markersReference, params)
 state = model.initSystem();
 state.setTime(valueOrAlternate(params, 'startTime', ...
     markersReference.getValidTimeRange().get(0)));
