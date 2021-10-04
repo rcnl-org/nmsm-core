eee5db52b22d6afaf1d27cf713249f2760592ebd
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-10-03 19:19:17 -0500

    implementd makeIKSolverFromMarkersReference

---
+++ src/core/InverseKinematicsSolver/makeIKSolverFromMarkersReference.m
@@ -0,0 +1,19 @@
+% This function makes an InverseKinematicsSolver from a given Model and
+% MarkersReference object. It also accepts params like
+% makeInverseKinematicsSolver.
+
+% Copyright RCNL *change later*
+
+% (Model, MarkersReference, struct) -> (InverseKinematicsSolver)
+% Makes an InverseKinematicsSolver from a given Model and MarkersReference
+function ikSolver = makeIKSolverFromMarkersReference(model, ...
+    markersReference, params)
+import org.opensim.modeling.*
+ikSolver = InverseKinematicsSolver(model, markersReference, ...
+    SimTKArrayCoordinateReference());
+applyParametersToIKSolver(ikSolver, params)
+state = model.initSystem();
+state.setTime(valueOrZero(params, 'startTime'));
+ikSolver.assemble(state);
+end
+
