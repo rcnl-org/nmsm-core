2546f7e8be692d5411dbeb4294589f94d63b4008
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-10-03 19:41:51 -0500

    added valueOrAlternative implementation
    Former-commit-id: adf18620181847c5d7993c2df4c5d393379ed773

---
+++ src/core/valueOrAlternative.m
@@ -0,0 +1,15 @@
+% This function returns either the value of the field given or the
+% alternative value provided as a third argument.
+
+% Copyright RCNL *change later*
+
+% (struct, string, Any) -> (Any)
+
+function output = valueOrAlternative(inputStruct, fieldName, alternative)
+if(isfield(inputStruct, fieldName))
+    output = inputStruct.(fieldName);
+else
+    output = alternative;
+end
+end
+
