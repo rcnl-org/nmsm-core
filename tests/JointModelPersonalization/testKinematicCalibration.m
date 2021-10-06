2c4fb57be94bbeddf5db7f7f4aeef552cceb7dc4
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-10-05 21:43:40 -0500

    updated tests

--- tests/JointModelPersonalization/testKinematicCalibration.m
+++ tests/JointModelPersonalization/testKinematicCalibration.m
@@ -1,15 +1,16 @@
-function newModel = testKinematicCalibration()
+
+%% Kinematic Calibration Simple
 import org.opensim.modeling.*
-model = Model(strcat(pwd, '\tests\JointModelPersonalization\subject01_gait2392_scaled.osim'));
-markerfilename=strcat(pwd, '\tests\JointModelPersonalization\walk_free_01.trc');
+model = Model(strcat(pwd, '\subject01_gait2392_scaled.osim'));
+markerfilename=strcat(pwd, '\walk_free_01.trc');
 params = struct();
 functions = {@(value,model)adjustChildTranslation(model, 'hip_r', 0, value), ...
     @(value,model)adjustChildTranslation(model, 'hip_r', 1, value), ...
-    @(value,model)adjustChildTranslation(model, 'hip_r', 2, value), ...
-    @(value,model)adjustChildOrientation(model, 'hip_r', 0, value), ...
-    @(value,model)adjustChildOrientation(model, 'hip_r', 1, value), ...
-    @(value,model)adjustChildOrientation(model, 'hip_r', 2, value)};
+    @(value,model)adjustChildTranslation(model, 'hip_r', 2, value)};
 
 newModel = computeKinematicCalibration(model, markerfilename, functions, params);
-end
 
+class(newModel)
+class(Model())
+
+assert(strcmp(class(newModel),class(Model())))
--- tests/core/InverseKinematicsSolver/implementationTest.m
+++ tests/core/InverseKinematicsSolver/implementationTest.m
@@ -1,7 +1,7 @@
 % preconditions
 import org.opensim.modeling.*
-model = Model(strcat(pwd, '\tests\core\InverseKinematicsSolver\subject01_gait2392_scaled.osim'));
-markerfilename=strcat(pwd, '\tests\core\InverseKinematicsSolver\walk_free_01.trc');
+model = Model(strcat(pwd, '\subject01_gait2392_scaled.osim'));
+markerfilename=strcat(pwd, '\walk_free_01.trc');
 params.excludedMarkers = ["Sternum", "TopHead"];
 params.markerWeights = struct("RAcromium", 10.0, "LAcromium", 0.1);
 params.accuracy = 1.1;
