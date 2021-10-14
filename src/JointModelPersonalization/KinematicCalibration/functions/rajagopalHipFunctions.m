cf010584858782195d00182b66d3dc3470dd8c80
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-10-14 12:03:48 -0500

    added empty function files

---
+++ src/JointModelPersonalization/KinematicCalibration/functions/rajagopalHipFunctions.m
@@ -0,0 +1,11 @@
+% This function builds a 1 by x cell array of functions to be passed to
+% makeKinematicCalibration for optimizing the joint center of the hip for
+% a standard hip joint as used in the Rajagopal model
+
+% Copyright RCNL *change later*
+
+% (None) -> ({1 by x} cell array)
+% Returns a cell array of functions for optimizing a Rajagopal type hip
+function functions = rajagopalHipFunctions()
+
+end
