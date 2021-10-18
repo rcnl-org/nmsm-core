f2cf79eb3fe5e2a542e8c27e6fbefb78135311e4
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-10-18 01:34:14 -0500

    implemented makeJointFunction
    Former-commit-id: eb73f6d263fcf8b1bba7e07f3f0474b28899ee70

---
+++ src/JointModelPersonalization/KinematicCalibration/functions/makeJointFunction.m
@@ -0,0 +1,28 @@
+% This function returns the joint function representing the function
+% parameters for use as a function to be passed in the Joint Model
+% Personalization and Kinematic Calibration modules.
+
+% Copyright RCNL *change later*
+
+% (string, boolean, boolean, integer) -> (function)
+% Returns the appropriate function for given input parameters
+function fn = makeJointFunction(jointName, isParent, isTranslation, ...
+    coordinateNumber)
+if(isParent && isTranslation)
+    fn = @(value, model) adjustParentTranslation(model, jointName, ...
+        coordinateNumber, value);
+end
+if(isParent && ~isTranslation)
+    fn = @(value, model) adjustParentOrientation(model, jointName, ...
+        coordinateNumber, value);
+end
+if(~isParent && isTranslation)
+    fn = @(value, model) adjustChildTranslation(model, jointName, ...
+        coordinateNumber, value);
+end
+if(~isParent && ~isTranslation)
+    fn=@(value, model) adjustChildOrientation(model, jointName, ...
+        coordinateNumber, value);
+end
+end
+
