da4171d80324f91020b1e81ce57a9ba03aa18bce
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-10-21 21:24:09 -0500

    Update testKinematicCalibration.m

--- tests/JointModelPersonalization/testKinematicCalibration.m
+++ tests/JointModelPersonalization/testKinematicCalibration.m
@@ -1,8 +1,8 @@
 
 %% Kinematic Calibration Simple
 import org.opensim.modeling.*
-model = Model(strcat(pwd, '\subject01_gait2392_scaled.osim'));
-markerfilename=strcat(pwd, '\walk_free_01.trc');
+model = Model(strcat(pwd, '\tests\JointModelPersonalization\models\subject01_gait2392_scaled.osim'));
+markerfilename=strcat(pwd, '\tests\JointModelPersonalization\trc\walk_free_01.trc');
 params = struct();
 functions = {@(value,model)adjustChildTranslation(model, 'hip_r', 0, value), ...
     @(value,model)adjustChildTranslation(model, 'hip_r', 1, value), ...
