127af4d2137515bc03799cf16435749fa46f29fd
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-10-26 16:33:23 -0500

    fixed implementation bugs in JMPT
    Former-commit-id: c3d8af4aecd92809a0961a6b9d66ba23d0331d5f

--- src/JointModelPersonalization/JointModelPersonalizationTool.m
+++ src/JointModelPersonalization/JointModelPersonalizationTool.m
@@ -7,40 +7,47 @@
 % (string) -> (None)
 % Run JointModelPersonalization from settings file
 function JointModelPersonalizationTool(settingsFileName)
-settingsTree = xml2struct(settingsFileName);
+settingsTree = xml2struct(settingsFileName)
 % check arguments in advance to quit before computation if incorrect
 outputFile = getFieldByNameOrError(settingsTree, 'output_model_file').Text;
 resultsDir = getFieldByNameOrError(settingsTree, 'results_directory').Text;
-inputs = getInputs(settingsTree);
-params = getParams(settingsTree);
+inputs = getInputs(settingsTree)
+params = getParams(settingsTree)
 newModel = JointModelPersonalization(inputs, params);
-newModel.print(strcat(pwd,outputFile))
+newModel.print(strcat(resultsDir,outputFile));
 end
 
 function output = getInputs(tree)
-inputDirectory = getFieldByName('input_directory');
-modelFile = getFieldByNameOrError("input_model_file");
+inputDirectory = getFieldByName(tree, 'input_directory').Text;
+modelFile = getFieldByNameOrError(tree, 'input_model_file').Text;
 if(inputDirectory)
-    output.model = strcat(input, modelFile);
+    output.model = strcat(inputDirectory, '\', modelFile);
 else
-    output.model = modelFile;
+    output.model = strcat(modelFile);
 end
 model = org.opensim.modeling.Model(output.model);
-output.tasks = getTasks(model, tree);
+output.tasks = getTasks(model, tree, inputDirectory);
 end
 
-function output = getTasks(model, tree)
+function output = getTasks(model, tree, inputDirectory)
     tasks = getFieldByNameOrError(tree, 'TaskSet');
     for i=1:length(tasks.Task)
-        output{i} = getTask(model, tasks.Task{i});
+        output{i} = getTask(model, tasks.Task{i}, inputDirectory);
     end
 end
 
-function output = getTask(model, tree)
-    output.markerFile = tree.marker_file_name;
-    output.parameters = getJointParameters(tree.Joint)
-    translationBounds = str2double(getFieldByName('translation_bounds'));
-    orientationBounds = str2double(getFieldByName('orientation_bounds'));
+function output = getTask(model, tree, inputDirectory)
+    output.markerFile = strcat(inputDirectory, '\', ...
+        tree.marker_file_name.Text);
+    output.parameters = getJointParameters(tree.Joint);%includes all joints
+    translationBounds = getFieldByName(tree, 'translation_bounds');
+    if(isstruct(translationBounds))
+        translationBounds = str2double(translationBounds.Text);
+    end
+    orientationBounds = getFieldByName(tree, 'orientation_bounds');
+    if(isstruct(orientationBounds))
+        orientationBounds = str2double(orientationBounds.Text);
+    end
     output.initialValues = getInitialValues(model, output.parameters);
     if(translationBounds || orientationBounds)
         [output.lowerBounds, output.upperBounds] = getBounds(...
@@ -49,33 +56,73 @@ function output = getTask(model, tree)
     end
 end
 
-function output = getJointParameters(tree)
-
+% this function is long and ugly but is a rote and imperative way to
+% solve this problem, it's fine
+function output = getJointParameters(jointTree)
+counter = 1; % for index of parameter in output
+for i=1:length(jointTree)
+    if(length(jointTree) == 1)
+        joint = jointTree;
+    else
+        joint = jointTree{i};
+    end
+    jointName = joint.Attributes.name;
+    parentTrans = strsplit(joint.parent_frame.translation.Text, ...
+        ' ');
+    verifyLength(parentTrans, 3);
+    for j=0:2
+        output{counter} = {jointName, true, true, j};
+        counter = counter + 1;
+    end
+    parentOrient = strsplit(joint.parent_frame.orientation.Text, ...
+        ' ');
+    verifyLength(parentOrient, 3);
+    for j=0:2
+        output{counter} = {jointName, true, false, j};
+        counter = counter + 1;
+    end
+    childTrans = strsplit(joint.child_frame.translation.Text, ' ');
+    verifyLength(childTrans, 3);
+    for j=0:2
+        output{counter} = {jointName, false, true, j};
+        counter = counter + 1;
+    end
+    childOrient = strsplit(joint.child_frame.orientation.Text, ' ');
+    verifyLength(childOrient, 3);
+    for j=0:2
+        output{counter} = {jointName, false, false, j};
+    end
+end
 end
 
 function output = getInitialValues(model, parameters)
-
+for i=1:length(parameters)
+    temp = parameters{i};
+    output(i) = getFrameCoordinateValue(model, temp{1}, ...
+        temp{2}, temp{3}, temp{4});
+end
 end
 
 function [lowerBounds, upperBounds] = getBounds(parameters, ...
     initialValues, translationBounds, orientationBounds)
 for i=1:length(parameters)
-    if(parameters{3})
-        lowerBounds(i) = initialValues - translationBounds;
-        upperBounds(i) = initialValues + translationBounds;
+    if(parameters{i}{3})
+        lowerBounds(i) = initialValues(i) - translationBounds;
+        upperBounds(i) = initialValues(i) + translationBounds;
     else
-        lowerBounds(i) = initialValues - orientationBounds;
-        upperBounds(i) = initialValues + orientationBounds;
+        lowerBounds(i) = initialValues(i) - orientationBounds;
+        upperBounds(i) = initialValues(i) + orientationBounds;
     end
 end
 end
+
 function output = getParams(tree)
-paramArgs = ["accuracy","diffMinChange", "optimalityTolerance", ...
+paramArgs = ["accuracy", "diffMinChange", "optimalityTolerance", ...
     "functionTolerance", "stepTolerance", "maxFunctionEvaluations"];
 for i=1:length(paramArgs)
     value = getFieldByName(tree, paramArgs(i));
-    if(value)
-        output.(paramArgs(i)) = value;
+    if(isstruct(value))
+        output.(paramArgs(i)) = str2double(value.Text);
     end
 end
 end
