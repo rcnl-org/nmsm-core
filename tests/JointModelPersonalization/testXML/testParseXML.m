dcdcaea5c891863a49b1bac252edc188b8ddb6b6
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-10-26 16:32:51 -0500

    added example of xml
    Former-commit-id: 9c7093fd59c1d12f814ab3dac051c9fd44031255

---
+++ tests/JointModelPersonalization/testXML/testParseXML.m
@@ -0,0 +1,4 @@
+% test = xml2struct('tests\core\JointModelPersonalizationSettingsBJF.xml')
+
+JointModelPersonalizationTool( ...
+    'tests\JointModelPersonalization\JointModelPersonalizationSettingsBJF.xml');
