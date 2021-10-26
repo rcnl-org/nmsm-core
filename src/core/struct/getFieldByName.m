d08a49e522e0a9ff43be34a1a8fe91c6e93c70e8
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-10-26 13:10:55 -0500

    added getFieldByName w/ and w/o error
    Former-commit-id: 2d5b0600aa494d81bffd72c01afb1989efe13daa

---
+++ src/core/struct/getFieldByName.m
@@ -0,0 +1,24 @@
+% This function returns the first instance of a field matching the given
+% name and can be used to find a field in a struct that has been parsed
+% from and XML (xml2struct)
+
+% Copyright RCNL *change later*
+
+% (struct, field) => (struct)
+% Find first instance of field in nested struct
+function output = getFieldByName(deepStruct, field)
+output = false;
+try
+    output = deepStruct.(field);
+    return
+catch
+end
+fields = fieldnames(deepStruct);
+for i=1:length(fields)
+    output = getFieldByName(deepStruct.(fields{i}),field);
+    if(isstruct(output))
+        return
+    end
+end
+end
+
