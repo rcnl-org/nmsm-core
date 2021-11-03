3a41791adff47222e12e02c08c11990eacc33069
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-11-03 16:41:07 -0500

    added tests for verify functions

---
+++ tests/core/verify/testVerify.m
@@ -0,0 +1,35 @@
+% Preconditions
+import org.opensim.modeling.*
+model = Model('arm26.osim');
+
+%% Test verifyChar
+
+assertNoException(@()verifyChar('test'));
+assertException(@()verifyChar(6));
+assertException(@()verifyChar({}));
+
+%% Test verifyJointModelPersonalizationFunctionArgs
+
+assertNoException(@()verifyJointModelPersonalizationFunctionsArgs( ...
+    model, {{'r_elbow', 1, 1, 0}}));
+assertException(@()verifyJointModelPersonalizationFunctionArgs(model, ...
+    'hello'));
+
+%% Test verifyMarkersReferenceArg
+
+assertNoException(@()verifyMarkersReferenceArg('walk_free_01.trc'));
+assertException(@()verifyMarkersReferenceArg('walk_free_02.trc'));
+assertException(@()verifyMarkersReferenceArg(7));
+
+%% Test verifyModelArg
+
+assertNoException(@()verifyModelArg(model))
+assertNoException(@()verifyModelArg('arm26.osim'))
+assertException(@()verifyModelArg(6));
+
+%% Test verifyNumeric
+
+assertNoException(@()verifyNumeric(5));
+assertNoException(@()verifyNumeric(-0.00123));
+assertException(@()verifyNumeric('Hello'));
+
