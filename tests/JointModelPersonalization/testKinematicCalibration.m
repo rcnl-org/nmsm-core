38a049a20fafa81f7c8cc051c9455f26511ccf5f
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-11-24 16:07:03 -0600

    fixed tests

--- tests/JointModelPersonalization/testKinematicCalibration.m
+++ tests/JointModelPersonalization/testKinematicCalibration.m
@@ -3,11 +3,12 @@
 import org.opensim.modeling.*
 model = 'subject01_gait2392_scaled.osim';
 markerfilename='walk_free_01.trc';
+desiredError=0.005;
 params = struct();
 functions = {@(value,model)adjustChildTranslation(model, 'hip_r', 0, value), ...
     @(value,model)adjustChildTranslation(model, 'hip_r', 1, value), ...
     @(value,model)adjustChildTranslation(model, 'hip_r', 2, value)};
 
-optimizedValues = computeKinematicCalibration(model, markerfilename, functions, params);
+optimizedValues = computeKinematicCalibration(model, markerfilename, functions, desiredError, params);
 
 assert(isvector(optimizedValues))
