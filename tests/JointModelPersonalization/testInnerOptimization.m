05358cb3e8af80c77dd3ced1240af4974b170ed5
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-12-03 00:20:04 -0600

    adjusted fns to new Model and initSystem fns

--- tests/JointModelPersonalization/testInnerOptimization.m
+++ tests/JointModelPersonalization/testInnerOptimization.m
@@ -1,12 +1,11 @@
 % preconditions 
-import org.opensim.modeling.*
 % model = Model(strcat(pwd, '\tests\JointModelPersonalization\subject01_gait2392_scaled.osim'));
 model = Model('subject01_gait2392_scaled.osim');
 % markerfilename = strcat(pwd, '\tests\JointModelPersonalization\walk_free_01.trc');
 markerfilename = 'walk_free_01.trc';
 params.desiredError = 0.005;
 markersReference = makeMarkersReference(model, markerfilename, params);
-coordinateReference = SimTKArrayCoordinateReference();
+coordinateReference = org.opensim.modeling.SimTKArrayCoordinateReference();
 
 %% Test computeInnerOptimizationHeuristic
 
