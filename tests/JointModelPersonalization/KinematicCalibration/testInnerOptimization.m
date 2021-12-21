89d62f0c03fd9176ffe55662135c38898bbc4a93
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2021-12-21 10:55:02 -0600

    added simple arm tests for JMP, KinCal

--- tests/JointModelPersonalization/KinematicCalibration/testInnerOptimization.m
+++ tests/JointModelPersonalization/KinematicCalibration/testInnerOptimization.m
@@ -1,10 +1,10 @@
 % preconditions 
 % model = Model(strcat(pwd, '\tests\JointModelPersonalization\subject01_gait2392_scaled.osim'));
-model = Model('subject01_gait2392_scaled.osim');
+model = Model(fullfile('..','simple_arm_oriented_away.osim'));
 % markerfilename = strcat(pwd, '\tests\JointModelPersonalization\walk_free_01.trc');
-markerfilename = 'walk_free_01.trc';
-params.desiredError = 0.005;
-markersReference = makeMarkersReference(model, markerfilename, params);
+markerFile = fullfile('..','simple_arm.trc');
+params.desiredError = 0.01;
+markersReference = makeMarkersReference(model, markerFile, params);
 coordinateReference = org.opensim.modeling.SimTKArrayCoordinateReference();
 
 %% Test computeInnerOptimizationHeuristic
