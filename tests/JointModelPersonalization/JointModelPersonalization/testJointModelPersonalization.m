deab6abc5308bb81a4cbec73b0ca1d1a86c18b8e
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2021-12-21 10:55:02 -0600

    added simple arm tests for JMP, KinCal
    Former-commit-id: 23c8fef22e6950fbaab5d8cda2a91c05e1141362

--- tests/JointModelPersonalization/JointModelPersonalization/testJointModelPersonalization.m
+++ tests/JointModelPersonalization/JointModelPersonalization/testJointModelPersonalization.m
@@ -1,25 +1,56 @@
+
+%% Test translation convergence
 import org.opensim.modeling.*
 % model file path assumes nmsm-core project is open
-inputs.model = 'subject01_gait2392_scaled.osim';
+inputs.model = fullfile('..','simple_arm_translated_away.osim');
 
 % Joint parameters to optimize
 task1.parameters = {
     %{jointName, isParent, isTranslation, paramNum}
-    {'hip_r', 1, 1, 0}, ... %Translation of x in the parent frame
-    {'hip_r', 1, 1, 1}, ... %Translation of y in the parent frame
-    {'hip_r', 1, 1, 2}, ... %Translation of z in the parent frame
+    {'r_elbow', 1, 1, 0}, ... %Translation of x in the parent frame
 };
 % Associated marker file for task 1
-task1.markerFile = 'walk_free_01.trc';
+task1.markerFile = fullfile('..','simple_arm.trc');
 % Add task to cell array of tasks as part of the input struct
 inputs.tasks{1} = task1;
 
 % Add desired error
-inputs.desiredError = 0.005;
+inputs.desiredError = 0.01;
 
 params.accuracy = 1e-6; % accuracy of the Inverse Kinematics Solver
 params.display = 'iter';
 
 newModel = JointModelPersonalization(inputs, params);
-
 assert(isa(newModel, 'org.opensim.modeling.Model'))
+
+frameValue = getFrameParameterValue(newModel, 'r_elbow', 1, 1, 0);
+assert(abs(frameValue-0.0061)<0.0001)
+
+%% Test orientation convergence
+% model file path assumes nmsm-core project is open
+inputs.model = fullfile('..','simple_arm_oriented_away.osim');
+
+% Joint parameters to optimize
+task1.parameters = {
+    %{jointName, isParent, isTranslation, paramNum}
+    {'r_elbow', 1, 0, 0}, ... %Orientation of x in the parent frame
+};
+% Associated marker file for task 1
+task1.markerFile = fullfile('..','simple_arm.trc');
+task1.initialValues=[0.5];
+task1.lowerBounds=[-0.5];
+task1.upperBounds=[1.5];
+% Add task to cell array of tasks as part of the input struct
+inputs.tasks{1} = task1;
+
+% Add desired error
+inputs.desiredError = 0.01;
+
+params.accuracy = 1e-6; % accuracy of the Inverse Kinematics Solver
+params.display = 'iter';
+
+newModel = JointModelPersonalization(inputs, params);
+assert(isa(newModel, 'org.opensim.modeling.Model'))
+
+frameValue = getFrameParameterValue(newModel, 'r_elbow', 1, 0, 0)
+assert(abs(frameValue)<0.0001)
