08c48221cc02e60f41ab126093a5c860864750e4
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2022-01-04 18:05:34 -0600

    added license to all files in src
    Former-commit-id: a5eb7abbb2844cdf0dfaca5392b2b53c20b7abb9

--- src/core/model/getMarkerBodyName.m
+++ src/core/model/getMarkerBodyName.m
@@ -1,10 +1,33 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
 % This function takes a model and marker name and returns the name of the
 % body the marker is attached to
-
-% Copyright RCNL *change later*
-
+%
 % (Model, string) -> (string)
 % Returns a body name for a given model and marker name
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Claire Hammond                                               %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
 function bodyName = getMarkerBodyName(model, markerName)
 bodyName = model.findComponent(model.getMarkerSet().get(markerName)...
     .getSocket('parent_frame').getConnecteePath()).getName().toCharArray';
--- src/core/model/getMarkersFromJoint.m
+++ src/core/model/getMarkersFromJoint.m
@@ -1,13 +1,36 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
 % This function returns the markers attached to the distal and proximal
 % bodies of a given model and joint name.
 % REQUIRES PREREQUISITE system initialized model: model.initSystem()
 % or using nmsm-core's shadow Model() function which initSystem's
 % automatically
-
-% Copyright RCNL *change later*
-
+%
 % (Model, string) -> (1D Array of strings)
 % Returns the names of markers attached to the bodies around a given joint
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Claire Hammond                                               %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
 function markerNames = getMarkersFromJoint(model, jointName)
 markerNames = {};
 [parentName, childName] = getJointBodyNames(model, jointName);
