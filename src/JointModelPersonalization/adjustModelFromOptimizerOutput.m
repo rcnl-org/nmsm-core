464412b6ace143d108ebd8599d24c6f85fd4466f
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2022-01-04 18:05:34 -0600

    added license to all files in src

--- src/JointModelPersonalization/adjustModelFromOptimizerOutput.m
+++ src/JointModelPersonalization/adjustModelFromOptimizerOutput.m
@@ -1,10 +1,33 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
 % The optimizer returns the optimized values after completion, these values
 % should be used to change the input model to reflect the optimized state.
-
-% Copyright RNCL *change later*
-
-% (Model, cellArray, array, struct) -> (Model)
+%
+% (Model, cellArray, array) -> (Model)
 % Adjust the model based on the output of the optimizer
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Claire Hammond                                               %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
 function outputModel = adjustModelFromOptimizerOutput(model, functions, ...
     values)
 outputModel = Model(model);
