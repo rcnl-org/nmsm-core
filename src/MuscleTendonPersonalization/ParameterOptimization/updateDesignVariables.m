31ee54e705c1ba5d534fb21b1b73706496b669d4
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2022-01-11 12:41:31 -0600

    added structure for updateDesignVariables
    Former-commit-id: dafb7ca98afcf740588c55c3367ff6d9c05d2df2

--- src/MuscleTendonPersonalization/ParameterOptimization/updateDesignVariables.m
+++ src/MuscleTendonPersonalization/ParameterOptimization/updateDesignVariables.m
@@ -1,7 +1,37 @@
-function [outputArg1,outputArg2] = updateDesignVariables(inputArg1,inputArg2)
-%UPDATEDESIGNVARIABLES Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function takes the primary values (values maintained between
+% optimization rounds) and updates them based on the secondary values from
+% an individual round of optimization. The params included dictate which
+% primary values are updated.
+%
+% (Array of number, Array of number, struct) -> (struct)
+% Updates the primary values from the optimized round secondary values
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Claire V. Hammond                                            %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function newPrimaryValues = updateDesignVariables(primaryValues, ...
+    secondaryValues, params)
+
 end
 
