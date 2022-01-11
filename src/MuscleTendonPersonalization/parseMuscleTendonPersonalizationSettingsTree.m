0c89f4b65234b67520b958fbfadb03b5efe71c31
Jason Hammond <61138239+CVHammond@users.noreply.github.com>
2022-01-10 21:14:48 -0600

    Update parseMuscleTendonPersonalizationSettingsTree.m

--- src/MuscleTendonPersonalization/parseMuscleTendonPersonalizationSettingsTree.m
+++ src/MuscleTendonPersonalization/parseMuscleTendonPersonalizationSettingsTree.m
@@ -1,7 +1,44 @@
-function [outputArg1,outputArg2] = parseMuscleTendonPersonalizationSettingsTree(inputArg1,inputArg2)
-%PARSEMUSCLETENDONPERSONALIZATIONSETTINGSTREE Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function parses the settings tree resulting from xml2struct of the
+% Joint Model Personalization Settings XML file.
+%
+% (struct) -> (string, struct, struct)
+% returns the input values for Joint Model Personalization
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Claire V. Hammond                                            %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function [inputs, params] = ...
+    parseMuscleTendonPersonalizationSettingsTree(settingsTree)
+inputs = getInputs(settingsTree);
+params = getParams(settingsTree);
+end
+
+function inputs = getInputs(settingsTree)
+
+end
+
+function params = getParams(settingsTree)
+
 end
 
