aa11bba716c8d6ffee251ae38aa8a4e6ad7d4a19
Marleny Vega <marlenyarones101@gmail.com>
2022-01-24 13:20:01 -0600

    Update computeMuscleTendonCostFunction.m
    Added function information

--- src/MuscleTendonPersonalization/FreeVariableOptimization/computeMuscleTendonCostFunction.m
+++ src/MuscleTendonPersonalization/FreeVariableOptimization/computeMuscleTendonCostFunction.m
@@ -1,24 +1,46 @@
-function [outputArg1,outputArg2] = computeMuscleTendonCostFunction(inputArg1,inputArg2)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function calculates the cost associated to joint moment matching   %
+% while penalizing muscle parameter differences and violations.           %
+%
+% inputs
+%
+% (Array of number, struct) -> (Array of number)
+% returns the cost for all rounds of the Muscle Tendon optimization
 
-% inputArg1 = guess
-% inputArg2 = parameters structure
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Marleny Vega                                                 %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
 
+function [outputCost] = computeMuscleTendonCostFunction(values,params)
 
-% load parameters
+% Write calcMuscleActivations
 
-% Write computeMuscleActivations
+[muscleActivations, muscleExcitations] = calcMuscleActivations(params);
 
-[muscleActivations, muscleExcitations] = computeMuscleActivations(inputArg2);
-
-% Write computeMuscleMomentAndLength
+% Write calcMuscleMomentAndLength
 
 [modelJointMoments, lmtilda, vmtilda, ...
     modelMomentArms, muscleMoments, Lmt, ...
-    muscleForces, VmT, Fmax] = computeMuscleMomentAndLength(inputArg2);
-
-
+    muscleForces, VmT, Fmax] = calcMuscleMomentAndLength(params);
 
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
