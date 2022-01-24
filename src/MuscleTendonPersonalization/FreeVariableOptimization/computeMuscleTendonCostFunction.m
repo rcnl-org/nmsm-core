b483f5cba7d37ce4c59cdd29fa90e385388b0969
Marleny Vega <marlenyarones101@gmail.com>
2022-01-24 01:45:13 -0600

    Update computeMuscleTendonCostFunction.m
    Determining structure and additional functions needed
    
    
    Former-commit-id: 5f1b8d7a2bfeb956d6e6772933f7757d2580c353

--- src/MuscleTendonPersonalization/FreeVariableOptimization/computeMuscleTendonCostFunction.m
+++ src/MuscleTendonPersonalization/FreeVariableOptimization/computeMuscleTendonCostFunction.m
@@ -1,6 +1,25 @@
 function [outputArg1,outputArg2] = computeMuscleTendonCostFunction(inputArg1,inputArg2)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+
+% inputArg1 = guess
+% inputArg2 = parameters structure
+
+
+% load parameters
+
+% Write computeMuscleActivations
+
+[muscleActivations, muscleExcitations] = computeMuscleActivations(inputArg2);
+
+% Write computeMuscleMomentAndLength
+
+[modelJointMoments, lmtilda, vmtilda, ...
+    modelMomentArms, muscleMoments, Lmt, ...
+    muscleForces, VmT, Fmax] = computeMuscleMomentAndLength(inputArg2);
+
+
+
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
 end
