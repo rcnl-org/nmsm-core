8efcc122ed9dc71a71f27b777081d6b47507ae6c
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2022-01-31 12:19:08 -0600

    added reportDistanceErrorByMarker
    Former-commit-id: 2f795b8b850d3b3be6c27903bba5aa89e0c65595

--- src/JointModelPersonalization/KinematicCalibration/computeInnerOptimization.m
+++ src/JointModelPersonalization/KinematicCalibration/computeInnerOptimization.m
@@ -35,35 +35,12 @@ model = Model(model);
 for i = 1:length(values)
     functions{i}(values(i), model);
 end
-markersReference = makeOptMarkerRef(model, markerFileName, params);
+markersReference = makeJmpMarkerRef(model, markerFileName, params);
 error = computeInnerOptimizationHeuristic(model, ...
     markersReference, params);
 markersReference = libpointer;
 java.lang.System.gc();
 end
 
-function markersReference = makeOptMarkerRef(model, markerFileName, params)
-import org.opensim.modeling.*
-if(isfield(params, 'markerNames'))
-    markersReference = MarkersReference(markerFileName);
-    markersReference.setMarkerWeightSet(makeMarkerWeightSet( ...
-        params.markerNames, ones(1, length(params.markerNames))));
-    removeNonUsedMarkers(model, params.markerNames)
-else
-    markersReference = makeMarkersReference(model, markerFileName, params);
-end
-end
 
-function removeNonUsedMarkers(model, keptMarkerNames)
-toBeRemoved = [];
-for i=0:model.getMarkerSet().getSize()-1
-    markerName = model.getMarkerSet().get(i).getName();
-    if(~markerIncluded(keptMarkerNames, markerName))
-        toBeRemoved(length(toBeRemoved) + 1) = i;
-    end
-end
-markerSet = model.updMarkerSet();
-for i=1:length(toBeRemoved)
-    markerSet.remove(toBeRemoved(length(toBeRemoved)-i+1));
-end
-end
+
