6fc5f026c264e62e761c916b0e11d8c7f0a120ff
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2022-02-28 16:10:34 -0600

    Create verifyParam.m

---
+++ src/core/verify/verifyParam.m
@@ -0,0 +1,42 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% verifyParam takes a struct and checks that the field exists and passes
+% the value to the fn to verify the field's type. If it can't verify these
+% steps, throw and exception with the string given
+%
+% (struct, string, function, string) -> (None)
+% Runs the Muscle Tendon Personalization algorithm
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Claire V. Hammond                                            %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function verifyParam(structure, field, evalFunction, errorMessage)
+if(~isfield(structure, field))
+    throw(MException('', errorMessage))
+end
+try
+    evalFunction(structure.(field))
+catch
+    throw(MException('', errorMessage))
+end
+end
+
