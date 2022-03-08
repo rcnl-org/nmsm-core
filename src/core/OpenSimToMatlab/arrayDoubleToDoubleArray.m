acb1f0f759e8cd685d41010589e41fc250eb95e2
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2022-03-07 19:57:33 -0600

    added core data structure change functions

---
+++ src/core/OpenSimToMatlab/arrayDoubleToDoubleArray.m
@@ -0,0 +1,38 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function iterates through the given ArrayDouble and adds each item
+% to a new MATLAB Array of type double and returns the MATLAB structure.
+% Allows easier manipulation of MATLAB double than OpenSim ArrayDouble.
+%
+% (ArrayDouble) -> (Array of double)
+% Rearranges the ArrayDouble into a MATLAB double array
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Claire V. Hammond                                            %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function doubleArray = arrayDoubleToDoubleArray(arrayDouble)
+doubleArray = zeros(1, arrayDouble.size());
+for i=0:arrayDouble.size()-1
+    doubleArray(i+1) = arrayDouble.getitem(i);
+end
+end
+
