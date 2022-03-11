8f48c05a1806b8394c27376afb1e03be0475fd91
Claire V. Hammond <61138239+CVHammond@users.noreply.github.com>
2022-03-11 15:24:46 -0600

    added helper functions for column data retrieval
    Former-commit-id: b26d831380835163f0cb634fe58f6c1d9d155366

---
+++ src/core/storage/findStorageColumn.m
@@ -0,0 +1,37 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% Retrieves the column from an OpenSim Storage object and converts it to a
+% 1D array of double.
+%
+% (Storage, integer or string) -> (Array of double)
+% Returns storage column as array of double
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Claire V. Hammond                                            %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function columnData = findStorageColumn(storage, intOrString)
+import org.opensim.modeling.ArrayDouble
+arrayDouble = ArrayDouble();
+storage.getDataColumn(intOrString, arrayDouble);
+columnData = arrayDoubleToDoubleArray(arrayDouble);
+end
+
