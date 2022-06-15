daebb9b231dfea8e1c985a32c7186fc95cedfd99
Claire V. Hammond <cvhammond@mailbox.org>
2022-06-15 11:51:18 -0500

    showing preprocessing muscle analysis
    Former-commit-id: e01d112661480f1697bcaff991ddf9a6e7920522

---
+++ examples/MTPPreprocessing/MuscleAnalysisDebug/testPreprocessing.m
@@ -0,0 +1,36 @@
+
+% MuscleTendonPersonalizationPreprocessingTool("MTPPreprocessingLatest.xml")
+MuscleTendonPersonalizationPreprocessingTool("preprocessingTest.xml")
+
+% import org.opensim.modeling.*
+% 
+% % settingsTree = xml2struct("MTPPreprocessingLatest.xml");
+% % [inputs, params] = parseMtpPreprocessingSettingsTree(settingsTree);
+% 
+% % storage = Storage(strcat(pwd, "/MuscleAnalysis/optModel_v6_MuscleAnalysis_MomentArm_hip_adduction_l.sto"));
+% data = storageToDoubleMatrix(storage);
+% time = findTimeColumn(storage);
+% columnNames = getStorageColumnNames(storage);
+% coordinates = ["hip_adduction_l", "hip_flexion_l", "hip_rotation_l", "knee_angle_l", "ankle_angle_l", "subtalar_angle_l"];
+% 
+% [newData, newNames] = removeUnusedColumns(data, columnNames, "CPRIT_Patient3_MuscleGroups.osim", coordinates);
+
+% inputs.resultsDir = fullfile(pwd, "preprocessed");
+% inputs.ikResultsFileName = fullfile(pwd, "ik.mot");
+% inputs.idResultsFileName = fullfile(pwd, "id.mot");
+% inputs.maResultsDir = fullfile(pwd, "MuscleAnalysis");
+% inputs.emgFileName = fullfile(pwd, "processedEMGLeft.mot");
+% inputs.coordinates = ["hip_adduction_l", "hip_flexion_l", "hip_rotation_l", "knee_angle_l", "ankle_angle_l", "subtalar_angle_l"];
+% inputs.model = "CPRIT_Patient3_MuscleGroups.osim";
+% inputs.timePairs = [
+%     0.593, 1.667;
+%     1.667, 2.767;
+%     2.767, 3.827;
+%     3.827, 4.907;
+%     4.907, 5.993;
+%     ];
+% inputs.prefix = "gait";
+% 
+% params = struct();
+% 
+% processMotionLabData(inputs, params)
