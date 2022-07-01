10d5093b051ee0c2ea7713f92f912f0fdcda1556
Claire V. Hammond <cvhammond@mailbox.org>
2022-07-01 13:16:53 -0500

    added makeNormalizedGrid

--- src/GroundContactPersonalization/BSpline/BsplineNodes.m
+++ src/GroundContactPersonalization/BSpline/BsplineNodes.m
@@ -3,8 +3,8 @@ function [nodes] = BsplineNodes(time,q,degree,numNodes)
 % Construct B-spline nodes for specified time vector, spline degree, and
 % number of B-spline nodes, assuming the initial curve to be fitted is
 % a zero vector of the same length as the time vector.
-numPts = size(time,1);
-interval = time(2,1)-time(1,1);
+numPts = size(time);
+interval = time(2)-time(1);
 [N,~,~] = BSplineMatrices(degree,numNodes,numPts,interval);
 % [N] = BSplineMatrix(degree,numNodes,numPts);
 % Note that the interval setting is only needed to calculate first and
