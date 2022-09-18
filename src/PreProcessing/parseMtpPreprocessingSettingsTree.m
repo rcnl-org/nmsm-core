d393b03b15f4f35c249c9eaf37eaaa01d79025ae
Claire V. Hammond <jph9@rice.edu>
2022-09-18 18:50:39 -0500

    added default value to params
    Former-commit-id: 07443d5c7a3dbaa58b637360acedad78e4aa4aa8

--- src/PreProcessing/parseMtpPreprocessingSettingsTree.m
+++ src/PreProcessing/parseMtpPreprocessingSettingsTree.m
@@ -79,6 +79,7 @@ end
 end
 
 function params = getParams(tree)
+params = struct();
 paramArgs = ["preprocess_emg", "preprocess_emg_filter_degree", ...
     "preprocess_emg_high_pass_cutoff", "preprocess_emg_low_pass_cutoff"];
 % name in matlab is different, use for output struct arg name
