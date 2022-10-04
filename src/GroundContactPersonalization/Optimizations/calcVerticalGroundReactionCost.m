ae95c9edd9573d9dba8ed5e8c1516ac79e7b62a1
stwilliams333 <stwilliamsmail@gmail.com>
2022-10-04 16:19:34 -0500

    Add all non-slope costs to round 1

--- src/GroundContactPersonalization/Optimizations/calcVerticalGroundReactionCost.m
+++ src/GroundContactPersonalization/Optimizations/calcVerticalGroundReactionCost.m
@@ -66,20 +66,20 @@ bSplineCoefficients(:, [1:4, 6]) = reshape( ...
 end
 
 function cost = calcCost(inputs, modeledValues, valuesStruct)
-% [footMarkerPositionError, footMarkerSlopeError] = ...
-%     calcFootMarkerPositionAndSlopeError(inputs, modeledValues);
-% cost = 2 * footMarkerPositionError; %270
-cost = [];
+[footMarkerPositionError, footMarkerSlopeError] = ...
+    calcFootMarkerPositionAndSlopeError(inputs, modeledValues);
+cost = 2 * footMarkerPositionError; %270
+% cost = [];
 % cost = [cost 1000 * footMarkerSlopeError]; %12.8
 % cost = [cost 1000 * calcKinematicCurveSlopeError(inputs, modeledValues, [1:4, 6])]; %27.3
 [groundReactionForceValueError, groundReactionForceSlopeError] = ...
     calcVerticalGroundReactionForceAndSlopeError(inputs, modeledValues);
 cost = [cost groundReactionForceValueError]; %32772
 % cost = [cost 1 / 100 * groundReactionForceSlopeError]; %375970
-% cost = [cost 1 / 150 * calcSpringConstantsErrorFromMean(valuesStruct.springConstants)];
-% cost = [cost 50 * calcDampingFactorsErrorFromMean(valuesStruct.dampingFactors)];
-% cost = [cost calcSpringConstantDeviationFromInitialValueError(inputs.springConstants, valuesStruct.springConstants)];
-% cost = [cost calcDampingFactorDeviationFromInitialValueError(inputs.dampingFactors, valuesStruct.dampingFactors)];
-% cost = [cost calcSpringRestingLengthError(inputs.initialRestingSpringLength, valuesStruct.restingSpringLength)];
+cost = [cost 1 / 150 * calcSpringConstantsErrorFromMean(valuesStruct.springConstants)];
+cost = [cost 50 * calcDampingFactorsErrorFromMean(valuesStruct.dampingFactors)];
+cost = [cost calcSpringConstantDeviationFromInitialValueError(inputs.springConstants, valuesStruct.springConstants)];
+cost = [cost calcDampingFactorDeviationFromInitialValueError(inputs.dampingFactors, valuesStruct.dampingFactors)];
+cost = [cost calcSpringRestingLengthError(inputs.initialRestingSpringLength, valuesStruct.restingSpringLength)];
 end
 
