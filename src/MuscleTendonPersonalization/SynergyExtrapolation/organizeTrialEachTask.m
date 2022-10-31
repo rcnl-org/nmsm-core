12837dc8526b019e52bd4e7396c1faabe71fa158
Marleny Vega <marlenyarones101@gmail.com>
2022-10-31 14:27:45 -0500

    Added all synx functions to refactored mtp code
    Former-commit-id: 80675ea3379c9ffeeae56dd4935928283f3eecb0

--- src/MuscleTendonPersonalization/SynergyExtrapolation/organizeTrialEachTask.m
+++ src/MuscleTendonPersonalization/SynergyExtrapolation/organizeTrialEachTask.m
@@ -1,10 +1,14 @@
 % This function is part of the NMSM Pipeline, see file for full license.
 %
-% This function calculates the cost for tracking values 
+% This function organizes trials based on tasks   %
 %
-% (Array of number, Array of number, number, number) -> (struct)
-% returns tracking cost
-
+% data:
+%   Tasks - cell arrays containing the task names
+%   nTrials - number of trials in total - double
+%   nTasks -  number of tasks - double
+%   TrialNames - trial names for each trial
+%
+% returns trial index for each task
 % ----------------------------------------------------------------------- %
 % The NMSM Pipeline is a toolkit for model personalization and treatment  %
 % optimization of neuromusculoskeletal models through OpenSim. See        %
@@ -13,7 +17,7 @@
 % National Institutes of Health (R01 EB030520).                           %
 %                                                                         %
 % Copyright (c) 2021 Rice University and the Authors                      %
-% Author(s): Marleny Vega                                                 %
+% Author(s): Di Ao, Marleny Vega                                          %
 %                                                                         %
 % Licensed under the Apache License, Version 2.0 (the "License");         %
 % you may not use this file except in compliance with the License.        %
@@ -27,12 +31,36 @@
 % permissions and limitations under the License.                          %
 % ----------------------------------------------------------------------- %
 
-function cost = calcTrackingCostTerm(modelValue, experimentalValue, ...
-    errorCenter, maxAllowableError)
+function  params = organizeTrialEachTask(params,nTrials,TrialNames)
+%---Identify the trial index according to the labels in 'Tasks'
+trialIndex = cell(1,length(params.taskNames));
+for i=1:nTrials
+    for j=1:length(params.taskNames)
+        if contains(TrialNames{i},params.taskNames{j})
+            if isempty(trialIndex{j})
+                trialIndex{j} = i;
+            else
+                trialIndex{j} = [trialIndex{j} i];
+            end
+        end
+    end
+end
 
-errorMatching = modelValue - experimentalValue;
-cost = ((errorMatching - errorCenter) ./ maxAllowableError) .^ 2 ./ ...
-    numel(errorMatching);
-cost(isnan(cost))=0;
-cost = sum(cost, 'all');
+params.synergyCategorizationOfTrials = getCategorizationOfTrials(...
+    params.synergyExtrapolationCategorization, trialIndex, nTrials);
+params.residualCategorizationOfTrials = getCategorizationOfTrials(...
+    params.residualCategorization, trialIndex, nTrials);
+end
+function categorizationOfTrials = getCategorizationOfTrials(...
+    categorizationMethod, trialIndex, nTrials)
+
+if strcmpi(categorizationMethod, 'trial')
+    for i = 1 : nTrials
+        categorizationOfTrials{i} = i;
+    end
+elseif strcmpi(categorizationMethod, 'task')
+    categorizationOfTrials = trialIndex;
+elseif strcmpi(categorizationMethod, 'subject')
+    categorizationOfTrials = {1 : nTrials};
+end
 end
---
+++ src/MuscleTendonPersonalization/SynergyExtrapolation/synergyExcitationExtraction.m
@@ -0,0 +1,109 @@
+
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function calculates extract muscle synergy excitations from measured %
+% muscle excitations for both synX and residual excitation construction     %
+%
+% data:
+%   synXCommands - synergy excitations extracted from measured muscle excitations
+%   params.numberOfSynergies - number of synergies - double
+%   size(emgData,2) - number of trials in total - double
+%   params.numberOfMeasuredEmgChannels - number of measured EMG channels in total - double
+%   length(params.taskNames) -  number of tasks - double
+%   size(emgData,1) - number of time frames for each trial - double
+%   params.matrixFactorizationMethod - matrix factorization algorithm - 'PCA' or 'NMF'
+%   SynXCategory - variability of synergy vector weights across trials for
+%                  SynX reconstruction
+%   params.residualCategorization  - variability of synergy vector weights across trials for
+%                  residual excitation reconstruction
+%   TrialIndex - trial index for each task - cell array (nTask cells)
+%   emgData - Processed EMG signals (normalized to the maximum values over
+%                   all trials)
+%   NMF_resultFile - file that stores the results from NMF analysis - string
+%
+% returns measured synergy excitations for constructing SynX and residual 
+% muscle excitations.
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Di Ao, Marleny Vega                                          %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function [synXCommands, residualCommands] = ...
+    synergyExcitationExtraction(emgData, numberOfSynergies, ...
+    matrixFactorizationMethod, synergyCategorizationOfTrials, ...
+    residualCategorizationOfTrials) 
+
+%--Normalize EMGs 
+max_all_trials = max(max(emgData,[],3),[],1);
+normalizedEMG = permute(emgData./max_all_trials, [3 2 1]);
+%--Extract synergy excitations from measured muscle excitations 
+if strcmpi(matrixFactorizationMethod,'PCA')
+    synXCommands = getPcaCommands(normalizedEMG, numberOfSynergies, ...
+        synergyCategorizationOfTrials);
+    residualCommands = getPcaCommands(normalizedEMG, numberOfSynergies, ...
+        residualCategorizationOfTrials);
+elseif strcmpi(matrixFactorizationMethod,'NMF')
+    options = statset('Display', 'off', 'TolX', 1e-10, 'TolFun', 1e-10);
+    if  ~exist('nmfResultsSynX.mat')
+        synXCommands = getNmfCommands(normalizedEMG, numberOfSynergies, ...
+            synergyCategorizationOfTrials, options);
+        residualCommands = getNmfCommands(normalizedEMG, numberOfSynergies, ...
+            residualCategorizationOfTrials, options);
+        save('nmfResultsSynX.mat','synXCommands', 'residualCommands');
+    else
+        load('nmfResultsSynX.mat');
+    end
+end
+end
+function nmfCommands = getNmfCommands(normalizedEMG, ...
+    numberOfSynergies, categorizationOfTrials, options)
+
+for i = 1 : length(categorizationOfTrials)
+    [nmfCommands{i}, nmfWeight] = nnmf(reshape(normalizedEMG(:, :, ...
+        categorizationOfTrials{i}), size(normalizedEMG, 1) * ...
+        size(categorizationOfTrials{i}, 2), size(normalizedEMG, 2)), ...
+        numberOfSynergies, 'replicates', 20, 'algorithm', 'mult', ...
+        'options', options);
+    nmfCommands{i} = reshape(nmfCommands{i}, size(normalizedEMG, 1), ...
+        size(categorizationOfTrials{i}, 2), numberOfSynergies);
+for k = 1 : size(categorizationOfTrials{i}, 2)
+for j = 1 : numberOfSynergies
+    nmfCommands{i}(:, k, j) = spline(linspace(1, size(normalizedEMG, ...
+        1), 21), nmfCommands{i}(1 : (size(normalizedEMG, 1) - 1)/(21 - ...
+        1) : end, k, j), 1 : size(normalizedEMG, 1));
+end
+end
+nmfCommands{i} = reshape(nmfCommands{i}, size(normalizedEMG, ...
+    1) * size(categorizationOfTrials{i}, 2), numberOfSynergies);
+NormFactor = sum(nmfWeight, 2);
+nmfCommands{i} = nmfCommands{i} .* NormFactor';
+end
+end
+
+function pcaCommands = getPcaCommands(normalizedEMG, numberOfSynergies, ...
+    categorizationOfTrials)
+
+for i = 1 : length(categorizationOfTrials)
+    [~, principleComponents] = pca(reshape(permute(normalizedEMG(:, :, ...
+        categorizationOfTrials{i}), [1 3 2]), size(normalizedEMG, 1) * ...
+        size(categorizationOfTrials{i}, 2), size(normalizedEMG, 2)));
+    pcaCommands{i} = principleComponents(:, (1 : numberOfSynergies));
+end
+end
