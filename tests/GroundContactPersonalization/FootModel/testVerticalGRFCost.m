e76533efa31f195a30425a8c531d5990db00f3e9
Claire V. Hammond <jph9@rice.edu>
2022-11-20 03:06:21 -0600

    updated toe axis

--- tests/GroundContactPersonalization/FootModel/testVerticalGRFCost.m
+++ tests/GroundContactPersonalization/FootModel/testVerticalGRFCost.m
@@ -43,8 +43,9 @@ markerVelocities.(markerNamesFields{i}) = calcBSplineDerivative(time, ...
 end
 
 footModel = makeFootModel(bodyModel, toesJointName);
+footModel = 
 footModel = addSpringsToModel(footModel, markerNames, gridWidth, ...
-    gridHeight, hindfootBodyName, toesBodyName, isLeftFoot);
+    gridHeight, hindfootBodyName, toesBodyName, toesJointName, isLeftFoot);
 footModel.print("footModel2.osim");
 % footModel = Model("footModel.osim");
 % numSpringMarkers = findNumSpringMarkers(footModel);
@@ -106,3 +107,52 @@ footModel.print("footModel2.osim");
 % hold on
 % scatter(testInputs.time, modeledValues.verticalGrf, [], "blue")
 % hold off
+
+function model = findTwoPointsOnToeJointAxis(model, toesJointName, body)
+[model, state] = Model(model);
+point1 = getVec3Vertical(model.getJointSet().get(toesJointName).getParentFrame().getPositionInGround(state));
+rotationMat = getRotationMatrix(model.getJointSet().get(toesJointName).getParentFrame().getRotationInGround(state).asMat33());
+position2 = [0; 0; 0.1];
+point2 = (rotationMat * position2) + point1;
+point1 = (rotationMat * [0; 0; -0.1]) + point1;
+point1 = point1';
+point2 = point2';
+import org.opensim.modeling.Marker
+import org.opensim.modeling.Vec3
+state = model.initSystem();
+marker = Marker();
+marker.setName("point1");
+marker.setParentFrame(model.getBodySet().get(body));
+bodyPosition = model.getBodySet().get(body).getPositionInGround(state);
+Vec3(point1(1) - bodyPosition.get(1), ...
+    point1(2) - bodyPosition.get(2), point1(3) - bodyPosition.get(3))
+marker.set_location(Vec3(point1(1) - bodyPosition.get(1), ...
+    point1(2) - bodyPosition.get(2), point1(3) - bodyPosition.get(3)));
+model.addMarker(marker);
+
+marker = Marker();
+marker.setName("point2");
+marker.setParentFrame(model.getBodySet().get(body));
+% bodyPosition = model.getBodySet().get(body).getPositionInGround(state);
+marker.set_location(Vec3(point2(1) - bodyPosition.get(1), ...
+    point2(2) - bodyPosition.get(2), point2(3) - bodyPosition.get(3)));
+model.addMarker(marker);
+%     model.getBodySet().get("calcn_r").getPositionInGround(state)
+model.finalizeConnections()
+end
+
+function rotationMat = getRotationMatrix(rotation)
+rotationMat = zeros(3);
+for i = 0:2
+    for j = 0:2
+        rotationMat(i+1, j+1) = rotation.get(i, j);
+    end
+end
+end
+
+function verticalVec = getVec3Vertical(position)
+verticalVec = zeros(3, 1);
+for i = 0:2
+    verticalVec(i+1) = position.get(i);
+end
+end
