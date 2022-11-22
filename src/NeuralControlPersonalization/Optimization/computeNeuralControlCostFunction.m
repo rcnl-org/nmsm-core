588472de290f58924fd44c57a220cac0ad38143b
msshourijeh <msshourijeh>
2022-11-22 11:42:17 -0600

    First NCP Commit
    The structure is created. Several functions created based on the legacy that may need to be refactored as the sample data become available.

--- src/NeuralControlPersonalization/Optimization/computeNeuralControlCostFunction.m
+++ src/NeuralControlPersonalization/Optimization/computeNeuralControlCostFunction.m
@@ -1,7 +1,65 @@
-function [outputArg1,outputArg2] = computeNeuralControlCostFunction(inputArg1,inputArg2)
-%COMPUTENEURALCONTROLCOSTFUNCTION Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+% function cost = computeNeuralControlCostFunction(secondaryValues, ...
+%     primaryValues, isIncluded, experimentalData, params)
+%
+% values = makeMtpValuesAsStruct(secondaryValues, primaryValues, isIncluded);
+% modeledValues = calcNcpModeledValues(values, experimentalData, params);
+% cost = calcNcpCost(values, modeledValues, experimentalData, params);
+% end
 
+function cost = computeNeuralControlCostFunction(x,params)
+
+cost = calculateCost(x,params);
+
+end
+%--------------------------------------------------------------------------
+function cost = calculateCost(x,params)
+
+errs = calculateErrors(x,params);
+
+cost = errs'*errs;
+end
+%--------------------------------------------------------------------------
+function errs = calculateErrors(x,params)
+nJoints = params.nJoints; nPts = params.nPts; nMuscles = params.nMuscles;
+nMuscles_legs = params.nMuscles_legs; nMuscles_trunk = params.nMuscles_trunk;
+w_MTrack = params.w_MTrack;
+w_ActMin = params.w_ActMin;
+w_ActTrack = params.w_ActTrack;
+lMTVals = params.lMTVals;
+vMTVals = params.vMTVals;
+rVals = params.rVals;
+IDmomentVals = params.IDmomentVals;
+% Form muscle activations from design variables
+aVals = calcActivationsFromSynergyDesignVariables(x,params);
+EMGact_all = params.EMGact_all;
+
+% Calculate torque errors
+muscleJointMoments = zeros(nPts,nJoints);
+% muscleJointMoments = calcMuscleJointMoments(experimentalData, ...
+%     muscleActivations, normalizedFiberLength, normalizedFiberVelocity);
+% net moment
+for i = 1:nPts
+    for j = 1:nJoints
+        for k = 1:nMuscles
+            FMT = calcMuscleTendonForce(aVals(i,k),lMTVals(i,k),vMTVals(i,k),k,params);
+            r = rVals(i,k,j);
+            muscleJointMoments(i,j) = muscleJointMoments(i,j) + r*FMT;
+        end
+    end
+end
+torqueErrors = muscleJointMoments - IDmomentVals;
+torqueErrors = torqueErrors(:);
+
+
+
+momentTr_allow_err = 1; % 1 Nm is the allowable error for moment tracking error
+actTr_allow_err = params.actTr_allow_err;%0.05; % 0.05 is the allowablw error for activation tracking error
+
+ActTr_err = aVals(:,1:nMuscles_legs) - EMGact_all;
+ActTr_err = w_ActTrack^0.5*(ActTr_err(:)/actTr_allow_err)/(nPts*nMuscles_legs)^0.5;
+T_err = w_MTrack^0.5*(torqueErrors/momentTr_allow_err)/(nPts*nJoints)^0.5;
+ActMin_err = reshape(aVals(:,nMuscles_legs+1:end),[params.nPts*(nMuscles_trunk),1]);
+ActMin_err = w_ActMin^0.5*ActMin_err/(nPts*nMuscles_trunk)^0.5;
+
+errs = 1/sqrt(w_MTrack + w_ActTrack + w_ActMin)*[T_err; ActTr_err; ActMin_err];
+end
--- src/NeuralControlPersonalization/Optimization/computeNeuralControlOptimization.m
+++ src/NeuralControlPersonalization/Optimization/computeNeuralControlOptimization.m
@@ -1,7 +1,48 @@
-function [outputArg1,outputArg2] = computeNeuralControlOptimization(inputArg1,inputArg2)
-%COMPUTENEURALCONTROLOPTIMIZATION Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+% function optimizedValues = computeNeuralControlOptimization( ...
+%     initialValues, primaryValues, isIncluded, lowerBounds, upperBounds, ...
+%     experimentalData, params, optimizerOptions)
+%
+% optimizedValues = fmincon(@(values)computeMuscleTendonCostFunction( ...
+%     values, primaryValues, isIncluded, experimentalData, params), ...
+%     initialValues, [], [], [], [], lowerBounds, upperBounds, ...
+%     @(values)calcMuscleTendonNonLinearConstraints(values, params), ...
+%     optimizerOptions);
+% end
 
+function x = computeNeuralControlOptimization(x0,params)
+
+% Constraints
+nSynergies = params.nSynergies;
+nMuscles = params.nMuscles;
+nNodes = params.nNodes;
+nDesignVars = nSynergies*(nMuscles/2 + nNodes);
+A = [];
+b = [];
+Aeq = zeros(nSynergies,nDesignVars);
+beq = 1*ones(nSynergies,1);
+for i4 = 1:nSynergies
+    Aeq(i4,(i4-1)*(nNodes+nMuscles/2)+nNodes+1:i4*(nNodes+nMuscles/2)) = 1;
+end
+lb = zeros(nDesignVars,1);
+ub = [];
+nonlcon = [];
+
+
+options = optimoptions('fmincon','Display','iter','MaxIterations',1e3,...
+    'MaxFunctionEvaluations',1e3*length(x0),'Algorithm','sqp',...
+    'TypicalX',ones(nDesignVars,1),'UseParallel','always');
+
+% NcpOptimSettings.A = A;
+% NcpOptimSettings.b = b;
+% NcpOptimSettings.Aeq = Aeq;
+% NcpOptimSettings.beq = beq;
+% NcpOptimSettings.lb = lb;
+% NcpOptimSettings.ub = ub;
+% NcpOptimSettings.nonlcon = nonlcon;
+% NcpOptimSettings.options = options;
+
+%         [x,~,exitflag,~] = fmincon(@(x)calculateCost(x,lMTVals,vMTVals,rVals,IDmomentVals,params),...
+%             x0,A,b,Aeq,beq,lb,ub,nonlcon,options);
+[x,~,exitflag,~] = fmincon(@(x)computeNeuralControlCostFunction(x,params),...
+    x0,A,b,Aeq,beq,lb,ub,nonlcon,options);
+end
