ae455be068ffcd2eac0d548da6a6e2acdfcbd19e
Claire V. Hammond <jph9@rice.edu>
2022-11-28 23:09:55 -0600

    FMo to maxIsometricForce
    Former-commit-id: ca7f7e40db79750f1902896d13428ac1195a6109

--- tests/Neural Control Personalization/testNCP.m
+++ tests/Neural Control Personalization/testNCP.m
@@ -17,7 +17,7 @@ etmData_righttrunk = load('Patient3_etmData_refitted_right_trunk_1trial.mat');
 TrunkMuscParam = load('TrunkMuscleParams.mat').TrunkMuscParam;
 
 % % Muscle Parameters
-inputs.FMo = [EMGD_data.Fmax, EMGD_data.Fmax, TrunkMuscParam.FMo, TrunkMuscParam.FMo];
+inputs.maxIsometricForce = [EMGD_data.Fmax, EMGD_data.Fmax, TrunkMuscParam.maxIsometricForce, TrunkMuscParam.maxIsometricForce];
 inputs.optimalFiberLength = [EMGD_data.lmoOpt, EMGD_data.lmoOpt, TrunkMuscParam.lMo, TrunkMuscParam.lMo];
 inputs.tendonSlackLength = [EMGD_data.ltsOpt, EMGD_data.ltsOpt, TrunkMuscParam.lTs, TrunkMuscParam.lTs];
 inputs.pennationAngle = [EMGD_data.alpha, EMGD_data.alpha, TrunkMuscParam.alpha, TrunkMuscParam.alpha];
