d8e71150372f681d340e72be445ced28eeba1249
Claire V. Hammond <jph9@rice.edu>
2022-11-28 23:27:01 -0600

    updated vMmax to optimalFiberLength * vMaxFactor

--- src/NeuralControlPersonalization/Modeling/calcMuscleTendonForce.m
+++ src/NeuralControlPersonalization/Modeling/calcMuscleTendonForce.m
@@ -1,7 +1,7 @@
-function [muscleTendonForce, passiveMuscleTendonForce] = calcMuscleTendonForce(a,lMT,vMT,k,inputs)
+function [muscleTendonForce, passiveMuscleTendonForce] = calcMuscleTendonForce(a, muscleTendonLength, muscleTendonVelocity, k, inputs)
 
-normalizedFiberLength = (lMT - inputs.tendonSlackLength(k)) ./ (inputs.optimalFiberLength(k) .* cos(inputs.pennationAngle(k)));
-normalizedFiberVelocity = muscleTendonVelocity / inputs.vMmax(k);
+normalizedFiberLength = (muscleTendonLength - inputs.tendonSlackLength(k)) ./ (inputs.optimalFiberLength(k) .* cos(inputs.pennationAngle(k)));
+normalizedFiberVelocity = muscleTendonVelocity / (inputs.optimalFiberLength(k) * inputs.vMaxFactor);
 
 passiveMuscleTendonForce = (inputs.maxIsometricForce(k) .* cos(inputs.pennationAngle(k))) .* passiveForceLengthCurve(normalizedFiberLength);
 activeMuscleTendonForce = (inputs.maxIsometricForce(k) .* cos(inputs.pennationAngle(k))) .* activeForceLengthCurve(normalizedFiberLength) .* forceVelocityCurve(normalizedFiberVelocity);
