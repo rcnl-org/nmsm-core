759be47ad08990aa5d7b08e421032248181d0597
Claire V. Hammond <jph9@rice.edu>
2022-11-28 23:09:55 -0600

    FMo to maxIsometricForce

--- src/NeuralControlPersonalization/Modeling/calcMuscleTendonForce.m
+++ src/NeuralControlPersonalization/Modeling/calcMuscleTendonForce.m
@@ -4,8 +4,8 @@ function [FMT,FMTpassive] = calcMuscleTendonForce(a,lMT,vMT,k,inputs)
 lMtilda = (lMT - inputs.tendonSlackLength(k)) ./ (inputs.optimalFiberLength(k) .* cos(inputs.pennationAngle(k)));
 vMtilda = vMT / inputs.vMmax(k);
 
-FMTpassive = (inputs.FMo(k) .* cos(inputs.pennationAngle(k))) .* passiveForceLengthCurve(lMtilda);
-FMTactive = (inputs.FMo(k) .* cos(inputs.pennationAngle(k))) .* activeForceLengthCurve(lMtilda) .* forceVelocityCurve(vMtilda);
+FMTpassive = (inputs.maxIsometricForce(k) .* cos(inputs.pennationAngle(k))) .* passiveForceLengthCurve(lMtilda);
+FMTactive = (inputs.maxIsometricForce(k) .* cos(inputs.pennationAngle(k))) .* activeForceLengthCurve(lMtilda) .* forceVelocityCurve(vMtilda);
 FMT = a * FMTactive + FMTpassive;
 
 end
