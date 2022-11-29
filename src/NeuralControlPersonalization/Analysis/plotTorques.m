92c6c54def0202c7f884efa0af96d69a810d0961
Claire V. Hammond <jph9@rice.edu>
2022-11-28 21:56:15 -0600

    moved calculating muscleJointMoments into plotTorques

--- src/NeuralControlPersonalization/Analysis/plotTorques.m
+++ src/NeuralControlPersonalization/Analysis/plotTorques.m
@@ -1,9 +1,22 @@
-function plotTorques(torqueVals, jointNames, inputs, params)
+function plotTorques(jointNames, inputs, params)
+muscleJointMoments = zeros(inputs.nPts, inputs.nJoints);
+% net moment
+for i = 1:inputs.nPts
+    for j = 1:inputs.nJoints
+        for k = 1:inputs.numMuscles
+            FMT = calcMuscleTendonForce(aVals(i, k), ...
+                inputs.muscleTendonLength(i, k), ...
+                inputs.muscleTendonVelocity(i, k), k, inputs);
+            r = inputs.rVals(i, k, j);
+            muscleJointMoments(i, j) = muscleJointMoments(i, j) + r * FMT;
+        end
+    end
+end
 figure;
 for i = 1:inputs.nJoints
     subplot(4, 4, i)
     plot(inputs.NCPtimePercent, inputs.IDmomentVals(:, i), 'k-', ...
-        inputs.NCPtimePercent, torqueVals(:, i));
+        inputs.NCPtimePercent, muscleJointMoments(:, i));
     hold all
     if rem(i, 4) == 1
         ylabel('Joint Torque (Nm)');
