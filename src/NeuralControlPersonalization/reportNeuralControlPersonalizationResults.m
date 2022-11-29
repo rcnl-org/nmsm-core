eef8a2019ff0bc8c4889e5dc3d2ba5f241cbcf21
Claire V. Hammond <jph9@rice.edu>
2022-11-28 21:32:13 -0600

    separated plotTorques

--- src/NeuralControlPersonalization/reportNeuralControlPersonalizationResults.m
+++ src/NeuralControlPersonalization/reportNeuralControlPersonalizationResults.m
@@ -66,26 +66,7 @@ end
 
 
 %--------------------------------------------------------------------------
-function plotTorques(torqueVals, jointNames, inputs, params)
 
-figure;
-
-for i = 1:inputs.nJoints
-    subplot(4, 4, i), plot(inputs.NCPtimePercent, inputs.IDmomentVals(:, i), 'k-', inputs.NCPtimePercent, torqueVals(:, i)); hold all
-
-    if rem(i, 4) == 1
-        ylabel('Joint Torque (Nm)');
-    end
-
-    title(jointNames{i})
-
-    if i == 4
-        legend('ID', 'NCP');
-    end
-
-end
-
-end
 
 %--------------------------------------------------------------------------
 function plotSynergies(C, W, inputs, params) % to be tidied up
