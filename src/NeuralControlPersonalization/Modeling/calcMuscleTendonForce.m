eeb58bda9a6658517c19751670a6bfb3a3ad0cbe
Claire V. Hammond <jph9@rice.edu>
2022-11-28 18:56:54 -0600

    switched params for inputs

--- src/NeuralControlPersonalization/Modeling/calcMuscleTendonForce.m
+++ src/NeuralControlPersonalization/Modeling/calcMuscleTendonForce.m
@@ -1,11 +1,11 @@
 %--------------------------------------------------------------------------
-function [FMT,FMTpassive] = calcMuscleTendonForce(a,lMT,vMT,k,params)
+function [FMT,FMTpassive] = calcMuscleTendonForce(a,lMT,vMT,k,inputs)
 
-lMtilda = (lMT - params.lTs(k)) ./ (params.lMo(k) .* cos(params.alpha(k)));
-vMtilda = vMT / params.vMmax(k);
+lMtilda = (lMT - inputs.lTs(k)) ./ (inputs.lMo(k) .* cos(inputs.alpha(k)));
+vMtilda = vMT / inputs.vMmax(k);
 
-FMTpassive = (params.FMo(k) .* cos(params.alpha(k))) .* passiveForceLengthCurve(lMtilda);
-FMTactive = (params.FMo(k) .* cos(params.alpha(k))) .* activeForceLengthCurve(lMtilda) .* forceVelocityCurve(vMtilda);
+FMTpassive = (inputs.FMo(k) .* cos(inputs.alpha(k))) .* passiveForceLengthCurve(lMtilda);
+FMTactive = (inputs.FMo(k) .* cos(inputs.alpha(k))) .* activeForceLengthCurve(lMtilda) .* forceVelocityCurve(vMtilda);
 FMT = a * FMTactive + FMTpassive;
 
 end
