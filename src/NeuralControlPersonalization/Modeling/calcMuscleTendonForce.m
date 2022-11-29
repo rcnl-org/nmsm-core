9dae3d53dde9c7af8a21173210a4d8e32803ae83
Claire V. Hammond <jph9@rice.edu>
2022-11-28 23:32:35 -0600

    reformated cost fn

--- src/NeuralControlPersonalization/Modeling/calcMuscleTendonForce.m
+++ src/NeuralControlPersonalization/Modeling/calcMuscleTendonForce.m
@@ -1,11 +1,11 @@
-function [muscleTendonForce, passiveMuscleTendonForce] = calcMuscleTendonForce(a, muscleTendonLength, muscleTendonVelocity, k, inputs)
+function [muscleTendonForce, passiveMuscleTendonForce] = calcMuscleTendonForce(activation, muscleTendonLength, muscleTendonVelocity, k, inputs)
 
 normalizedFiberLength = (muscleTendonLength - inputs.tendonSlackLength(k)) ./ (inputs.optimalFiberLength(k) .* cos(inputs.pennationAngle(k)));
 normalizedFiberVelocity = muscleTendonVelocity / (inputs.optimalFiberLength(k) * inputs.vMaxFactor);
 
 passiveMuscleTendonForce = (inputs.maxIsometricForce(k) .* cos(inputs.pennationAngle(k))) .* passiveForceLengthCurve(normalizedFiberLength);
 activeMuscleTendonForce = (inputs.maxIsometricForce(k) .* cos(inputs.pennationAngle(k))) .* activeForceLengthCurve(normalizedFiberLength) .* forceVelocityCurve(normalizedFiberVelocity);
-muscleTendonForce = a * activeMuscleTendonForce + passiveMuscleTendonForce;
+muscleTendonForce = activation * activeMuscleTendonForce + passiveMuscleTendonForce;
 
 end
 
--- src/NeuralControlPersonalization/Optimization/computeNeuralControlCostFunction.m
+++ src/NeuralControlPersonalization/Optimization/computeNeuralControlCostFunction.m
@@ -7,33 +7,34 @@
 % end
 
 function cost = computeNeuralControlCostFunction(x, inputs, params)
-activations = calcActivationsFromSynergyDesignVariables(x,inputs, params);
+activations = calcActivationsFromSynergyDesignVariables(x, inputs, params);
 errs = calculateErrors(activations, inputs, params);
-cost = errs'*errs;
+cost = errs' * errs;
 end
 
 function errs = calculateErrors(activations, inputs, params)
 % Form muscle activations from design variables
 
 % Calculate torque errors
-muscleJointMoments = zeros(inputs.numPoints,inputs.numJoints);
+muscleJointMoments = zeros(inputs.numPoints, inputs.numJoints);
 % net moment
 for i = 1:inputs.numPoints
     for j = 1:inputs.numJoints
         for k = 1:inputs.numMuscles
-            FMT = calcMuscleTendonForce(activations(i,k),inputs.muscleTendonLength(i,k),inputs.muscleTendonVelocity(i,k),k,inputs);
-            r = inputs.momentArms(i,k,j);
-            muscleJointMoments(i,j) = muscleJointMoments(i,j) + r*FMT;
+            FMT = calcMuscleTendonForce(activations(i, k), inputs.muscleTendonLength(i, k), inputs.muscleTendonVelocity(i, k), k, inputs);
+            r = inputs.momentArms(i, k, j);
+            muscleJointMoments(i, j) = muscleJointMoments(i, j) + r * FMT;
         end
     end
 end
+
 torqueErrors = muscleJointMoments - inputs.inverseDynamicsMoments;
 
-actTrackErr = activations(:,1:inputs.numLegMuscles) - inputs.emgActivation;
-actTrackErr = inputs.activationTrackingWeight^0.5*(actTrackErr(:)/inputs.activationTrackingAllowableError)/(inputs.numPoints*inputs.numLegMuscles)^0.5;
-momentErr = inputs.momentTrackingWeight^0.5*(torqueErrors(:)/inputs.momentTrackingAllowableError)/(inputs.numPoints*inputs.numJoints)^0.5;
-actMinErr = reshape(activations(:,inputs.numLegMuscles+1:end),[inputs.numPoints*(inputs.numTrunkMuscles),1]);
-actMinErr = inputs.activationMinimizationWeight^0.5*(actMinErr/inputs.activationMinimizationAllowableError)/(inputs.numPoints*inputs.numTrunkMuscles)^0.5;
+actTrackErr = activations(:, 1:inputs.numLegMuscles) - inputs.emgActivation;
+actTrackErr = inputs.activationTrackingWeight^0.5 * (actTrackErr(:) / inputs.activationTrackingAllowableError) / (inputs.numPoints * inputs.numLegMuscles)^0.5;
+momentErr = inputs.momentTrackingWeight^0.5 * (torqueErrors(:) / inputs.momentTrackingAllowableError) / (inputs.numPoints * inputs.numJoints)^0.5;
+actMinErr = reshape(activations(:, inputs.numLegMuscles + 1:end), [inputs.numPoints * (inputs.numTrunkMuscles), 1]);
+actMinErr = inputs.activationMinimizationWeight^0.5 * (actMinErr / inputs.activationMinimizationAllowableError) / (inputs.numPoints * inputs.numTrunkMuscles)^0.5;
 
-errs = 1/sqrt(inputs.momentTrackingWeight + inputs.activationTrackingWeight + inputs.activationMinimizationWeight)*[momentErr; actTrackErr; actMinErr];
+errs = 1 / sqrt(inputs.momentTrackingWeight + inputs.activationTrackingWeight + inputs.activationMinimizationWeight) * [momentErr; actTrackErr; actMinErr];
 end
