25cba1cc866c3ed77f8c656b946fd335c860e006
Claire V. Hammond <jph9@rice.edu>
2022-11-28 21:58:05 -0600

    changed aVals to activations

--- src/NeuralControlPersonalization/Optimization/computeNeuralControlCostFunction.m
+++ src/NeuralControlPersonalization/Optimization/computeNeuralControlCostFunction.m
@@ -7,12 +7,12 @@
 % end
 
 function cost = computeNeuralControlCostFunction(x, inputs, params)
-aVals = calcActivationsFromSynergyDesignVariables(x,inputs, params);
-errs = calculateErrors(aVals, inputs, params);
+activations = calcActivationsFromSynergyDesignVariables(x,inputs, params);
+errs = calculateErrors(activations, inputs, params);
 cost = errs'*errs;
 end
 
-function errs = calculateErrors(aVals, inputs, params)
+function errs = calculateErrors(activations, inputs, params)
 % Form muscle activations from design variables
 
 % Calculate torque errors
@@ -21,7 +21,7 @@ muscleJointMoments = zeros(inputs.numPoints,inputs.numJoints);
 for i = 1:inputs.numPoints
     for j = 1:inputs.numJoints
         for k = 1:inputs.numMuscles
-            FMT = calcMuscleTendonForce(aVals(i,k),inputs.muscleTendonLength(i,k),inputs.muscleTendonVelocity(i,k),k,inputs);
+            FMT = calcMuscleTendonForce(activations(i,k),inputs.muscleTendonLength(i,k),inputs.muscleTendonVelocity(i,k),k,inputs);
             r = inputs.rVals(i,k,j);
             muscleJointMoments(i,j) = muscleJointMoments(i,j) + r*FMT;
         end
@@ -29,10 +29,10 @@ for i = 1:inputs.numPoints
 end
 torqueErrors = muscleJointMoments - inputs.IDmomentVals;
 
-actTrackErr = aVals(:,1:inputs.numMuscles_legs) - inputs.EMGact_all;
+actTrackErr = activations(:,1:inputs.numMuscles_legs) - inputs.EMGact_all;
 actTrackErr = inputs.w_ActTrack^0.5*(actTrackErr(:)/inputs.actTrackAllowErr)/(inputs.numPoints*inputs.numMuscles_legs)^0.5;
 momentErr = inputs.w_MTrack^0.5*(torqueErrors(:)/inputs.momentTrackAllowErr)/(inputs.numPoints*inputs.numJoints)^0.5;
-actMinErr = reshape(aVals(:,inputs.numMuscles_legs+1:end),[inputs.numPoints*(inputs.numMuscles_trunk),1]);
+actMinErr = reshape(activations(:,inputs.numMuscles_legs+1:end),[inputs.numPoints*(inputs.numMuscles_trunk),1]);
 actMinErr = inputs.w_ActMin^0.5*(actMinErr/inputs.actMinAllowErr)/(inputs.numPoints*inputs.numMuscles_trunk)^0.5;
 
 errs = 1/sqrt(inputs.w_MTrack + inputs.w_ActTrack + inputs.w_ActMin)*[momentErr; actTrackErr; actMinErr];
