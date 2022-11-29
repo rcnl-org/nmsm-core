0abc5b06ed1b59715c3da9ae68638725921666e9
Claire V. Hammond <jph9@rice.edu>
2022-11-28 21:58:05 -0600

    changed aVals to activations
    Former-commit-id: 25cba1cc866c3ed77f8c656b946fd335c860e006

--- src/NeuralControlPersonalization/Analysis/plotMuscleActivations.m
+++ src/NeuralControlPersonalization/Analysis/plotMuscleActivations.m
@@ -1,4 +1,4 @@
-function plotMuscleActivations(aVals, MuscLabels, inputs, params)
+function plotMuscleActivations(activations, MuscLabels, inputs, params)
 
 % Plot activation results
 rightLegMuscleIndices = 1:inputs.numMuscles_legs / 2;
@@ -6,10 +6,10 @@ leftLegMuscleIndices = inputs.numMuscles_legs / 2 + 1:inputs.numMuscles_legs;
 rightTrunkMuscleIndices = inputs.numMuscles_legs + 1:inputs.numMuscles_legs + inputs.numMuscles_trunk / 2;
 leftTrunkMuscleIndices = inputs.numMuscles_legs + inputs.numMuscles_trunk / 2 + 1:inputs.numMuscles_legs + inputs.numMuscles_trunk;
 
-if ~isempty(aVals)
+if ~isempty(activations)
     figure('Name', 'Right Leg Activations');
     for i = rightLegMuscleIndices
-        subplot(5, 9, i), plot(inputs.NCPtimePercent, aVals(:, i)); hold all;
+        subplot(5, 9, i), plot(inputs.NCPtimePercent, activations(:, i)); hold all;
         plot(inputs.NCPtimePercent, inputs.EMGact_all(:, i));
         title(sprintf('%s', MuscLabels{i}))
         axis([0 100 0 1])
@@ -25,7 +25,7 @@ if ~isempty(aVals)
     figure('Name', 'Left Leg Activations');
 
     for i = leftLegMuscleIndices
-        subplot(5, 9, i - rightLegMuscleIndices(end)), plot(inputs.NCPtimePercent, aVals(:, i)); hold all;
+        subplot(5, 9, i - rightLegMuscleIndices(end)), plot(inputs.NCPtimePercent, activations(:, i)); hold all;
         plot(inputs.NCPtimePercent, inputs.EMGact_all(:, i));
         title(sprintf('%s', MuscLabels{i}))
         axis([0 100 0 1])
@@ -41,7 +41,7 @@ if ~isempty(aVals)
     figure('Name', 'Right Trunk Activations');
 
     for i = rightTrunkMuscleIndices
-        subplot(5, 9, i - leftLegMuscleIndices(end)), plot(inputs.NCPtimePercent, aVals(:, i)); hold all;
+        subplot(5, 9, i - leftLegMuscleIndices(end)), plot(inputs.NCPtimePercent, activations(:, i)); hold all;
         title(sprintf('%s', MuscLabels{i}))
         axis([0 100 0 1])
 
@@ -55,7 +55,7 @@ if ~isempty(aVals)
     figure('Name', 'Left Trunk Activations');
 
     for i = leftTrunkMuscleIndices
-        subplot(5, 9, i - rightTrunkMuscleIndices(end)), plot(inputs.NCPtimePercent, aVals(:, i)); hold all;
+        subplot(5, 9, i - rightTrunkMuscleIndices(end)), plot(inputs.NCPtimePercent, activations(:, i)); hold all;
         title(sprintf('%s', MuscLabels{i}))
         axis([0 100 0 1])
 
--- src/NeuralControlPersonalization/reportNeuralControlPersonalizationResults.m
+++ src/NeuralControlPersonalization/reportNeuralControlPersonalizationResults.m
@@ -9,19 +9,19 @@ end
 save(fullfile(pwd, 'result', inputs.savefilename + ".mat"), 'x');
 
 % Reconstruct activation values
-aVals = calcActivationsFromSynergyDesignVariables(x, inputs, params);
+activations = calcActivationsFromSynergyDesignVariables(x, inputs, params);
 
 % Calculate muscle-tendon forces from optimal activations
 for i5 = 1:inputs.numPoints
     for k2 = 1:inputs.numMuscles
-        [FMTVals(i5, k2), FTPassive(i5, k2)] = calcMuscleTendonForce(aVals(i5, k2), inputs.muscleTendonLength(i5, k2), inputs.muscleTendonVelocity(i5, k2), k2, inputs);
+        [FMTVals(i5, k2), FTPassive(i5, k2)] = calcMuscleTendonForce(activations(i5, k2), inputs.muscleTendonLength(i5, k2), inputs.muscleTendonVelocity(i5, k2), k2, inputs);
     end
 end
 
-fprintf('Maximum estimated muscle activation is %f\n', max(max(aVals)));
+fprintf('Maximum estimated muscle activation is %f\n', max(max(activations)));
 
 % Plot activations and musce-ltendon forces
-plotMuscleActivations(aVals, inputs.MuscNames, inputs)
+plotMuscleActivations(activations, inputs.MuscNames, inputs)
 % plotMuscleForces(FMTVals, inputs.MuscNames,inputs) % can be added
 
 % Plot joint torque results
