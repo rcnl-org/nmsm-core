516ce24edb8348184f65aee9983ad2f7c6127b66
Claire V. Hammond <jph9@rice.edu>
2022-11-28 20:09:34 -0600

    updated names and shape of functions

--- src/NeuralControlPersonalization/Modeling/calcActivationsFromSynergyDesignVariables.m
+++ src/NeuralControlPersonalization/Modeling/calcActivationsFromSynergyDesignVariables.m
@@ -1,10 +1,10 @@
 function a = calcActivationsFromSynergyDesignVariables(x, inputs, params)
-nSynergies = inputs.nSynergies;
-nMuscles_leg = inputs.nMuscles_legs/2;
+numSynergies = inputs.numSynergies;
+numMuscles_leg = inputs.numMuscles_legs/2;
 % Unpack design variables
 [commands,weights] = unpackDesignVariables(x,inputs);
 
 % Form feedforward muscle activations from synergies
-a_right = commands(:,1:nSynergies/2)*weights(1:nSynergies/2,:);
-a_left = commands(:,nSynergies/2+1:end)*weights(nSynergies/2+1:end,:);
-a = [a_right(:,1:nMuscles_leg),a_left(:,1:nMuscles_leg),a_right(:,nMuscles_leg+1:end),a_left(:,nMuscles_leg+1:end)];
+a_right = commands(:,1:numSynergies/2)*weights(1:numSynergies/2,:);
+a_left = commands(:,numSynergies/2+1:end)*weights(numSynergies/2+1:end,:);
+a = [a_right(:,1:numMuscles_leg),a_left(:,1:numMuscles_leg),a_right(:,numMuscles_leg+1:end),a_left(:,numMuscles_leg+1:end)];
