76583fedb89b64246e280620563309462daafdb5
stwilliams333 <stwilliamsmail@gmail.com>
2022-12-11 18:34:50 -0600

    Preparing a series of tests for GCP settings

--- tests/GroundContactPersonalization/Stage0and1/testSeriesGcp.m
+++ tests/GroundContactPersonalization/Stage0and1/testSeriesGcp.m
@@ -1,116 +1,27 @@
 clear
 
-[inputs, params] = parseGroundContactPersonalizationSettingsTree(...
-    xml2struct("GCP_settings.xml"));
-inputs = prepareInputs(inputs, params);
-
-% Latest stage of GCP to test
 lastStage = 2;
 
-% Stage 0
-inputs = optimizeDeflectionAndSpringContants(inputs, params);
-% inputs.restingSpringLength = 0.04;
+for i = 1:8
 
-% Stage 1
-if lastStage >= 1
-    inputs = optimizeByVerticalGroundReactionForce(inputs, params);
+try
+    [inputs, params] = parseGroundContactPersonalizationSettingsTree(...
+        xml2struct("GCP_test_" + i + ".xml"));
+    inputs = prepareInputs(inputs, params);
+    inputs = optimizeDeflectionAndSpringContants(inputs, params);
+    if lastStage >= 1
+        inputs = optimizeByVerticalGroundReactionForce(inputs, params);
+    end
+    if lastStage >= 2
+        inputs = optimizeByGroundReactionForces(inputs, params);
+    end
+    save("testResultsGcp_" + i + ".mat")
+catch
+    disp("Failed to run test " + i)
 end
 
-% Stage 2
-if lastStage >= 2
-    inputs = optimizeByGroundReactionForces(inputs, params);
 end
 
-
-%% Report results, plot forces and kinematics
-
-[modeledJointPositions, modeledJointVelocities] = calcGCPJointKinematics( ...
-    inputs.experimentalJointPositions, inputs.jointKinematicsBSplines, ...
-    inputs.bSplineCoefficients);
-modeledValues = calcGCPModeledValues(inputs, inputs, ...
-    modeledJointPositions, modeledJointVelocities, [1, 1, (lastStage >= 2), 0], 0);
-modeledValues.jointPositions = modeledJointPositions;
-modeledValues.jointVelocities = modeledJointVelocities;
-
-disp('Unweighted Vertical GRF Cost: ')
-[groundReactionForceValueError, ~] = ...
-    calcVerticalGroundReactionForceAndSlopeError(inputs, ...
-    modeledValues);
-disp(sum(abs(groundReactionForceValueError)))
-if lastStage >= 2
-    [groundReactionForceValueErrors, ~] = ...
-        calcGroundReactionForceAndSlopeError(inputs, modeledValues);
-disp('Unweighted Anterior GRF Cost: ')
-disp(sum(abs(groundReactionForceValueErrors(1, :))))
-disp('Unweighted Lateral GRF Cost: ')
-disp(sum(abs(groundReactionForceValueErrors(3, :))))
-end
-disp('Unweighted Marker Tracking Cost: ')
-[footMarkerPositionError, ~] = ...
-    calcFootMarkerPositionAndSlopeError(inputs, modeledValues);
-disp(sum(abs(footMarkerPositionError)))
-
-figure(1)
-
-if lastStage < 2
-    scatter(inputs.time, ...
-        inputs.experimentalGroundReactionForces(2, :), [], "red")
-    hold on
-    scatter(inputs.time, modeledValues.verticalGrf, [], "blue")
-    hold off
-end
-if lastStage == 2
-    subplot(1,3,1);
-    scatter(inputs.time, ...
-        inputs.experimentalGroundReactionForces(2, :), [], "red")
-    hold on
-    scatter(inputs.time, modeledValues.verticalGrf, [], "blue")
-    hold off
-    subplot(1,3,2);
-    scatter(inputs.time, ...
-        inputs.experimentalGroundReactionForces(1, :), [], "red")
-    hold on
-    scatter(inputs.time, modeledValues.anteriorGrf, [], "blue")
-    hold off
-    subplot(1,3,3);
-    scatter(inputs.time, ...
-        inputs.experimentalGroundReactionForces(3, :), [], "red")
-    hold on
-    scatter(inputs.time, modeledValues.lateralGrf, [], "blue")
-    hold off
-end
-
-figure(2)
-
-subplot(1,4,1);
-scatter(inputs.time, inputs.experimentalJointPositions(1, :), [], "red")
-hold on
-scatter(inputs.time, modeledValues.jointPositions(1, :), [], "blue")
-hold off
-
-subplot(1,4,2);
-scatter(inputs.time, inputs.experimentalJointPositions(2, :), [], "red")
-hold on
-scatter(inputs.time, modeledValues.jointPositions(2, :), [], "blue")
-hold off
-
-subplot(1,4,3);
-scatter(inputs.time, inputs.experimentalJointPositions(3, :), [], "red")
-hold on
-scatter(inputs.time, modeledValues.jointPositions(3, :), [], "blue")
-hold off
-
-subplot(1,4,4);
-scatter(inputs.time, inputs.experimentalJointPositions(4, :), [], "red")
-hold on
-scatter(inputs.time, modeledValues.jointPositions(4, :), [], "blue")
-hold off
-
-%% Spring constant plot
-
-figure(3)
-footModel = Model("footModel2.osim");
-plotSpringConstants(footModel, inputs, inputs.toesBodyName, inputs.hindfootBodyName)
 
 %% Supporting functions from GroundContactPersonalization.m
 
