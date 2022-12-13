b4d61852edecae98f7eb9fa469b4394a87f104bf
stwilliams333 <stwilliamsmail@gmail.com>
2022-12-13 16:41:25 -0600

    GCP stage 3 parsing and cost function implemented
    Former-commit-id: 4c8375ad38ccd7b23b798f1f83b85648122f6105

--- src/GroundContactPersonalization/Optimizations/calcGroundReactionAndCenterOfPressureAndFreeMomentCost.m
+++ src/GroundContactPersonalization/Optimizations/calcGroundReactionAndCenterOfPressureAndFreeMomentCost.m
@@ -28,35 +28,131 @@
 % ----------------------------------------------------------------------- %
 
 function cost = calcGroundReactionAndCenterOfPressureAndFreeMomentCost( ...
-    values, inputs, params)
-[footMarkerPositionError, footMarkerSlopeError] = ...
-    calcFootMarkerPositionAndSlopeError();
-cost = 2 * footMarkerPositionError;
-cost = [cost 1000 * footMarkerSlopeError];
-cost = [cost 10000 * calcKinematicCurveSlopeError()];
-[groundReactionForceValueError, groundReactionForceSlopeError] = ...
-    calcGroundReactionForceAndSlopeError();
-cost = [cost groundReactionForceValueError];
-cost = [cost 1 / 5 * groundReactionForceSlopeError];
-cost = [cost 1 / 10 * calcSpringConstantsErrorFromMean()];
-cost = [cost 1 / 100 * calcKValueFromInitialValueError()];
-cost = [cost 100 * calcDampingFactorsErrorFromMean()];
-cost = [cost calcSpringRestingLengthError()];
-cost = [cost calcDampingFactorDeviationFromInitialValueError()];
-cost = [cost calcSpringConstantDeviationFromInitialValueError()];
-cost = [cost calcStaticFrictionDeviationError()];
-cost = [cost calcDynamicFrictionDeviationError()];
-cost = [cost calcViscousFrictionDeviationError()];
-cost = [cost 1000 * calcStaticToDynamicFrictionDeviationError()];
-[centerOfPressurePositionError, centerOfPressureSlopeError] = ...
-    calcCenterOfPressurePositionAndSlopeError();
-cost = [cost centerOfPressurePositionError];
-cost = [cost centerOfPressureSlopeError];
-[freeMomentValueError, freeMomentSlopeError] = ...
-    calcFreeMomentValueAndSlopeError();
-cost = [cost freeMomentValueError];
-cost = [cost freeMomentSlopeError];
+    values, fieldNameOrder, inputs, params)
+valuesStruct = unpackValues(values, inputs, fieldNameOrder);
+valuesBSplineCoefficients = ...
+    reshape(valuesStruct.bSplineCoefficients, [], 7);
+[modeledJointPositions, modeledJointVelocities] = calcGCPJointKinematics( ...
+    inputs.experimentalJointPositions, inputs.jointKinematicsBSplines, ...
+    valuesBSplineCoefficients);
+modeledValues = calcGCPModeledValues(inputs, valuesStruct, ...
+    modeledJointPositions, modeledJointVelocities, [1, 1, 1, 1], 1);
+modeledValues.jointPositions = modeledJointPositions;
+modeledValues.jointVelocities = modeledJointVelocities;
 
-cost = cost / 250;
+cost = calcCost(inputs, params, modeledValues, valuesStruct);
 end
+
+function valuesStruct = unpackValues(values, inputs, fieldNameOrder)
+valuesStruct = struct();
+start = 1;
+for i=1:length(fieldNameOrder)
+    valuesStruct.(fieldNameOrder(i)) = values(start:start + ...
+        numel(inputs.(fieldNameOrder(i))) - 1);
+    start = start + numel(inputs.(fieldNameOrder(i)));
+end
+end
+
+function cost = calcCost(inputs, params, modeledValues, valuesStruct)
+cost = [];
+if (params.stageThreeCosts.markerPositionError.isEnabled || ...
+        params.stageThreeCosts.markerSlopeError.isEnabled)
+    [footMarkerPositionError, footMarkerSlopeError] = ...
+        calcFootMarkerPositionAndSlopeError(inputs, modeledValues);
+end
+if (params.stageThreeCosts.markerPositionError.isEnabled)
+    maxAllowableError = ...
+        params.stageThreeCosts.markerPositionError.maxAllowableError;
+    cost = [cost sqrt(1 / (12 * 101)) * (1 / maxAllowableError * ...
+        footMarkerPositionError)];
+end
+if (params.stageThreeCosts.markerSlopeError.isEnabled)
+    maxAllowableError = ...
+        params.stageThreeCosts.markerSlopeError.maxAllowableError;
+    cost = [cost sqrt(1 / (12 * 101)) * (1 / maxAllowableError * ...
+        footMarkerSlopeError)];
+end
+if (params.stageThreeCosts.coordinateCoefficientError.isEnabled)
+    maxAllowableError = ...
+        params.stageThreeCosts.coordinateCoefficientError.maxAllowableError;
+    cost = [cost sqrt(1 / (25 * 7)) * (1 / maxAllowableError * ...
+        calcKinematicsBSplineCoefficientError(...
+        valuesStruct.bSplineCoefficients))];
+end
+if (params.stageThreeCosts.verticalGrfError.isEnabled || ...
+        params.stageThreeCosts.verticalGrfSlopeError.isEnabled || ...
+        params.stageThreeCosts.horizontalGrfError.isEnabled || ...
+        params.stageThreeCosts.horizontalGrfSlopeError.isEnabled)
+    [groundReactionForceValueErrors, groundReactionForceSlopeErrors] = ...
+        calcGroundReactionForceAndSlopeError(inputs, modeledValues);
+end
+if (params.stageThreeCosts.verticalGrfError.isEnabled)
+    maxAllowableError = ...
+        params.stageThreeCosts.verticalGrfError.maxAllowableError;
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionForceValueErrors(2, :)];
+end
+if (params.stageThreeCosts.verticalGrfSlopeError.isEnabled)
+    maxAllowableError = ...
+        params.stageThreeCosts.verticalGrfSlopeError.maxAllowableError;
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionForceSlopeErrors(2, :)];
+end
+if (params.stageThreeCosts.horizontalGrfError.isEnabled)
+    maxAllowableError = ...
+        params.stageThreeCosts.horizontalGrfError.maxAllowableError;
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionForceValueErrors(1, :)];
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionForceValueErrors(3, :)];
+end
+if (params.stageThreeCosts.horizontalGrfSlopeError.isEnabled)
+    maxAllowableError = ...
+        params.stageThreeCosts.horizontalGrfSlopeError.maxAllowableError;
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionForceSlopeErrors(1, :)];
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionForceSlopeErrors(3, :)];
+end
+if (params.stageThreeCosts.groundReactionMomentError.isEnabled || ...
+        params.stageThreeCosts.groundReactionMomentSlopeError.isEnabled)
+    [groundReactionMomentErrors, groundReactionMomentSlopeErrors] = ...
+        calcGroundReactionMomentAndSlopeError(inputs, modeledValues); 
+    % TODO implement moment and slope cost
+end
+if (params.stageThreeCosts.groundReactionMomentError.isEnabled)
+    maxAllowableError = ...
+        params.stageThreeCosts.groundReactionMomentError.maxAllowableError;
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionMomentErrors(1, :)];
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionMomentErrors(2, :)];
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionMomentErrors(3, :)];
+end
+if (params.stageThreeCosts.groundReactionMomentSlopeError.isEnabled)
+    maxAllowableError = ...
+        params.stageThreeCosts.groundReactionMomentSlopeError.maxAllowableError;
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionMomentSlopeErrors(1, :)];
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionMomentSlopeErrors(2, :)];
+    cost = [cost sqrt(1 / 101) * (1 / maxAllowableError) * ...
+        groundReactionMomentSlopeErrors(3, :)];
+end
+if (params.stageThreeCosts.springConstantErrorFromMean.isEnabled)
+    maxAllowableError = ...
+        params.stageThreeCosts.springConstantErrorFromMean.maxAllowableError;
+    cost = [cost sqrt(1 / length(valuesStruct.springConstants)) * ...
+        (1 / maxAllowableError) * ...
+        calcSpringConstantsErrorFromMean(valuesStruct.springConstants)];
+end
+if (params.stageThreeCosts.dampingFactorErrorFromMean.isEnabled)
+    maxAllowableError = ...
+        params.stageThreeCosts.dampingFactorErrorFromMean.maxAllowableError;
+    cost = [cost (1 / maxAllowableError) * ...
+        calcDampingFactorsErrorFromMean(valuesStruct.dampingFactors)];
+end
+end
+
 
