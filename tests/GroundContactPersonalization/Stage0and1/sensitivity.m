05bede5400996e0d3abbfb3b33cb01472c76c48b
stwilliams333 <stwilliamsmail@gmail.com>
2022-12-12 19:30:59 -0600

    Move GCP prepare inputs to separate file
    Former-commit-id: 1eb9229bf51a93968b7903698423ec3ad42f667c

--- tests/GroundContactPersonalization/Stage0and1/sensitivity.m
+++ tests/GroundContactPersonalization/Stage0and1/sensitivity.m
@@ -3,7 +3,7 @@ settingsFileName = "GCP_settings.xml";
 settingsTree = xml2struct(settingsFileName);
 [inputs, params, resultsDirectory] = ...
     parseGroundContactPersonalizationSettingsTree(settingsTree);
-inputs = prepareInputs(inputs, params);
+inputs = prepareGroundContactPersonalizationInputs(inputs, params);
 
 % inputs
 
@@ -33,87 +33,5 @@ if (params.stageOne.bSplineCoefficients.isEnabled)
     initialValues = [initialValues ...
         reshape(inputs.bSplineCoefficientsVerticalSubset, 1, [])];
     fieldNameOrder = [fieldNameOrder "bSplineCoefficientsVerticalSubset"];
-end
-end
-
-%% prepareInputs from file 'GroundContactPersonalization.m'
-% (struct, struct) -> (struct)
-% prepares optimization values from inputs
-function inputs = prepareInputs(inputs, params)
-% inputs.right.markerNames.toe = "R.Toe";
-% inputs.right.markerNames.medial = "R.Toe.Medial";
-% inputs.right.markerNames.lateral = "R.Toe.Lateral";
-% inputs.right.markerNames.heel = "R.Heel";
-% inputs.left.markerNames.toe = "L.Toe";
-% inputs.left.markerNames.medial = "L.Toe.Medial";
-% inputs.left.markerNames.lateral = "L.Toe.Lateral";
-% inputs.left.markerNames.heel = "L.Heel";
-inputs.gridWidth = 5;
-inputs.gridHeight = 15;
-% inputs.left.isLeftFoot = true;
-% inputs.right.isLeftFoot = false;
-
-if inputs.right.isEnabled
-    % Potentially refactor to use inputs.right if allowing multiple sides
-    inputs = prepareInputsForSide(inputs, inputs);
-end
-if inputs.left.isEnabled
-    inputs.left = prepareInputsForSide(inputs.left, inputs);
-end
-
-inputs.restingSpringLength = inputs.initialRestingSpringLength;
-inputs.dynamicFrictionCoefficient = ...
-    inputs.initialDynamicFrictionCoefficient;
-
-%% NEEDED TO BE ADDED MANUALLY %%
-initialSpringConstants = 2500; % Jackson et al 2016 Table 2
-initialDampingFactors = 1e-2; % 3e-6
-inputs.jointKinematicsBSplines = makeJointKinematicsBSplines(inputs.time, 4, 25);
-inputs.bSplineCoefficients = calcInitialDeviationNodes(25, 7);
-numSpringMarkers = findNumSpringMarkers(inputs.model);
-inputs.springConstants = initialSpringConstants * ...
-    ones(1, numSpringMarkers);
-inputs.dampingFactors = initialDampingFactors * ones(1, numSpringMarkers);
-coordinatesOfInterest = findGCPFreeCoordinates(inputs.bodyModel, inputs.toesBodyName);
-[footPosition, markerPositions] = makeFootKinematics(inputs.bodyModel, ...
-    inputs.motionFileName, coordinatesOfInterest, inputs.hindfootBodyName, ...
-    inputs.toesCoordinateName, inputs.markerNames);
-inputs.experimentalJointPositions = footPosition;
-inputs.experimentalMarkerPositions = markerPositions;
-markerNamesFields = fieldnames(inputs.markerNames);
-for i=1:length(markerNamesFields)
-markerVelocities.(markerNamesFields{i}) = calcBSplineDerivative(inputs.time, ...
-    markerPositions.(markerNamesFields{i}), 4, 21);
-end
-inputs.experimentalMarkerVelocities = markerVelocities;
-inputs.experimentalGroundReactionForcesSlope = calcBSplineDerivative( ...
-    inputs.time, inputs.experimentalGroundReactionForces, 2, 25);
-%% NEEDED TO BE ADDED MANUALLY %%
-end
-
-%% prepareInputsForSide from file 'GroundContactPersonalization.m'
-% (struct, struct) -> (struct)
-% prepares optimization values specific to a side
-function inputs = prepareInputsForSide(inputs, sharedInputs)
-inputs.toesJointName = char(Model(sharedInputs.bodyModel ...
-    ).getCoordinateSet().get(inputs.toesCoordinateName).getJoint(...
-    ).getName());
-[inputs.hindfootBodyName, inputs.toesBodyName] = ...
-    getJointBodyNames(Model(sharedInputs.bodyModel), inputs.toesJointName);
-inputs.coordinatesOfInterest = findGCPFreeCoordinates(...
-    Model(sharedInputs.bodyModel), string(inputs.toesBodyName));
-
-[footPosition, markerPositions] = makeFootKinematics(...
-    sharedInputs.bodyModel, sharedInputs.motionFileName, ... % import motion in parse settings tree or here?
-    inputs.coordinatesOfInterest, inputs.hindfootBodyName, ...
-    inputs.toesCoordinateName, inputs.markerNames);
-
-footVelocity = calcBSplineDerivative(inputs.time, footPosition, ...
-    4, 21);
-markerNamesFields = fieldnames(inputs.markerNames);
-for i=1:length(markerNamesFields)
-markerVelocities.(markerNamesFields{i}) = ...
-    calcBSplineDerivative(inputs.time, markerPositions.(...
-    markerNamesFields{i}), 4, 21);
 end
 end
