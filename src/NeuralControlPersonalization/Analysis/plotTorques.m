50ab6995d3c5b53753c465dddf38b0ea3e24af43
Claire V. Hammond <jph9@rice.edu>
2023-01-03 15:21:09 -0600

    changed bug on plot torques as well

--- src/NeuralControlPersonalization/Analysis/plotTorques.m
+++ src/NeuralControlPersonalization/Analysis/plotTorques.m
@@ -2,11 +2,12 @@ function plotTorques(activations, jointNames, inputs, params)
 muscleJointMoments = zeros(inputs.numPoints, inputs.numJoints);
 % net moment
 for i = 1:inputs.numPoints
-    for j = 1:inputs.numJoints
-        for k = 1:inputs.numMuscles
-            muscleTendonForce = calcMuscleTendonForce(activations(i, k), ...
-                inputs.muscleTendonLength(i, k), ...
-                inputs.muscleTendonVelocity(i, k), k, inputs);
+    for k = 1:inputs.numMuscles
+        muscleTendonForce = calcMuscleTendonForce(activations(i, k), ...
+            inputs.muscleTendonLength(i, k), ...
+            inputs.muscleTendonVelocity(i, k), k, inputs);
+        for j = 1:inputs.numJoints
+
             muscleJointMoments(i, j) = muscleJointMoments(i, j) + inputs.momentArms(i, k, j) * muscleTendonForce;
         end
     end
