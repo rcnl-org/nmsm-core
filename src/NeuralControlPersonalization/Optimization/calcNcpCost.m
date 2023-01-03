b8a1501659ff3ed4232332dedeb45bc798f9f48c
Claire V. Hammond <jph9@rice.edu>
2023-01-03 14:35:12 -0600

    moved ncp cost calculations to its own file

--- src/NeuralControlPersonalization/Optimization/calcNcpCost.m
+++ src/NeuralControlPersonalization/Optimization/calcNcpCost.m
@@ -29,10 +29,29 @@
 % permissions and limitations under the License.                          %
 % ----------------------------------------------------------------------- %
 
-function cost = computeNeuralControlCostFunction(x, inputs, params)
-activations = calcActivationsFromSynergyDesignVariables(x, inputs, params);
-errs = calcNcpCost(activations, inputs, params);
-cost = errs' * errs;
+function errs = calcNcpCost(activations, inputs, params)
+% Form muscle activations from design variables
+
+% Calculate torque errors
+muscleJointMoments = zeros(inputs.numPoints, inputs.numJoints);
+% net moment
+for i = 1:inputs.numPoints
+    for j = 1:inputs.numJoints
+        for k = 1:inputs.numMuscles
+            muscleTendonForce = calcMuscleTendonForce(activations(i, k), inputs.muscleTendonLength(i, k), inputs.muscleTendonVelocity(i, k), k, inputs);
+            momentArm = inputs.momentArms(i, k, j);
+            muscleJointMoments(i, j) = muscleJointMoments(i, j) + momentArm * muscleTendonForce;
+        end
+    end
 end
 
+torqueErrors = muscleJointMoments - inputs.inverseDynamicsMoments;
 
+actTrackErr = activations(:, 1:inputs.numLegMuscles) - inputs.emgActivation;
+actTrackErr = inputs.activationTrackingWeight^0.5 * (actTrackErr(:) / inputs.activationTrackingAllowableError) / (inputs.numPoints * inputs.numLegMuscles)^0.5;
+momentErr = inputs.momentTrackingWeight^0.5 * (torqueErrors(:) / inputs.momentTrackingAllowableError) / (inputs.numPoints * inputs.numJoints)^0.5;
+actMinErr = reshape(activations(:, inputs.numLegMuscles + 1:end), [inputs.numPoints * (inputs.numTrunkMuscles), 1]);
+actMinErr = inputs.activationMinimizationWeight^0.5 * (actMinErr / inputs.activationMinimizationAllowableError) / (inputs.numPoints * inputs.numTrunkMuscles)^0.5;
+
+errs = 1 / sqrt(inputs.momentTrackingWeight + inputs.activationTrackingWeight + inputs.activationMinimizationWeight) * [momentErr; actTrackErr; actMinErr];
+end
