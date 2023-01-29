4811c70a5ab08d3277f41d0dcf67376495dc46f3
Claire V. Hammond <jph9@rice.edu>
2023-01-29 16:52:04 -0600

    added tests removed unused fns

--- tests/core/model/testModelFns.m
+++ tests/core/model/testModelFns.m
@@ -20,3 +20,10 @@ assert(getFrameParameterValue(model, 'r_elbow', 0, 1, 1)==newCoord);
 newCoord = 13;
 applyFrameParameterValue(model, newCoord, 'r_elbow', 0, 0, 1);
 assert(getFrameParameterValue(model, 'r_elbow', 0, 0, 1)==newCoord);
+
+%% Check getBodyByName
+assertNoException(getBodyByName(model, "r_humerus"))
+
+%% Check getEnabledMusclesInOrder
+assert(all(strcmp(getEnabledMusclesInOrder(model), ...
+    ["TRIlong" "TRIlat" "TRImed" "BIClong" "BICshort" "BRA"])))
