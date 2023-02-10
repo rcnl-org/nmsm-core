7379affb7db3146cef048fa09965d937b5b002c3
stwilliams333 <stwilliamsmail@gmail.com>
2023-02-10 17:48:54 -0600

    Cutoff frequency to spline nodes

---
+++ src/core/BSpline/CutoffFrequencyToNumberOfNodes/splFitWithCutoff.m
@@ -0,0 +1,17 @@
+function nNodes = splFitWithCutoff(time, data, fCutoff, splineDegree, task)
+% Matlab function to extract time vector and data curves from an input data
+% file and determine the number of B-spline nodes required to match a
+% specified cutoff frequency for all curves
+
+% Extract relevant parameters
+params.fCutoff = fCutoff;
+params.splineDegree = splineDegree;
+params.plotFlag = 1; % plot comparison curves when finished
+
+% Calculate number of B-spline nodes required to match the specific cutoff
+% frequently for all curves
+nNodes = calcNumBsplNodesFromCutoffFreq(time, data, params);
+
+disp("The number of B-spline nodes for foot " + task + " is " + nNodes);
+
+end
