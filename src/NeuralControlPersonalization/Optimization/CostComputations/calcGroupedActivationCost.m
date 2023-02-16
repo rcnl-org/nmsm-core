25de7c1766eaab2b8d43af181aa7a52487cd7ec3
Claire V. Hammond <jph9@rice.edu>
2023-02-15 20:05:22 -0600

    Update calcGroupedActivationCost.m

--- src/NeuralControlPersonalization/Optimization/CostComputations/calcGroupedActivationCost.m
+++ src/NeuralControlPersonalization/Optimization/CostComputations/calcGroupedActivationCost.m
@@ -1,9 +1,8 @@
 function cost = calcGroupedActivationCost(activations, inputs, params) % This should be ONLY for those that DO NOT have EMG activations to track.
 cost = [];
 for i = 1 : length(params.activationGroups)
-    columns = ismember(inputs.activationColumnNames, ...
-        params.activationGroups{i});
-    groupActivations = activations(:, columns, :);
-    cost = [cost, groupActivations - mean(groupActivations, 2)];
+    groupActivations = activations(:, params.activationGroups{i}, :);
+    cost = [cost reshape(groupActivations - mean(groupActivations, 2), ...
+        1, [])];
 end
 end
