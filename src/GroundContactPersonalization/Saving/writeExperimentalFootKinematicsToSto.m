651c5504a47e39b87cdcaadce0c7eabff50c80ee
stwilliams333 <stwilliamsmail@gmail.com>
2023-02-22 20:41:05 -0600

    Save modeled and experimental ground reactions and kinematics to sto files

--- src/GroundContactPersonalization/Saving/writeExperimentalFootKinematicsToSto.m
+++ src/GroundContactPersonalization/Saving/writeExperimentalFootKinematicsToSto.m
@@ -1,7 +1,43 @@
-function [outputArg1,outputArg2] = writeExperimentalFootKinematicsToSto(inputArg1,inputArg2)
-%WRITEEXPERIMENTALFOOTKINEMATICSTOSTO Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% 
+%
+% (struct) -> (None)
+% Optimize ground contact parameters according to Jackson et al. (2016)
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Spencer Williams                                             %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function writeExperimentalFootKinematicsToSto(inputs, resultsDirectory, ...
+    modelName)
+columnLabels = ["Toe.Angle", "X.Rotation", "Y.Rotation", "Z.Rotation", ...
+    "X.Translation", "Y.Translation", "Z.Translation"];
+for foot = 1:length(inputs.tasks)
+    writeToSto( ...
+        columnLabels, ...
+        inputs.tasks{foot}.time, ...
+        inputs.tasks{foot}.experimentalJointPositions', ...
+        fullfile(resultsDirectory, strcat(modelName, "_Foot_", ...
+            num2str(foot), "_experimentalFootKinematics.sto")));
+end
 end
 
--- src/GroundContactPersonalization/Saving/writeReplacedExperimentalGroundReactionsToSto.m
+++ src/GroundContactPersonalization/Saving/writeReplacedExperimentalGroundReactionsToSto.m
@@ -1,7 +1,50 @@
-function [outputArg1,outputArg2] = writeReplacedExperimentalGroundReactionsToSto(inputArg1,inputArg2)
-%WRITEREPLACEDEXPERIMENTALGROUNDREACTIONSTOSTO Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% 
+%
+% (struct) -> (None)
+% Optimize ground contact parameters according to Jackson et al. (2016)
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Spencer Williams                                             %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function writeReplacedExperimentalGroundReactionsToSto(inputs, ...
+    resultsDirectory, modelName)
+columnLabels = ["Fx" "Fy" "Fz" "Mx" "My" "Mz" "ECx" "ECy" "ECz"];
+for foot = 1:length(inputs.tasks)
+    data = inputs.tasks{foot}.experimentalGroundReactionForces';
+    data = [data inputs.tasks{foot}.experimentalGroundReactionMoments'];
+    [~, markerPositions] = ...
+        makeFootKinematics(inputs.bodyModel, ...
+        inputs.motionFileName, inputs.tasks{foot}.coordinatesOfInterest,...
+        inputs.tasks{foot}.hindfootBodyName, ...
+        inputs.tasks{foot}.toesCoordinateName, ...
+        inputs.tasks{foot}.markerNames, ...
+        inputs.tasks{foot}.time(1), inputs.tasks{foot}.time(end));
+    markerPositions.midfootSuperior(2, :) = inputs.restingSpringLength;
+    data = [data markerPositions.midfootSuperior'];
+    writeToSto(columnLabels, inputs.tasks{foot}.time, data, ...
+        fullfile(resultsDirectory, strcat(modelName, "_Foot_", ...
+            num2str(foot), "_replacedExperimentalGroundReactions.sto")));
+end
 end
 
