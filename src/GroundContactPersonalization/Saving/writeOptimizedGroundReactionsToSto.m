37bd28f77a3881f9cafdb01352686439f9794894
stwilliams333 <stwilliamsmail@gmail.com>
2023-02-23 13:40:08 -0600

    GCP plotting functions using results files

--- src/GroundContactPersonalization/Saving/writeOptimizedGroundReactionsToSto.m
+++ src/GroundContactPersonalization/Saving/writeOptimizedGroundReactionsToSto.m
@@ -2,7 +2,7 @@
 %
 % 
 %
-% (struct) -> (None)
+% (struct, struct, string, string) -> (None)
 % Optimize ground contact parameters according to Jackson et al. (2016)
 
 % ----------------------------------------------------------------------- %
