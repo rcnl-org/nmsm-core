f7ea5702f1d7394180749dc8f7499e4b912bc737
stwilliams333 <stwilliamsmail@gmail.com>
2023-03-28 22:28:28 -0500

    Refactor GCP for inputs.surfaces

--- src/GroundContactPersonalization/prepareGroundContactPersonalizationInputs.m
+++ src/GroundContactPersonalization/prepareGroundContactPersonalizationInputs.m
@@ -38,11 +38,12 @@ inputs.gridHeight = 15;
 % number of spring markers. 
 meanRightFootMarkerLocations = getMeanFootMarkerLocations(inputs);
 
-for task = 1:length(inputs.tasks)
-    inputs.tasks{task} = prepareInputsForFoot(inputs.tasks{task}, ...
-        inputs, meanRightFootMarkerLocations, task);
+for surface = 1:length(inputs.surfaces)
+    inputs.surfaces{surface} = prepareInputsForFoot( ...
+        inputs.surfaces{surface}, inputs, meanRightFootMarkerLocations, ...
+        surface);
 end
-inputs.numSpringMarkers = confirmNumSpringMarkers(inputs.tasks);
+inputs.numSpringMarkers = confirmNumSpringMarkers(inputs.surfaces);
 
 % Initialize potential design variables from parsed initial values.
 inputs.springConstants = inputs.initialSpringConstants * ones(1, ...
@@ -57,58 +58,59 @@ end
 
 % (struct, struct, struct, double) -> (struct)
 % Prepares optimization values specific to a foot.
-function task = prepareInputsForFoot(task, inputs, meanMarkerLocations, ...
-    taskNumber)
-task.toesJointName = char(Model(inputs.bodyModel ...
-    ).getCoordinateSet().get(task.toesCoordinateName).getJoint(...
+function surface = prepareInputsForFoot(surface, inputs, ...
+    meanMarkerLocations, surfaceNumber)
+surface.toesJointName = char(Model(inputs.bodyModel ...
+    ).getCoordinateSet().get(surface.toesCoordinateName).getJoint( ...
     ).getName());
-[task.hindfootBodyName, task.toesBodyName] = ...
-    getJointBodyNames(Model(inputs.bodyModel), task.toesJointName);
-task.coordinatesOfInterest = findGCPFreeCoordinates(...
-    Model(inputs.bodyModel), string(task.toesBodyName));
+[surface.hindfootBodyName, surface.toesBodyName] = ...
+    getJointBodyNames(Model(inputs.bodyModel), surface.toesJointName);
+surface.coordinatesOfInterest = findGCPFreeCoordinates(...
+    Model(inputs.bodyModel), string(surface.toesBodyName));
 
 [footPosition, markerPositions] = ...
     makeFootKinematics(inputs.bodyModel, ...
-    inputs.motionFileName, task.coordinatesOfInterest, ...
-    task.hindfootBodyName, task.toesCoordinateName, ...
-    task.markerNames, task.time(1), task.time(end), task.isLeftFoot);
+    inputs.motionFileName, surface.coordinatesOfInterest, ...
+    surface.hindfootBodyName, surface.toesCoordinateName, ...
+    surface.markerNames, surface.time(1), surface.time(end), ...
+    surface.isLeftFoot);
 
 % Use a user-defined cutoff frequency to determine the number of B-spline
 % nodes needed to represent kinematics. 
-task.splineNodes = splFitWithCutoff(task.time, footPosition, ... 
-    inputs.kinematicsFilterCutoff, 4, taskNumber);
+surface.splineNodes = splFitWithCutoff(surface.time, footPosition, ... 
+    inputs.kinematicsFilterCutoff, 4, surfaceNumber);
 
-footVelocity = calcBSplineDerivative(task.time, footPosition, ...
-    4, task.splineNodes);
-markerNamesFields = fieldnames(task.markerNames);
+footVelocity = calcBSplineDerivative(surface.time, footPosition, ...
+    4, surface.splineNodes);
+markerNamesFields = fieldnames(surface.markerNames);
 for i=1:length(markerNamesFields)
 markerVelocities.(markerNamesFields{i}) = ...
-    calcBSplineDerivative(task.time, markerPositions.(...
-    markerNamesFields{i}), 4, task.splineNodes);
+    calcBSplineDerivative(surface.time, markerPositions.(...
+    markerNamesFields{i}), 4, surface.splineNodes);
 end
 
 taskFootModel = makeFootModel(Model(inputs.bodyModel), ...
-    task.toesJointName, task.isLeftFoot);
-taskFootModel = addSpringsToModel(taskFootModel, task.markerNames, ...
+    surface.toesJointName, surface.isLeftFoot);
+taskFootModel = addSpringsToModel(taskFootModel, surface.markerNames, ...
     inputs.gridWidth, inputs.gridHeight, ...
-    task.hindfootBodyName, task.toesBodyName, task.toesJointName, ...
-    task.isLeftFoot, meanMarkerLocations); 
-task.model = "footModel_" + taskNumber + ".osim";
-taskFootModel.print(task.model);
-task.numSpringMarkers = findNumSpringMarkers(task.model);
+    surface.hindfootBodyName, surface.toesBodyName, surface.toesJointName, ...
+    surface.isLeftFoot, meanMarkerLocations); 
+surface.model = "footModel_" + surfaceNumber + ".osim";
+taskFootModel.print(surface.model);
+surface.numSpringMarkers = findNumSpringMarkers(surface.model);
 
-task.experimentalMarkerPositions = markerPositions;
-task.experimentalMarkerVelocities = markerVelocities;
-task.experimentalJointPositions = footPosition;
-task.experimentalJointVelocities = footVelocity;
-task.midfootSuperiorPosition = markerPositions.midfootSuperior;
+surface.experimentalMarkerPositions = markerPositions;
+surface.experimentalMarkerVelocities = markerVelocities;
+surface.experimentalJointPositions = footPosition;
+surface.experimentalJointVelocities = footVelocity;
+surface.midfootSuperiorPosition = markerPositions.midfootSuperior;
 
-task.experimentalGroundReactionForcesSlope = calcBSplineDerivative( ...
-    task.time, task.experimentalGroundReactionForces, 2, ...
-    task.splineNodes);
-task.jointKinematicsBSplines = makeJointKinematicsBSplines(...
-    task.time, 4, task.splineNodes);
-task.bSplineCoefficients = ones(task.splineNodes, 7);
+surface.experimentalGroundReactionForcesSlope = calcBSplineDerivative( ...
+    surface.time, surface.experimentalGroundReactionForces, 2, ...
+    surface.splineNodes);
+surface.jointKinematicsBSplines = makeJointKinematicsBSplines(...
+    surface.time, 4, surface.splineNodes);
+surface.bSplineCoefficients = ones(surface.splineNodes, 7);
 end
 
 % (struct) -> (struct)
@@ -117,15 +119,15 @@ end
 % positions. Left foot marker Z positions are negated to be comparable. 
 function meanRightFootMarkerLocations = getMeanFootMarkerLocations(inputs)
 bodyModel = Model(inputs.bodyModel);
-for task = 1:length(inputs.tasks)
-    footMarkerLocations(task) = findMarkerPositions(bodyModel, ...
-        inputs.tasks{task}.markerNames);
-    if inputs.tasks{task}.isLeftFoot
-        for marker = 1:length(fieldnames(footMarkerLocations(task)))
-            fieldNames = fieldnames(footMarkerLocations(task));
-            currentMarker = footMarkerLocations(task).(fieldNames{marker});
+for surface = 1:length(inputs.surfaces)
+    footMarkerLocations(surface) = findMarkerPositions(bodyModel, ...
+        inputs.surfaces{surface}.markerNames);
+    if inputs.surfaces{surface}.isLeftFoot
+        for marker = 1:length(fieldnames(footMarkerLocations(surface)))
+            fieldNames = fieldnames(footMarkerLocations(surface));
+            currentMarker = footMarkerLocations(surface).(fieldNames{marker});
             currentMarker(2) = -1 * currentMarker(2);
-            footMarkerLocations(task).(fieldNames{marker}) = currentMarker;
+            footMarkerLocations(surface).(fieldNames{marker}) = currentMarker;
         end
     end
 end
@@ -134,19 +136,19 @@ for marker = 1:length(fieldnames(meanRightFootMarkerLocations))
     fieldNames = fieldnames(meanRightFootMarkerLocations);
     meanRightFootMarkerLocations.(fieldNames{marker}) = [0 0];
 end
-for task = 1:length(inputs.tasks)
-    for marker = 1:length(fieldnames(footMarkerLocations(task)))
-        fieldNames = fieldnames(footMarkerLocations(task));
+for surface = 1:length(inputs.surfaces)
+    for marker = 1:length(fieldnames(footMarkerLocations(surface)))
+        fieldNames = fieldnames(footMarkerLocations(surface));
         meanRightFootMarkerLocations.(fieldNames{marker}) = ...
             meanRightFootMarkerLocations.(fieldNames{marker}) + ...
-            footMarkerLocations(task).(fieldNames{marker});
+            footMarkerLocations(surface).(fieldNames{marker});
     end
 end
 for marker = 1:length(fieldnames(meanRightFootMarkerLocations))
     fieldNames = fieldnames(meanRightFootMarkerLocations);
     meanRightFootMarkerLocations.(fieldNames{marker}) = ...
         meanRightFootMarkerLocations.(fieldNames{marker}) ./ ...
-        length(inputs.tasks);
+        length(inputs.surfaces);
 end
 end
 
