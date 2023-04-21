b20ee632b8844898ae9cca48cac135f4810922a3
Claire V. Hammond <cvhammond@mailbox.org>
2023-04-21 14:47:55 -0500

    updated jmp examples

--- examples/JointModelPersonalization/IsolatedKneeExample/IsolatedKneeExample.m
+++ examples/JointModelPersonalization/IsolatedKneeExample/IsolatedKneeExample.m
@@ -19,7 +19,7 @@
 % implied. See the License for the specific language governing            %
 % permissions and limitations under the License.                          %
 % ----------------------------------------------------------------------- %
-clear;clc;
+
 % Make sure the nmsm-core project is open by double-clicking Project.prj in
 % the nmsm-core repository.
 settingsFileName = 'IsolatedReinboltKneeExample.xml';
