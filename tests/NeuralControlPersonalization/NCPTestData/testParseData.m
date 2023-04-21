bd40c4cd02a851c1645c06574bd71b640c1123c6
stwilliams333 <stwilliamsmail@gmail.com>
2023-04-20 19:53:08 -0500

    Fix NCP tests
    Former-commit-id: b37764afa246fa14072e0bcb8e20b7ee45777c8c

--- tests/NeuralControlPersonalization/NCPTestData/testParseData.m
+++ tests/NeuralControlPersonalization/NCPTestData/testParseData.m
@@ -5,5 +5,3 @@ settingsTree = xml2struct("newNCPSettingsFile.xml");
 [inputs, params, resultsDirectory] = ...
     parseNeuralControlPersonalizationSettingsTree(settingsTree);
 % NeuralControlPersonalization(inputs, params);
-
-% org.opensim.modeling.Storage("D:\RCNL\Repositories\nmsm-core\examples\NeuralControlPersonalization\NCPParse\preprocessed\mtpResults\muscleActivations\speed1_gait_1_muscleActivations.sto")
