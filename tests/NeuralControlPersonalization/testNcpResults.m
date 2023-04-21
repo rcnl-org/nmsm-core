b37764afa246fa14072e0bcb8e20b7ee45777c8c
stwilliams333 <stwilliamsmail@gmail.com>
2023-04-20 19:53:08 -0500

    Fix NCP tests

--- tests/NeuralControlPersonalization/testNcpResults.m
+++ tests/NeuralControlPersonalization/testNcpResults.m
@@ -1,11 +1,13 @@
 inputs = load("inputs.mat","inputs").inputs;
+params = load("params.mat","params").params;
+optimizedValues = load("optimizedValues.mat", "optimizedValues").optimizedValues;
 
 %% check activation calculation correctness
-activations = calcActivationsFromSynergyDesignVariables(inputs.initialValues, inputs, struct());
+activations = calcActivationsFromSynergyDesignVariables(optimizedValues, inputs, params);
 
 assertWithinRange(activations, load("activations.mat","activations").activations, 1e-9)
 
 %% check cost calculation correctness
-cost = calcNcpCost(load("activations.mat","activations").activations, inputs, struct());
+cost = calcNcpCost(load("activations.mat","activations").activations, inputs, params, optimizedValues);
 
 assertWithinRange(cost, load("cost.mat","cost").cost, 1e-9)
