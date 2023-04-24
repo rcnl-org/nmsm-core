7004e1ba829d65fcb26a0b4ea84725c1df29bd7d
Claire V. Hammond <61138239+cvhammond@users.noreply.github.com>
2023-04-24 15:18:22 -0500

    Create getModelOrOsimxInputs.m
    Co-Authored-By: Marleny Vega <29265181+MarlenyVega@users.noreply.github.com>
    
    Former-commit-id: 7948b5968dd6d4dba35244ce9bc164bb401bda2a

---
+++ src/core/parse/getModelOrOsimxInputs.m
@@ -0,0 +1,71 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% () -> ()
+% 
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Marleny Vega                                                 %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function inputs = getModelOrOsimxInputs(inputs)
+if ~isa(inputs.model, 'org.opensim.modeling.Model')
+    model = Model(inputs.model);
+end
+inputs.optimalFiberLength = [];
+inputs.tendonSlackLength = [];
+inputs.pennationAngle = [];
+inputs.maxIsometricForce = [];
+for i = 1 : inputs.numMuscles
+    if isfield(inputs.osimx.muscles, inputs.muscleNames(i))
+        if isnan(inputs.osimx.muscles.(inputs.muscleNames(i)).optimalFiberLength)
+            inputs.optimalFiberLength(end+1) = model.getForceSet(). ...
+                getMuscles().get(i-1).getOptimalFiberLength();
+        else
+            inputs.optimalFiberLength(end+1) = inputs.osimx.muscles. ...
+                (inputs.muscleNames(i)).optimalFiberLength;
+        end
+        if isnan(inputs.osimx.muscles.(inputs.muscleNames(i)).tendonSlackLength)
+            inputs.tendonSlackLength(end+1) = model.getForceSet(). ...
+                getMuscles().get(i-1).getTendonSlackLength();
+        else
+            inputs.tendonSlackLength(end+1) = inputs.osimx.muscles. ...
+                (inputs.muscleNames(i)).tendonSlackLength;
+        end
+        if isnan(inputs.osimx.muscles.(inputs.muscleNames(i)).maxIsometricForce)
+            inputs.maxIsometricForce(end+1) = model.getForceSet(). ...
+                getMuscles().get(i-1).getMaxIsometricForce();
+        else
+            inputs.maxIsometricForce(end+1) = inputs.osimx.muscles. ...
+                (inputs.muscleNames(i)).maxIsometricForce;
+        end
+    else
+        inputs.optimalFiberLength(end+1) = model.getForceSet(). ...
+            getMuscles().get(i-1).getOptimalFiberLength();
+        inputs.tendonSlackLength(end+1) = model.getForceSet(). ...
+            getMuscles().get(i-1).getTendonSlackLength();
+        inputs.maxIsometricForce(end+1) = model.getForceSet(). ...
+            getMuscles().get(i-1).getMaxIsometricForce();
+    end
+    inputs.pennationAngle(end+1) = model.getForceSet(). ...
+        getMuscles().get(i-1). ...
+        getPennationAngleAtOptimalFiberLength();
+end
+end
