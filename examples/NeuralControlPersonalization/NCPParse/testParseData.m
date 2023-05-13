258201286d762f0f453739cb0ed1d31f3eb8d6e6
Claire V. Hammond <61138239+cvhammond@users.noreply.github.com>
2023-05-13 09:14:23 -0700

    removed examples
    Former-commit-id: 76ef137f01f2b3c1685ccbb4635d2ed72d40c245

--- examples/NeuralControlPersonalization/NCPParse/testParseData.m
+++
@@ -1,7 +0,0 @@
-settingsTree = xml2struct("NCPLatest.xml");
-[inputs, params, resultsDirectory] = ...
-    parseNeuralControlPersonalizationSettingsTree(settingsTree);
-NeuralControlPersonalization(inputs, ...
-    params);
-
-% org.opensim.modeling.Storage("D:\RCNL\Repositories\nmsm-core\examples\NeuralControlPersonalization\NCPParse\preprocessed\mtpResults\muscleActivations\speed1_gait_1_muscleActivations.sto")
--- examples/TrackingOptimization/LegsExample/SynergyDriven/test.m
+++
@@ -1,6 +0,0 @@
-TrackingOptimizationTool("updatedSynergyDrivenTrackingOptimizationSettings.xml")
-
-% load('test.mat','pathConstraintTermsTree')
-% 
-% pathConstraints = getFieldByName(pathConstraintTermsTree, ...
-%     'RCNLConstraintTerm')
