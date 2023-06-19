89cb30872552ec8fcd1a2c6a8d20a40fd7e39bbf
Marleny Vega <marlenyarones101@gmail.com>
2023-06-18 23:06:43 -0500

    Added max isometric force parameter option

---
+++ src/core/TreatmentOptimization/BuiltInParameters/applyMaxIsometricForceParameter.m
@@ -0,0 +1,45 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function adjusts the given model joint parent frame translation
+% coordinate to the given value argument.
+%
+% (Model, string, integer, number) -> ()
+% Mutates model joint parent frame translation coordinate to new value
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Marleny Vega                                                 %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function inputs = applyMaxIsometricForceParameter(inputs, values)
+
+parameterSet = inputs.auxdata.userDefinedVariables;
+inputs.auxdata.model = Model(inputs.auxdata.model);
+counter = 1;
+for i = 1:length(parameterSet)
+    if strcmp(parameterSet{i}.type, 'max_isometric_force')
+        adjustModelMaxIsometricForce(inputs.auxdata.model, ...
+            parameterSet{i}.muscle, values.max_isometric_force(counter));
+        inputs = adjustMaxIsometricForce(inputs, ...
+            parameterSet{i}.muscle, values.max_isometric_force(counter));
+        counter = counter + 1;
+    end
+end
+end
