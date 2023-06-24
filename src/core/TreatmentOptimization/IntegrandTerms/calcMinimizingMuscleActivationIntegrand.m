eaa0fcfd0f4b4fe8560f995964a067ec225a433e
Marleny Vega <marlenyarones101@gmail.com>
2023-06-23 23:17:05 -0500

    Added function descriptions for cost functions related to muscle activations

--- src/core/TreatmentOptimization/IntegrandTerms/calcMinimizingMuscleActivationIntegrand.m
+++ src/core/TreatmentOptimization/IntegrandTerms/calcMinimizingMuscleActivationIntegrand.m
@@ -1,6 +1,8 @@
 % This function is part of the NMSM Pipeline, see file for full license.
 %
-% () -> ()
+% This function minimizes the muscle activation for the specified muscle.
+%
+% (2D matrix, struct, Array of string) -> (Array of number)
 % 
 
 % ----------------------------------------------------------------------- %
