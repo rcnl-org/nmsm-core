5e0c418009a58158c4fa967fa32d1dd1584074f4
Spencer Williams <54556034+SpencerTWilliams@users.noreply.github.com>
2023-08-03 13:37:10 +0900

    Documentation for plotMomentMatchingResults

--- src/NeuralControlPersonalization/Analysis/plotMomentMatchingResults.m
+++ src/NeuralControlPersonalization/Analysis/plotMomentMatchingResults.m
@@ -1,3 +1,34 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% Plot modeled and experimental moments for one trial resulting from Neural 
+% Control Personalization or other tools from files. These plots include 
+% RMS error. 
+%
+% (string, string, double, double) -> (None)
+% Plot modeled and experimental moments from files. 
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Benjamin J. Fregly, Spencer Williams                         %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
 function plotMomentMatchingResults(experimentalMomentsFile, ...
     modeledMomentsFile, figureWidth, figureHeight)
 import org.opensim.modeling.Storage
