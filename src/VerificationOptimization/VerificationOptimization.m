1e3e06b19ae16647229693ff82bc4996fe57a932
Claire V. Hammond <61138239+cvhammond@users.noreply.github.com>
2023-09-06 03:00:32 -0500

    move setupMuscleSynergies

--- src/VerificationOptimization/VerificationOptimization.m
+++ src/VerificationOptimization/VerificationOptimization.m
@@ -32,13 +32,8 @@ function [output, inputs] = VerificationOptimization(inputs, params)
 inputs = makeTreatmentOptimizationInputs(inputs, params);
 initializeMexOrMatlabParallelFunctions(inputs.mexModel);
 if strcmp(inputs.controllerType, 'synergy')
-    inputs = setupMuscleSynergies(inputs);
 end
 output = computeVerificationOptimizationMainFunction(inputs, params);
 end
 
-function inputs = setupMuscleSynergies(inputs)
-inputs.splineSynergyActivations = spaps(inputs.initialGuess.time/inputs.initialGuess.time(end), ...
-    inputs.initialGuess.control(:, inputs.numCoordinates + 1:end)', 0.0000001);
-inputs.synergyLabels = inputs.initialGuess.controlLabels(:, inputs.numCoordinates + 1:end);
-end
+
