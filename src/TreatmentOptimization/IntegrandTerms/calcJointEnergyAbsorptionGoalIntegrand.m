d3b9bf552580e77e2e0f4befe94be8786be7397e
stwilliams333 <stwilliamsmail@gmail.com>
2023-11-13 16:44:31 -0600

    Joint energy absorption and generation terms

---
+++ src/TreatmentOptimization/IntegrandTerms/calcJointEnergyAbsorptionGoalIntegrand.m
@@ -0,0 +1,54 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function calculates the joint power error for the specified
+% coordinate and absorption goal. Only negative power (absorbed energy) is
+% included in the output. 
+%
+% (2D matrix, 2D matrix, struct, Array of string) -> (Array of number)
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Spencer Williams, Marleny Vega                               %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function cost = calcJointEnergyGenerationGoalIntegrand(costTerm, ...
+    jointVelocity, time, jointMoment, params, loadName)
+normalizeByFinalTime = valueOrAlternate(costTerm, ...
+    "normalize_by_final_time", true);
+loadName = erase(loadName, '_moment');
+loadName = erase(loadName, '_force');
+indx = find(strcmp(convertCharsToStrings(params.coordinateNames), ...
+    loadName));
+momentLabelsNoSuffix = erase(params.inverseDynamicsMomentLabels, '_moment');
+momentLabelsNoSuffix = erase(momentLabelsNoSuffix, '_force');
+includedJointMomentCols = ismember(momentLabelsNoSuffix, ...
+    convertCharsToStrings(params.coordinateNames));
+if isequal(mexext, 'mexw64')
+    jointMoment = jointMoment(:, includedJointMomentCols);
+end
+jointPower = jointMoment(:, indx) .* jointVelocity(:, indx);
+
+cost = imag(sqrt((jointPower - costTerm.errorCenter) / ...
+    costTerm.maxAllowableError));
+
+if normalizeByFinalTime
+    cost = cost / time(end);
+end
+end
