8b9b99461856d4945b46fd69e29fb61d9883106f
RobSalati <rs200@rice.edu>
2023-11-24 15:20:06 -0600

    saveMuscleTendonOptimizationResults done
    Have a function to save optimization parameters. Currently not integrated directly with MTP.

--- src/MuscleTendonPersonalization/saveMuscleTendonOptimizationResults.m
+++ src/MuscleTendonPersonalization/saveMuscleTendonOptimizationResults.m
@@ -7,190 +7,208 @@
 % Saves data from optimization to 6 .sto files
 
 function saveMuscleTendonOptimizationResults(optimizedParams, ...
-        mtpInputs, precalInputs, resultsDirectory)
-    if nargin < 3; precalInputs = []; end
-        [finalValues, results, resultsSynx, resultsSynxNoResiduals] = ...
-            getValuesToReport(mtpInputs, precalInputs, optimizedParams)
-    if ~isempty(precalInputs)
-        saveMuscleTendonLengthInitializationResults(precalInputs, mtpInputs, ...
-            resultsDirectory)  % Change to just save the data
-    end
+    mtpInputs, precalInputs, resultsDirectory)
+if nargin < 3
+    precalInputs = [];
+end
+[finalValues, results, resultsSynx, resultsSynxNoResiduals] = ...
+    getValuesToReport(mtpInputs, precalInputs, optimizedParams);
+if ~isempty(precalInputs)
+    modeledValues = getMuscleTendonLengthInitializationData(precalInputs, ...
+        mtpInputs);  % Modeled passive force data & params from experimental data.
+end
 
-    printJointMomentMatchingError(resultsSynx.muscleJointMoments, ...
-        mtpInputs.inverseDynamicsMoments);  % Keep this
-    
-    saveExcitationAndActivationData(results, resultsSynx, mtpInputs, ...
-        mtpInputs.synergyExtrapolation, resultsDirectory);  % Change to just save the data
+printJointMomentMatchingError(resultsSynx.muscleJointMoments, ...
+    mtpInputs.inverseDynamicsMoments);  % Keep this
 
-    % makeModelParameterPlots(finalValues, mtpInputs, ...
-    %     mtpInputs.synergyExtrapolation)  % Change to just save the data
+%% Save excitations and activations
+saveActivationAndExcitationData(mtpInputs, results, resultsSynx, resultsDirectory);
 
-    % makeTaskSpecificMomentMatchingPlots(...
-    %     permute(resultsSynxNoResiduals.muscleJointMoments, [3 1 2]), ...
-    %     permute(resultsSynx.muscleJointMoments, [3 1 2]), ...
-    %     permute(mtpInputs.inverseDynamicsMoments, [3 1 2]), ...
-    %     mtpInputs.coordinateNames, mtpInputs.synergyExtrapolation)  % Change to just save the data
+%% Save Noramlized Fiber lengths
+saveAnalysisData(mtpInputs.muscleNames, mtpInputs.prefixes, results.normalizedFiberLength, ...
+    strcat(resultsDirectory, "\normalizedFiberLengths"), "_normalizedFiberLengths.sto")
 
-    % makeTaskSpecificNormalizedFiberLengthsPlots( ...
-    %     permute(resultsSynx.normalizedFiberLength, [3 1 2]), ...
-    %     mtpInputs, mtpInputs.synergyExtrapolation)  % Change to just save the data
+%% Save Joint moments
+saveJointMomentData(mtpInputs, results, resultsSynx, resultsSynxNoResiduals, resultsDirectory);
+
+%% Save hill type model params
+saveMuscleModelParameters(finalValues, fullfile(resultsDirectory, ...
+    "muscleModelParemeters"));
+
+%% Save Passive moments
+savePassiveMomentData(precalInputs, modeledValues, resultsDirectory);
+
+%% Save passive forces
+savePassiveForceData(mtpInputs, modeledValues, results, resultsSynx, ...
+    resultsSynxNoResiduals, resultsDirectory);
+end
+
+function savePassiveMomentData(precalInputs, modeledValues, resultsDirectory)
+% Passive moments need processing first.
+modelPassiveMoments = permute(modeledValues.passiveModelMoments, [3 1 2]);
+sizeTemp = size(modelPassiveMoments,1);
+experimentalPassiveMoments = permute(precalInputs.passiveData.inverseDynamicsMoments, [3 1 2]);
+
+columnsWithAllZeros = all(experimentalPassiveMoments == 0, 1);
+
+experimentalPassiveMoments = experimentalPassiveMoments(repmat(~columnsWithAllZeros, ...
+    size(experimentalPassiveMoments, 1), 1, 1));
+
+modelPassiveMoments = modelPassiveMoments(repmat(~columnsWithAllZeros, ...
+    size(modelPassiveMoments, 1), 1, 1));
+
+experimentalPassiveMoments = ...
+    reshape(experimentalPassiveMoments, sizeTemp, []);
+experimentalPassiveMoments = ...
+    reshape(experimentalPassiveMoments', 1, 12, 101); % Want a better way to do this
+modelPassiveMoments = ...
+    reshape(modelPassiveMoments, sizeTemp, []);
+modelPassiveMoments = ...
+    reshape(modelPassiveMoments', 1, 12, 101); % Want a better way to do this
+saveAnalysisData(precalInputs.passivePrefixes, precalInputs.prefixes, ...
+    experimentalPassiveMoments, fullfile(resultsDirectory, ...
+    "passiveJointMomentsExperimental"), "_passiveJointMomentsExperimental.sto")
+saveAnalysisData(precalInputs.passivePrefixes, precalInputs.prefixes, ...
+    modelPassiveMoments, fullfile(resultsDirectory, ...
+    "passiveJointMomentsModeled"), "_passiveJointMomentsModeled.sto")
+end
+
+function saveJointMomentData(mtpInputs, results, resultsSynx, ...
+    resultsSynxNoResiduals, resultsDirectory)
+saveAnalysisData(mtpInputs.coordinateNames, mtpInputs.prefixes, ...
+    results.muscleJointMoments, strcat(resultsDirectory, ...
+    "\modelJointMomentsNoSynx"), "_modelJointMomentsNoSynx.sto")
+saveAnalysisData(mtpInputs.coordinateNames, mtpInputs.prefixes, ...
+    resultsSynx.muscleJointMoments, strcat(resultsDirectory, ...
+    "\modelJointMomentsSynx"), "_modelJointMomentsSynx.sto")
+saveAnalysisData(mtpInputs.coordinateNames, mtpInputs.prefixes, ...
+    resultsSynxNoResiduals.muscleJointMoments, strcat(resultsDirectory, ...
+    "\modelJointMomentsSynxNoResiduals"), "_modelJointMomentsSynxNoResiduals.sto")
+saveAnalysisData(mtpInputs.coordinateNames, mtpInputs.prefixes, ...
+    mtpInputs.inverseDynamicsMoments, strcat(resultsDirectory, ...
+    "\inverseDynamicsJointMoments"), "_inverseDynamicsJointMoments.sto")
+end
+
+function saveActivationAndExcitationData(mtpInputs, results, ...
+    resultsSynx, resultsDirectory)
+saveAnalysisData(mtpInputs.muscleNames, mtpInputs.prefixes, ...
+    results.muscleExcitations, strcat(resultsDirectory, ...
+    "\muscleExcitations"), "_muscleExcitations.sto")
+saveAnalysisData(mtpInputs.muscleNames, mtpInputs.prefixes, ...
+    resultsSynx.muscleExcitations, strcat(resultsDirectory, ...
+    "\muscleExcitationsSynx"), "_muscleExcitationsSynx.sto")
+saveAnalysisData(mtpInputs.muscleNames, mtpInputs.prefixes, ...
+    results.muscleActivations, strcat(resultsDirectory, ...
+    "\muscleActivations"), "_muscleActivations.sto")
+saveAnalysisData(mtpInputs.muscleNames, mtpInputs.prefixes, ...
+    resultsSynx.muscleActivations, strcat(resultsDirectory, ...
+    "\muscleActivationsSynx"), "_muscleActivationsSynx.sto")
+end
+
+function saveMuscleModelParameters(finalValues, resultsDirectory)
+if ~exist(resultsDirectory, "dir")
+    mkdir(resultsDirectory);
+end
+columnLabels = ["Activation Time Constant", "Activation Nonlinearity", ...
+    "Electromechanical Time Delay", "EMG Scaling Factor", ...
+    "Optimal Fiber Length Scaling Factor", "Tendon Slack Length Scaling Factor"];
+
+dataPoints = [finalValues.activationTimeConstants;
+    finalValues.activationNonlinearityConstants;
+    finalValues.electromechanicalDelays;
+    finalValues.emgScaleFactors;
+    finalValues.optimalFiberLengthScaleFactors;
+    finalValues.tendonSlackLengthScaleFactors]';
+
+writeToSto(columnLabels, 1:1:size(dataPoints,1), dataPoints, ...
+    fullfile(resultsDirectory, "muscleModelParameters.sto"));
+end
+
+function savePassiveForceData(mtpInputs, modeledValues, results, resultsSynx, ...
+    resultsSynxNoResiduals, resultsDirectory)
+saveAnalysisData(mtpInputs.muscleNames, mtpInputs.prefixes, ...
+    modeledValues.passiveForce, strcat(resultsDirectory, ...
+    "\passiveForcesExperimental"), "_passiveForcesExperimental.sto");
+saveAnalysisData(mtpInputs.muscleNames, mtpInputs.prefixes, ...
+    results.passiveForce, strcat(resultsDirectory, ...
+    "\passiveForcesModel"), "_passiveForcesModel.sto");
+saveAnalysisData(mtpInputs.muscleNames, mtpInputs.prefixes, ...
+    resultsSynx.passiveForce, strcat(resultsDirectory, ...
+    "\passiveForcesModelSynx"), "_passiveForcesModelSynx.sto");
+saveAnalysisData(mtpInputs.muscleNames, mtpInputs.prefixes, ...
+    resultsSynxNoResiduals.passiveForce, strcat(resultsDirectory, ...
+    "\passiveForcesModelSynxNoResiduals"), "_passiveForcesModelSynxNoResiduals.sto");
 end
 
 function [finalValues, results, resultsSynx, resultsSynxNoResiduals] = ...
-        getValuesToReport(mtpInputs, precalInputs, optimizedParams)  % Need to downscale excitation arrays
+    getValuesToReport(mtpInputs, precalInputs, optimizedParams)  % Need to downscale excitation arrays
 
-    finalValues = makeMtpValuesAsStruct([], optimizedParams, zeros(1, 7));
-    save('finalvalues.mat', 'finalValues')
-    resultsSynx = calcMtpSynXModeledValues(finalValues, mtpInputs, struct());
-    finalValues.synergyWeights(mtpInputs.numberOfExtrapolationWeights + 1 : end) = 0;
-    resultsSynxNoResiduals = calcMtpSynXModeledValues(finalValues, mtpInputs, struct());
-    results = calcMtpModeledValues(finalValues, mtpInputs, struct());
-    results.time = mtpInputs.emgTime(:, mtpInputs.numPaddingFrames + 1 : ...
-        end - mtpInputs.numPaddingFrames);
-    results.muscleExcitations = results.muscleExcitations(:, :, ...
-        mtpInputs.numPaddingFrames + 1 : end - mtpInputs.numPaddingFrames);
-    resultsSynx.muscleExcitations = resultsSynx.muscleExcitations(:, :, ...
-        mtpInputs.numPaddingFrames + 1 : end - mtpInputs.numPaddingFrames);
+finalValues = makeMtpValuesAsStruct([], optimizedParams, zeros(1, 7));
+save('finalvalues.mat', 'finalValues')
+resultsSynx = calcMtpSynXModeledValues(finalValues, mtpInputs, struct());
+finalValues.synergyWeights(mtpInputs.numberOfExtrapolationWeights + 1 : end) = 0;
+resultsSynxNoResiduals = calcMtpSynXModeledValues(finalValues, mtpInputs, struct());
+results = calcMtpModeledValues(finalValues, mtpInputs, struct());
+results.time = mtpInputs.emgTime(:, mtpInputs.numPaddingFrames + 1 : ...
+    end - mtpInputs.numPaddingFrames);
+results.muscleExcitations = results.muscleExcitations(:, :, ...
+    mtpInputs.numPaddingFrames + 1 : end - mtpInputs.numPaddingFrames);
+resultsSynx.muscleExcitations = resultsSynx.muscleExcitations(:, :, ...
+    mtpInputs.numPaddingFrames + 1 : end - mtpInputs.numPaddingFrames);
 end
 
-function saveMuscleTendonLengthInitializationResults(...
-    precalInputs, mtpInputs, resultsDirectory)
+function modeledValues = getMuscleTendonLengthInitializationData(...
+    precalInputs, mtpInputs)
 
-    tempValues.optimalFiberLengthScaleFactors = ...
-        mtpInputs.optimalFiberLength ./ precalInputs.optimalFiberLength;
-    tempValues.tendonSlackLengthScaleFactors = ...
-        mtpInputs.tendonSlackLength ./ precalInputs.tendonSlackLength;
-    precalInputs.maxIsometricForce = mtpInputs.maxIsometricForce;
-    precalInputs.optimizeIsometricMaxForce = 0;
-    modeledValues = calcMuscleTendonLengthInitializationModeledValues(tempValues, precalInputs);
-    savePassiveForceData(permute(modeledValues.passiveForce, [3 1 2]), ...
-        precalInputs.muscleNames, resultsDirectory);
-%     if precalInputs.passiveMomentDataExists
-%         plotPassiveMomentData(permute(modeledValues.passiveModelMoments, [3 1 2]), ...
-%             permute(precalInputs.passiveData.inverseDynamicsMoments, [3 1 2]), ...
-%             precalInputs.passivePrefixes)
-%     end
+tempValues.optimalFiberLengthScaleFactors = ...
+    mtpInputs.optimalFiberLength ./ precalInputs.optimalFiberLength;
+
+tempValues.tendonSlackLengthScaleFactors = ...
+    mtpInputs.tendonSlackLength ./ precalInputs.tendonSlackLength;
+
+precalInputs.maxIsometricForce = mtpInputs.maxIsometricForce;
+
+precalInputs.optimizeIsometricMaxForce = 0;
+
+modeledValues = calcMuscleTendonLengthInitializationModeledValues(tempValues, precalInputs);
 end
-
-function savePassiveForceData(modeledValue, muscleNames, resultsDirectory)
-
-    meanModeledValue = squeeze(mean(modeledValue, 2));
-    stdModeledValue = squeeze(std(modeledValue, [], 2));
-    
-    data = {meanModeledValue, stdModeledValue};
-    dataNames = ["meanModeledValue", "stdModeledValue"];
-
-    [stoDataArray, stoColumnLabels] = groupDataByMuscle(data, dataNames, muscleNames);
-
-    writeToSto(stoColumnLabels, 1:1:length(stoDataArray), stoDataArray, ...
-        strcat(resultsDirectory, "/passiveForceData/", "_passiveForceData.sto"))  % Maybe needs counter for gait cycles?
-end 
 
 function printJointMomentMatchingError(muscleJointMoments, inverseDynamicsMoments)  % Good
 
-    for i = 1 : size(muscleJointMoments, 2)
-        jointMomentsRmse(i) = sqrt(sum((muscleJointMoments(:, i, :) - ...
-            inverseDynamicsMoments(:, i, :)) .^ 2, 'all') / ...
-            (numel(inverseDynamicsMoments(:, 1, :)) - 1));
-        jointMomentsMae(i) = sum(abs(muscleJointMoments(:, i, :) - ...
-            inverseDynamicsMoments(:, i, :)) / ...
-            numel(inverseDynamicsMoments(:, 1, :)), 'all');
-    end
-    fprintf(['The root mean sqrt (RMS) errors between model-predicted ' ...
-        'and inverse dynamic moments are: \n' ]);
-    fprintf(['\n ' num2str(jointMomentsRmse) ' \n']);
-    fprintf(['The mean absolute errors (MAEs) between model-predicted ' ...
-        'and inverse dynamic moments are: \n' ]);
-    fprintf(['\n ' num2str(jointMomentsMae) ' \n']);
+for i = 1 : size(muscleJointMoments, 2)
+    jointMomentsRmse(i) = sqrt(sum((muscleJointMoments(:, i, :) - ...
+        inverseDynamicsMoments(:, i, :)) .^ 2, 'all') / ...
+        (numel(inverseDynamicsMoments(:, 1, :)) - 1));
+    jointMomentsMae(i) = sum(abs(muscleJointMoments(:, i, :) - ...
+        inverseDynamicsMoments(:, i, :)) / ...
+        numel(inverseDynamicsMoments(:, 1, :)), 'all');
+end
+fprintf(['The root mean sqrt (RMS) errors between model-predicted ' ...
+    'and inverse dynamic moments are: \n' ]);
+fprintf(['\n ' num2str(jointMomentsRmse) ' \n']);
+fprintf(['The mean absolute errors (MAEs) between model-predicted ' ...
+    'and inverse dynamic moments are: \n' ]);
+fprintf(['\n ' num2str(jointMomentsMae) ' \n']);
 end
 
-function saveExcitationAndActivationData(results, resultsSynx, ...
-        experimentalData, synergyParameters, resultsDirectory)
-    muscleLabels = getSynxMuscleNames(experimentalData.muscleNames, ...
-        synergyParameters.missingEmgChannelGroups);
-    for i = 1 : numel(synergyParameters.taskNames)
-        
-        [stoDataArray, stoColumnLabels] = formatMuscleExcitationsAndActivations(...
-            results.muscleExcitations(synergyParameters.trialIndex{i}, :, :), ...
-            resultsSynx.muscleExcitations(synergyParameters.trialIndex{i}, :, :), ...
-            results.muscleActivations(synergyParameters.trialIndex{i}, :, :), ...
-            resultsSynx.muscleActivations(synergyParameters.trialIndex{i}, :, :), ...
-            muscleLabels);
-        writeToSto(stoColumnLabels, 1:1:length(stoDataArray), stoDataArray, ...
-            strcat(resultsDirectory, "/muscleExcitationsAndActivations/", ...
-            synergyParameters.taskNames{i}, "_muscleExcitationsActivations.sto"))
-    end
+function saveAnalysisData(columnLabels, taskNames, data, directory, fileName)
+if ~exist(directory, "dir")
+    mkdir(directory);
 end
-
-function [stoDataArray, stoColumnLabels] = formatMuscleExcitationsAndActivations( ...
-        muscleExcitations, muscleExcitationsSynx, muscleActivations, ...
-        muscleActivationsSynx, muscleLabels)
-    
-    meanMuscleExcitation = permute(mean(muscleExcitations, 1), [3 2 1]);
-    stdMuscleExcitation = permute(std(muscleExcitations, [], 1), [3 2 1]);
-    meanMuscleExcitationSynx = permute(mean(muscleExcitationsSynx, 1), [3 2 1]);
-    stdMuscleExcitationSynx = permute(std(muscleExcitationsSynx, [], 1), [3 2 1]);
-    meanMuscleActivation = permute(mean(muscleActivations, 1), [3 2 1]);
-    stdMuscleActivation = permute(std(muscleActivations, [], 1), [3 2 1]);
-    meanMuscleActivationSynx = permute(mean(muscleActivationsSynx, 1), [3 2 1]);
-    stdMuscleActivationSynx = permute(std(muscleActivationsSynx, [], 1), [3 2 1]);
-    excitationsAndActivations = {
-        meanMuscleExcitation, stdMuscleExcitation, ...
-        meanMuscleExcitationSynx, stdMuscleExcitationSynx, ...
-        meanMuscleActivation, stdMuscleActivation, ...
-        meanMuscleActivationSynx, stdMuscleActivationSynx};
-    dataNames = ["meanMuscleExcitation", "stdMuscleExcitation", ...
-        "meanMuscleExcitationSynx", "stdMuscleExcitationSynx", ...
-        "meanMuscleActivation", "stdMuscleActivation", ...
-        "meanMuscleActivationSynx", "stdMuscleActivationSynx"];
-    [stoDataArray, stoColumnLabels] = groupDataByMuscle(excitationsAndActivations, ...
-        dataNames, muscleLabels);
-%     for i = 0 : numel(muscleLabels) - 1
-%         stoDataArray(:, i*8+1) = meanMuscleExcitation(1:101, i + 1);
-%         stoDataArray(:, i*8+2) = stdMuscleExcitation(1:101, i+1);
-%         stoDataArray(:, i*8+3) = meanMuscleExcitationSynx(1:101, i+1);
-%         stoDataArray(:, i*8+4) = stdMuscleExcitationSynx(1:101, i+1);
-%         stoDataArray(:, i*8+5) = meanMuscleActivation(1:101, i+1);
-%         stoDataArray(:, i*8+6) = stdMuscleActivation(1:101, i+1);
-%         stoDataArray(:, i*8+7) = meanMuscleActivationSynx(1:101, i+1);
-%         stoDataArray(:, i*8+8) = stdMuscleActivationSynx(1:101, i+1);
-%     end
-%     
-%     stoColumnLabels = strings(8*numel(muscleLabels),1);
-%     for i=0:numel(muscleLabels)-1
-%         stoColumnLabels(8*i+1) = strcat(muscleLabels{i+1}, "_", "meanMuscleExcitation");
-%         stoColumnLabels(8*i+2) = strcat(muscleLabels{i+1}, "_", "stdMuscleExcitation");
-%         stoColumnLabels(8*i+3) = strcat(muscleLabels{i+1}, "_", "meanMuscleExcitationSynx");
-%         stoColumnLabels(8*i+4) = strcat(muscleLabels{i+1}, "_", "stdMuscleExcitationSynx");
-%         stoColumnLabels(8*i+5) = strcat(muscleLabels{i+1}, "_", "meanMuscleActivation");
-%         stoColumnLabels(8*i+6) = strcat(muscleLabels{i+1}, "_", "stdMuscleActivation");
-%         stoColumnLabels(8*i+7) = strcat(muscleLabels{i+1}, "_", "meanMuscleActivationSynx");
-%         stoColumnLabels(8*i+8) = strcat(muscleLabels{i+1}, "_", "stdMuscleActivationSynx");
+for i = 1 : size(data,1)
+    writeToSto(columnLabels, 1:1:length(data(i,:,:)), ...
+        permute(data(i,:,:), [3 2 1]), strcat(directory, "\", taskNames(i), fileName))
 end
-
-function [dataArray, columnNames] = groupDataByMuscle(...
-    data, dataNames, muscleNames)
-    
-    lengthMuscleNames = numel(muscleNames);
-    lengthDataNames = numel(dataNames);
-    dataArray = zeros(length(data{1}), lengthMuscleNames*lengthDataNames);
-    columnNames = strings(lengthDataNames*lengthMuscleNames, 1);
-    for i = 0 : lengthMuscleNames - 1
-        for j = 1 : lengthDataNames
-            dataArray(:, lengthDataNames * i + j) = data{j}(:, i + 1);
-            columnNames(lengthDataNames * i + j) = strcat(muscleNames(i + 1), "_", dataNames(j));
-        end
-    end
 end
 
 function muscleLabels = getSynxMuscleNames(muscleNames, ...
     missingEmgChannelGroups)
 
-    for i = 1 : numel(muscleNames)
-        if ismember(i, [missingEmgChannelGroups{:}])
-            muscleLabels{i} = [muscleNames{i} '(*)'];
-        else
-            muscleLabels{i} = muscleNames{i};
-        end
+for i = 1 : numel(muscleNames)
+    if ismember(i, [missingEmgChannelGroups{:}])
+        muscleLabels{i} = [muscleNames{i} '(*)'];
+    else
+        muscleLabels{i} = muscleNames{i};
     end
+end
 end
