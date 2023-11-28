1eb93375d8cc2a12381e43a9ab0a751f8f59918f
stwilliams333 <stwilliamsmail@gmail.com>
2023-11-27 22:57:50 -0600

    Fix torque control tracking indices

--- src/TreatmentOptimization/IntegrandTerms/calcTrackingCoordinateIntegrand.m
+++ src/TreatmentOptimization/IntegrandTerms/calcTrackingCoordinateIntegrand.m
@@ -45,8 +45,13 @@ else
     experimentalJointAngles = fnval(auxdata.splineJointAngles, time);
 end
 
-cost = calcTrackingCostArrayTerm(experimentalJointAngles, ...
-    statePositions, indx);
+assert(all([any(auxdata.statesCoordinateNames == coordinateName), ...
+    any(string(auxdata.coordinateNames) == coordinateName)]));
+stateIndex = auxdata.statesCoordinateNames == coordinateName;
+experimentalIndex = string(auxdata.coordinateNames) == coordinateName;
+
+cost = calcTrackingCostArrayTerm(experimentalJointAngles(:, stateIndex),...
+    statePositions(:, experimentalIndex), 1);
 
 if normalizeByFinalTime
     cost = cost / time(end);
