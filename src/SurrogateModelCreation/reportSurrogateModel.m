4ded19e472e1af6ce397fa8e6fca10751b57cff2
Claire V. Hammond <61138239+cvhammond@users.noreply.github.com>
2024-04-09 21:48:40 -0500

    fix surrogate plotting, allow surrogate or experimental plot
    Co-Authored-By: Spencer Williams <54556034+SpencerTWilliams@users.noreply.github.com>

--- src/SurrogateModelCreation/reportSurrogateModel.m
+++ src/SurrogateModelCreation/reportSurrogateModel.m
@@ -1,7 +1,7 @@
 % This function is part of the NMSM Pipeline, see file for full license.
 %
 % () -> ()
-% 
+%
 
 % ----------------------------------------------------------------------- %
 % The NMSM Pipeline is a toolkit for model personalization and treatment  %
@@ -11,7 +11,7 @@
 % National Institutes of Health (R01 EB030520).                           %
 %                                                                         %
 % Copyright (c) 2021 Rice University and the Authors                      %
-% Author(s): Marleny Vega                                                 %
+% Author(s): Marleny Vega, Claire V. Hammond, Spencer Williams            %
 %                                                                         %
 % Licensed under the Apache License, Version 2.0 (the "License");         %
 % you may not use this file except in compliance with the License.        %
@@ -26,76 +26,159 @@
 % ----------------------------------------------------------------------- %
 
 function reportSurrogateModel(inputs)
+if valueOrAlternate(inputs, 'plotSurrogateResults', false)
+    plotSurrogateResults(inputs);
+end
+if valueOrAlternate(inputs, 'plotExperimentalResults', false)
+    plotExperimentalResults(inputs);
+end
+end
 
+function plotSurrogateResults(inputs)
+inputs = getMuscleSpecificSurrogateModelDataSurrogate(inputs);
 [newMuscleTendonLengths, newMomentArms, newMuscleTendonVelocities] = ...
     calcSurrogateModel(inputs, inputs.muscleSpecificJointAngles, ...
     inputs.muscleSpecificJointVelocities);
 plotSurrogateModelFitting(inputs, newMuscleTendonLengths, ...
-    newMomentArms, newMuscleTendonVelocities);
+    newMomentArms, newMuscleTendonVelocities, false);
 end
+
+function plotExperimentalResults(inputs)
+inputs = getMuscleSpecificSurrogateModelDataExperimental(inputs);
+[newMuscleTendonLengths, newMomentArms, newMuscleTendonVelocities] = ...
+    calcSurrogateModel(inputs, inputs.muscleSpecificJointAngles, ...
+    inputs.muscleSpecificJointVelocities);
+plotSurrogateModelFitting(inputs, newMuscleTendonLengths, ...
+    newMomentArms, newMuscleTendonVelocities, true);
+end
+
 function plotSurrogateModelFitting(inputs, newMuscleTendonLengths, ...
-    newMomentArms, newMuscleTendonVelocities)
+    newMomentArms, newMuscleTendonVelocities, plottingExperimental)
 
 nplots = ceil(sqrt(inputs.numMuscles));
 % Plot muscle tendon lengths
 figure('name', 'Muscle Tendon Lengths')
 for i = 1 : inputs.numMuscles
-subplot(nplots, nplots, i)
-plotData(inputs.muscleTendonLengths(:, i), newMuscleTendonLengths(:, i), ...
-    inputs.muscleNames{i})
-axis([1 size(inputs.muscleTendonLengths, 1) min(inputs.muscleTendonLengths,  [], ...
-    'all') max(inputs.muscleTendonLengths,  [], 'all')])
-if i > inputs.numMuscles - nplots; xlabel('Data Points'); 
-else xticklabels(''); end
-if ismember(i, 1 : nplots : inputs.numMuscles)
-    ylabel({'Muscle','Tendon Lengths'});
-else yticklabels(''); end
-if i == inputs.numMuscles
-    legend('Original', 'Predicted'); 
-end
+    subplot(nplots, nplots, i)
+    plotData(inputs.muscleTendonLengths(:, i), newMuscleTendonLengths(:, i), ...
+        inputs.muscleNames{i})
+    axis([1 size(inputs.muscleTendonLengths, 1) min(inputs.muscleTendonLengths,  [], ...
+        'all') max(inputs.muscleTendonLengths,  [], 'all')])
+    if i > inputs.numMuscles - nplots; xlabel('Data Points');
+    else xticklabels(''); end
+    if ismember(i, 1 : nplots : inputs.numMuscles)
+        ylabel({'Muscle','Tendon Lengths'});
+    else yticklabels(''); end
+    if i == inputs.numMuscles
+        legend('Original', 'Predicted');
+    end
 end
 
-nplots = ceil(sqrt(inputs.numMuscles));
-% Plot muscle tendon velocities
-figure('name', 'Muscle Tendon Velocities')
-for i = 1 : inputs.numMuscles
-subplot(nplots, nplots, i)
-plotData(inputs.muscleTendonVelocities(:, i), newMuscleTendonVelocities(:, i), ...
-    inputs.muscleNames{i})
-axis([1 size(inputs.muscleTendonVelocities, 1) min(inputs.muscleTendonVelocities,  [], ...
-    'all') max(inputs.muscleTendonVelocities,  [], 'all')])
-if i > inputs.numMuscles - nplots; xlabel('Data Points'); 
-else xticklabels(''); end
-if ismember(i, 1 : nplots : inputs.numMuscles)
-    ylabel({'Muscle','Tendon Velocities'});
-else yticklabels(''); end
-if i == inputs.numMuscles
-    legend('Original', 'Predicted'); 
-end
+if plottingExperimental
+    nplots = ceil(sqrt(inputs.numMuscles));
+    % Plot muscle tendon velocities
+    figure('name', 'Muscle Tendon Velocities')
+    for i = 1 : inputs.numMuscles
+        subplot(nplots, nplots, i)
+        plotData(inputs.muscleTendonVelocities(:, i), newMuscleTendonVelocities(:, i), ...
+            inputs.muscleNames{i})
+        axis([1 size(inputs.muscleTendonVelocities, 1) min(inputs.muscleTendonVelocities,  [], ...
+            'all') max(inputs.muscleTendonVelocities,  [], 'all')])
+        if i > inputs.numMuscles - nplots; xlabel('Data Points');
+        else xticklabels(''); end
+        if ismember(i, 1 : nplots : inputs.numMuscles)
+            ylabel({'Muscle','Tendon Velocities'});
+        else yticklabels(''); end
+        if i == inputs.numMuscles
+            legend('Original', 'Predicted');
+        end
+    end
 end
 
 % Plot moment arms
 for j = 1 : length(inputs.surrogateModelCoordinateNames)
-figure('name', 'Moment Arms')
-for i = 1 : inputs.numMuscles
-subplot(nplots, nplots, i)
-for k = 1:length(inputs.coordinateNames)
-if strcmp(inputs.surrogateModelCoordinateNames(j), inputs.coordinateNames(k))
-plotData(inputs.momentArms(:,j,i), newMomentArms(:,k,i), inputs.muscleNames(i));
-end
-end
-if i > inputs.numMuscles - nplots; xlabel('Data Points'); 
-else xticklabels(''); end
-if ismember(i, 1 : nplots : inputs.numMuscles)
-    ylabel({'Moment','Arms'}); end
-if i == inputs.numMuscles
-    legend('Original', 'Predicted'); 
-end
-end
+    figure('name', 'Moment Arms')
+    for i = 1 : inputs.numMuscles
+        subplot(nplots, nplots, i)
+        for k = 1:length(inputs.coordinateNames)
+            if strcmp(inputs.surrogateModelCoordinateNames(j), inputs.coordinateNames(k))
+                plotData(inputs.surrogateModelMomentArms(:,j,i), newMomentArms(:,k,i), inputs.muscleNames(i));
+            end
+        end
+        if i > inputs.numMuscles - nplots; xlabel('Data Points');
+        else xticklabels(''); end
+        if ismember(i, 1 : nplots : inputs.numMuscles)
+            ylabel({'Moment','Arms'}); end
+        if i == inputs.numMuscles
+            legend('Original', 'Predicted');
+        end
+    end
 end
 end
 function plotData(original, predicted, muscleLabel)
 plot(original, 'r', 'LineWidth', 2)
 hold on; plot(predicted, 'k')
 title(strrep(muscleLabel, '_', ' '))
+end
+
+function inputs = getMuscleSpecificSurrogateModelDataExperimental(inputs)
+inputs.momentArms = parseSelectMomentArms("../preprocessing/preprocessed_right/MAData/gait_1", ...
+    inputs.surrogateModelCoordinateNames, inputs.muscleNames);
+inputs.momentArms = reshape(permute(inputs.momentArms, [1 4 2 3]), [], ...
+    length(inputs.surrogateModelCoordinateNames), length(inputs.muscleNames));
+inputs.splineJointAngles = makeGcvSplineSet(inputs.experimentalTime, ...
+    inputs.experimentalJointAngles', inputs.coordinateNames);
+inputs.experimentalJointVelocities = evaluateGcvSplines( ...
+    inputs.splineJointAngles, inputs.coordinateNames, ...
+    inputs.experimentalTime, 1);
+inputs.splinedJointAngles = evaluateGcvSplines(inputs.splineJointAngles, ...
+    inputs.coordinateNames, inputs.experimentalTime, 0);
+inputs.splinedJointSpeeds = evaluateGcvSplines(inputs.splineJointAngles, ...
+    inputs.coordinateNames, inputs.experimentalTime, 1);
+inputs.muscleSpecificJointAngles = {};
+inputs.muscleSpecificJointVelocities = {};
+inputs.muscleSpecificMomentArms = {};
+for i = 1:inputs.numMuscles
+    counter = 1;
+    for j = 1:length(inputs.surrogateIkCoordinateNames)
+        for k = 1:length(inputs.surrogateModelCoordinateNames)
+            if strcmp(inputs.surrogateIkCoordinateNames(j), inputs.surrogateModelCoordinateNames(k))
+                if max(abs(inputs.surrogateModelMomentArms(:,k,i))) > inputs.epsilon
+                    % inputs.surrogateModelLabels{i}(counter) = ...
+                    %     inputs.coordinateNames(j);
+                    inputs.muscleSpecificJointAngles{i}(:,counter) = ...
+                        inputs.splinedJointAngles(:,j);
+                    inputs.muscleSpecificJointVelocities{i}(:,counter) = ...
+                        inputs.splinedJointSpeeds(:,j);
+                    inputs.muscleSpecificMomentArms{i}(:,counter) = ...
+                        inputs.momentArms(:,k,i);
+                    counter = counter + 1;
+                end
+            end
+        end
+    end
+end
+end
+
+function inputs = getMuscleSpecificSurrogateModelDataSurrogate(inputs)
+inputs.splineJointAngles = makeGcvSplineSet(inputs.surrogateTime, ...
+    inputs.surrogateModelJointAngles', inputs.coordinateNames);
+inputs.splinedJointSpeeds = evaluateGcvSplines( ...
+    inputs.splineJointAngles, inputs.coordinateNames, ...
+    inputs.surrogateTime, 1);
+inputs.muscleSpecificJointVelocities = {};
+for i = 1:inputs.numMuscles
+    counter = 1;
+    for j = 1:length(inputs.surrogateIkCoordinateNames)
+        for k = 1:length(inputs.surrogateModelCoordinateNames)
+            if strcmp(inputs.surrogateIkCoordinateNames(j), inputs.surrogateModelCoordinateNames(k))
+                if max(abs(inputs.surrogateModelMomentArms(:,k,i))) > inputs.epsilon
+                    inputs.muscleSpecificJointVelocities{i}(:,counter) = ...
+                        inputs.splinedJointSpeeds(:,j);
+                    counter = counter + 1;
+                end
+            end
+        end
+    end
+end
 end
