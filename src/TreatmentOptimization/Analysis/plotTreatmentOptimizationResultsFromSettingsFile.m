2c1340112511a13ac6dfbdeef597238b258a500a
RobSalati <rs200@rice.edu>
2024-07-24 14:59:05 -0500

    Plot treatment optimization from settings file

---
+++ src/TreatmentOptimization/Analysis/plotTreatmentOptimizationResultsFromSettingsFile.m
@@ -0,0 +1,51 @@
+function plotTreatmentOptimizationResultsFromSettingsFile(settingsFileName)
+settingsTree = xml2struct(settingsFileName);
+toolName = findToolName(tree);
+resultsDirectory = getTextFromField(getFieldByName(settingsTree, ...
+    'results_directory'));
+trackedQuantitiesDirectory = getTextFromField(getFieldByName(settingsTree, ...
+    'tracked_quantities_directory'));
+[isTorque, isSynergy] = parseControllers(settingsTree);
+modelFileName = parseElementTextByName(settingsTree, 'input_model_file');
+trialPrefix = getTextFromField(getFieldByName(settingsTree, ...
+    'trial_name'));
+
+
+plotTreatmentOptimizationJointAngles( ...
+    modelFileName, ...
+    fullfile(trackedQuantitiesDirectory, "IKData", strcat(trialPrefix, ".sto")), ...
+    fullfile(resultsDirectory, "IKData", strcat(trialPrefix, ".sto")));
+
+plotTreatmentOptimizationJointLoads( ...
+    fullfile(trackedQuantitiesDirectory, "IDData", strcat(trialPrefix, ".sto")), ...
+    fullfile(resultsDirectory, "IDData", strcat(trialPrefix, ".sto")));
+
+plotTreatmentOptimizationGroundReactions( ...
+    fullfile(trackedQuantitiesDirectory, "GRFData", strcat(trialPrefix, ".sto")), ...
+    fullfile(resultsDirectory, "GRFData", strcat(trialPrefix, ".sto")));
+
+if isTorque
+    plotTreatmentOptimizationControls( ...
+    fullfile(resultsDirectory, strcat(trialPrefix, "_torqueControls.sto")))
+end
+if isSynergy
+    plotTreatmentOptimizationControls( ...
+    fullfile(resultsDirectory, strcat(trialPrefix, "_synergyCommands.sto")))
+end
+end
+
+function [isTorque, isSynergy] = parseControllers(settingsTree)
+synergy = getFieldByName(settingsTree, 'RCNLSynergyController');
+if isstruct(synergy)
+    isSynergy = true;
+else
+    isSynergy = false;
+end
+torque = getFieldByName(settingsTree, 'RCNLTorqueController');
+if isstruct(torque)
+    isTorque = true;
+else
+    isTorque = false;
+end
+end
+
