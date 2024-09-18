03154d5f6a6a9a79c08442178d51a5ba3e37f004
RobSalati <rs200@rice.edu>
2024-09-17 20:04:55 -0500

    plot joint velocity

---
+++ src/TreatmentOptimization/Analysis/plotTreatmentOptimizationJointVelocities.m
@@ -0,0 +1,117 @@
+function plotTreatmentOptimizationJointVelocities(modelFileName, ...
+    trackedDataFile, modelDataFiles, figureWidth, figureHeight)
+import org.opensim.modeling.Storage
+model = Model(modelFileName);
+trackedDataStorage = Storage(trackedDataFile);
+coordinateLabels = getStorageColumnNames(trackedDataStorage);
+trackedData = storageToDoubleMatrix(trackedDataStorage)';
+trackedDataTime = findTimeColumn(trackedDataStorage);
+if trackedDataTime(1) ~= 0
+    trackedDataTime = trackedDataTime - trackedDataTime(1);
+end
+trackedDataTime = trackedDataTime / trackedDataTime(end);
+for i = 1 : size(trackedData, 2)
+    if model.getCoordinateSet().get(coordinateLabels(i)).getMotionType() ...
+        .toString().toCharArray()' == "Rotational"
+        trackedData(:, i) = trackedData(:, i) * 1;
+    end
+end
+for j=1:numel(modelDataFiles)
+    modeledStatesStorage = Storage(modelDataFiles(j));
+    modeledStates = storageToDoubleMatrix(modeledStatesStorage)';
+    modeledStatesTime = findTimeColumn(modeledStatesStorage);
+    modeledStatesLabels = getStorageColumnNames(modeledStatesStorage);
+    if modeledStatesTime ~= 0
+        modeledStatesTime = modeledStatesTime - modeledStatesTime(1);
+    end
+    modeledStatesTime = modeledStatesTime / modeledStatesTime(end);
+    
+    modeledVelocities{j} = modeledStates(:, size(modeledStates, 2)/2+1:end);
+    modeledVelocitiesLabels = modeledStatesLabels(size(modeledStates, 2)/2+1:end);
+    modeledVelocitiesTime{j} = modeledStatesTime;
+
+    for i = 1 : size(modeledStates(:, 1:size(modeledStates, 2)/2), 2)
+        model.getCoordinateSet().get(modeledStatesLabels(i))
+        if model.getCoordinateSet().get(modeledStatesLabels(i)).getMotionType() ...
+            .toString().toCharArray()' == "Rotational"
+            modeledVelocities{j}(:, i) = modeledVelocities{j}(:, i) * 1;
+
+        end
+    end
+end
+experimentalSpline = makeGcvSplineSet(trackedDataTime, ...
+    trackedData, coordinateLabels);
+trackedVelocities = evaluateGcvSplines(experimentalSpline, coordinateLabels, ...
+    trackedDataTime, 1);
+resampledExperimentalVelocities = {};
+for j = 1 : numel(modelDataFiles)
+    resampledExperimentalVelocities{j}= evaluateGcvSplines(experimentalSpline, ...
+        coordinateLabels, modeledVelocitiesTime{j}, 1);
+end
+if nargin < 4
+    figureWidth = ceil(sqrt(numel(modeledVelocitiesLabels)));
+    figureHeight = ceil(numel(modeledVelocitiesLabels)/figureWidth);
+elseif nargin < 5
+    figureHeight = ceil(sqrt(numel(modeledVelocitiesLabels)));
+end
+figureSize = figureWidth * figureHeight;
+figure(Name = "Treatment Optimization Joint Velocities", ...
+    Units='normalized', ...
+    Position=[0.05 0.05 0.9 0.85])
+subplotNumber = 1;
+figureNumber = 1;
+t = tiledlayout(figureHeight, figureWidth, ...
+    TileSpacing='compact', Padding='compact');
+xlabel(t, "Percent Movement [0-100%]")
+ylabel(t, "Joint Velocities [deg/s]")
+
+for i=1:numel(modeledVelocitiesLabels)
+    if i > figureSize * figureNumber
+        figureNumber = figureNumber + 1;
+        figure(Name="Treatment Optimization Joint Angles", ...
+            Units='normalized', ...
+            Position=[0.05 0.05 0.9 0.85])
+        t = tiledlayout(figureHeight, figureWidth, ...
+            TileSpacing='Compact', Padding='Compact');
+        xlabel(t, "Percent Movement [0-100%]")
+        ylabel(t, "Joint Angle [deg]")
+        subplotNumber = 1;
+    end
+    modeledStatesLabels(i);
+    coordinateIndex = find(modeledStatesLabels(i) == coordinateLabels);
+    if ~isempty(coordinateIndex)
+        nexttile(subplotNumber);
+        hold on
+        plot(trackedDataTime*100, trackedVelocities(:, coordinateIndex), lineWidth=2)
+        for j = 1 : numel(modelDataFiles)
+            plot(modeledVelocitiesTime{j}*100, modeledVelocities{j}(:, i), ...
+                lineWidth=2)
+        end
+        hold off
+        titleString = [sprintf("%s", strrep(coordinateLabels(coordinateIndex), "_", " "))];
+        for j = 1 : numel(modelDataFiles)
+            rmse = rms(resampledExperimentalVelocities{j}(:, i) - ...
+                modeledVelocities{j}(:, i));
+            titleString(j+1) = sprintf("RMSE %d: %.4f", j, rmse);
+        end
+        title(titleString)
+        xlim("tight")
+        maxData = [];
+        minData = [];
+        for j = 1 : numel(modelDataFiles)
+            maxData(j) = max(modeledVelocities{j}(:, i), [], "all");
+            minData(j) = min(modeledVelocities{j}(:, i), [], "all");
+        end
+        maxData(j+1) = max(trackedVelocities(:, i), [], "all");
+        minData(j+1) = min(trackedVelocities(:, i), [], "all");
+        yLimitUpper = max(maxData);
+        yLimitLower = min(minData);
+        minimum = 3;
+        if yLimitUpper - yLimitLower < minimum
+            ylim([(yLimitUpper+yLimitLower)/2-minimum, (yLimitUpper+yLimitLower)/2+minimum])
+        end
+        subplotNumber = subplotNumber + 1;
+    end
+end
+
+end
