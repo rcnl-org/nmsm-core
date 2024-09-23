e525ca4d6feacda26b56e06718d5a27fcc2bb9fe
RobSalati <142843461+RobSalati@users.noreply.github.com>
2024-09-23 12:13:54 -0500

    Plot initial guess controls as well for VO plots.

--- src/TreatmentOptimization/Analysis/plotTreatmentOptimizationResultsFromSettingsFile.m
+++ src/TreatmentOptimization/Analysis/plotTreatmentOptimizationResultsFromSettingsFile.m
@@ -81,7 +81,7 @@ if exist(fullfile(resultsDirectory, "GRFData"), 'dir')
 end
 
 if isTorque
-    if strcmp(toolName, "DesignOptimization")
+    if strcmp(toolName, "DesignOptimization") | strcmp(toolName, "VerificationOptimization")
         plotTreatmentOptimizationControls( ...
             [fullfile(initialGuessDirectory, strcat(trialPrefix, "_torqueControls.sto")), ...
             fullfile(resultsDirectory, strcat(trialPrefix, "_torqueControls.sto"))])
@@ -91,10 +91,11 @@ if isTorque
     end
 end
 if isSynergy
-    if strcmp(toolName, "DesignOptimization")
+    if strcmp(toolName, "DesignOptimization") | strcmp(toolName, "VerificationOptimization")
         plotTreatmentOptimizationControls( ...
             [fullfile(initialGuessDirectory, strcat(trialPrefix, "_synergyCommands.sto")), ...
             fullfile(resultsDirectory, strcat(trialPrefix, "_synergyCommands.sto"))])
+
     else
         plotTreatmentOptimizationControls( ...
             fullfile(resultsDirectory, strcat(trialPrefix, "_synergyCommands.sto")))
@@ -119,68 +120,3 @@ else
     isTorque = false;
 end
 end
-
-% function plotTreatmentOptimizationResultsFromSettingsFile(settingsFileName)
-% settingsTree = xml2struct(settingsFileName);
-% toolName = findToolName(settingsTree);
-% resultsDirectory = getTextFromField(getFieldByName(settingsTree, ...
-%     'results_directory'));
-% trackedQuantitiesDirectory = getTextFromField(getFieldByName(settingsTree, ...
-%     'tracked_quantities_directory'));
-% initialGuessDirectory = getTextFromField(getFieldByName(settingsTree, ...
-%     'initial_guess_directory'));
-% [isTorque, isSynergy] = parseControllers(settingsTree);
-% modelFileName = parseElementTextByName(settingsTree, 'input_model_file');
-% trialPrefix = getTextFromField(getFieldByName(settingsTree, ...
-%     'trial_name'));
-% 
-% 
-% plotTreatmentOptimizationJointAngles( ...
-%     modelFileName, ...
-%     fullfile(trackedQuantitiesDirectory, "IKData", strcat(trialPrefix, ".sto")), ...
-%     fullfile(resultsDirectory, "IKData", strcat(trialPrefix, ".sto")));
-% 
-% plotTreatmentOptimizationJointLoads( ...
-%     fullfile(trackedQuantitiesDirectory, "IDData", strcat(trialPrefix, ".sto")), ...
-%     fullfile(resultsDirectory, "IDData", strcat(trialPrefix, ".sto")));
-% 
-% plotTreatmentOptimizationGroundReactions( ...
-%     fullfile(trackedQuantitiesDirectory, "GRFData", strcat(trialPrefix, ".sto")), ...
-%     fullfile(resultsDirectory, "GRFData", strcat(trialPrefix, ".sto")));
-% 
-% if isTorque
-%     if strcmp(toolName, "DesignOptimization")
-%         plotTreatmentOptimizationControls( ...
-%             [fullfile(initialGuessDirectory, strcat(trialPrefix, "_torqueControls.sto")), ...
-%             fullfile(resultsDirectory, strcat(trialPrefix, "_torqueControls.sto"))])
-%     else
-%         plotTreatmentOptimizationControls( ...
-%             fullfile(resultsDirectory, strcat(trialPrefix, "_torqueControls.sto")))
-%     end
-% end
-% if isSynergy
-%     if strcmp(toolName, "DesignOptimization")
-%         plotTreatmentOptimizationControls( ...
-%             [fullfile(initialGuessDirectory, strcat(trialPrefix, "_synergyCommands.sto")), ...
-%             fullfile(resultsDirectory, strcat(trialPrefix, "_synergyCommands.sto"))])
-%     else
-%         plotTreatmentOptimizationControls( ...
-%             fullfile(resultsDirectory, strcat(trialPrefix, "_synergyCommands.sto")))
-%     end
-% end
-% end
-% 
-% function [isTorque, isSynergy] = parseControllers(settingsTree)
-% synergy = getFieldByName(settingsTree, 'RCNLSynergyController');
-% if isstruct(synergy)
-%     isSynergy = true;
-% else
-%     isSynergy = false;
-% end
-% torque = getFieldByName(settingsTree, 'RCNLTorqueController');
-% if isstruct(torque)
-%     isTorque = true;
-% else
-%     isTorque = false;
-% end
-% end
