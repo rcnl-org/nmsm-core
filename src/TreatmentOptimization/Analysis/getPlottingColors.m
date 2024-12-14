92e10599513d48bc95641d0b0bbbfc916397136e
RobSalati <142843461+RobSalati@users.noreply.github.com>
2024-12-13 19:23:14 -0500

    move params function to src\core

--- src/TreatmentOptimization/Analysis/getPlottingColors.m
+++
@@ -1,5 +0,0 @@
-function colors = getPlottingColors()
-% colors = ["#004488", "#DDAA33", "#BB5566"];
-colors = ["#4477AA", "#EE6677", "#228833", "#CCBB44", "#66CCEE"];
-end
-
---
+++ src/core/plot/getPlottingParams.m
@@ -0,0 +1,8 @@
+function params = getPlottingParams()
+params.colors = ["#4477AA", "#EE6677", "#228833", "#CCBB44", "#66CCEE"];
+params.units = 'normalized'; % pixels, normalized, inches, centimeters, points
+params.figureSize = [0.05 0.05 0.9 0.85];
+params.axisLabelFontSize = 20;
+params.subplotTitleFontSize = 15;
+params.legendFontSize = 15;
+end
