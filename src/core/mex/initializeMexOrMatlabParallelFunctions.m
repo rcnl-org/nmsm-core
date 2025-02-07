9759412cdcc004a006352ccade0ca268a8a10b1d
Spencer Williams <54556034+SpencerTWilliams@users.noreply.github.com>
2025-02-07 13:37:51 -0600

    Use single thread point kinematics

--- src/core/mex/initializeMexOrMatlabParallelFunctions.m
+++ src/core/mex/initializeMexOrMatlabParallelFunctions.m
@@ -36,7 +36,7 @@ if isequal(mexext, 'mexw64')
         pointKinematicsMexWindows40501(modelFile);
         inverseDynamicsWithExtraCalcsMexWindows40501(modelFile);
     else
-        pointKinematicsMexWindows40400(modelFile);
+        pointKinematics1(modelFile);
         inverseDynamicsWithExtraCalcsMexWindows40400(modelFile);
     end
 end
