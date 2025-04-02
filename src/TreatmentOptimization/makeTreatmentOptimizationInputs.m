80faf8c56c7779b41dd937fa427703a5f6086858
stwilliams333 <stwilliamsmail@gmail.com>
2025-04-01 22:52:37 -0500

    center of pressure terms

--- src/TreatmentOptimization/makeTreatmentOptimizationInputs.m
+++ src/TreatmentOptimization/makeTreatmentOptimizationInputs.m
@@ -45,6 +45,7 @@ inputs = makeSurrogateModel(inputs);
     = makeMaxAllowableError(inputs.toolName, inputs.costTerms);
 inputs = makeMarkerTracking(inputs);
 inputs = makeOrientationTracking(inputs);
+inputs = makeCenterOfPressureTracking(inputs);
 inputs = makePathConstraintBounds(inputs);
 inputs = makeTerminalConstraintBounds(inputs);
 inputs = makeOptimalControlBounds(inputs);
