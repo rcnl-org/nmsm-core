019a9a7954a74392bcc2538d425e73c2f64123cc
stwilliams333 <stwilliamsmail@gmail.com>
2025-04-16 22:08:07 -0500

    smoothness path constraints

---
+++ src/TreatmentOptimization/PathTerms/calcInverseDynamicsLoadSmoothnessDeviationPathConstraint.m
@@ -0,0 +1,48 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function calculates the difference between the experimental and
+% predicted joint angles for the specified coordinate.
+%
+% (struct, Array of number, 2D matrix, Array of string) -> (Array of number)
+%
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Spencer Williams                                             %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function [pathConstraint, constraintTerm] = ...
+    calcInverseDynamicsLoadSmoothnessDeviationPathConstraint( ...
+    constraintTerm, inputs, time, inverseDynamicsMoments, loadName)
+[idMoment, constraintTerm] = findDataByLabels(constraintTerm, ...
+    inverseDynamicsMoments, inputs.inverseDynamicsMomentLabels, ...
+    loadName);
+
+polynomialDegree = valueOrAlternate(constraintTerm, ...
+    "polynomial_degree", 1);
+harmonics = valueOrAlternate(constraintTerm, "harmonics", 7);
+fit = fitSmoothingTermCurve(inputs, time, idMoment, ...
+    polynomialDegree, harmonics);
+
+pathConstraint = idMoment - fit;
+
+[pathConstraint, constraintTerm] = applyTermTimeRanges(pathConstraint, ...
+    constraintTerm, time);
+end
