867420a2e42562cde933f805ad5a418d8e4f9601
Spencer Williams <54556034+SpencerTWilliams@users.noreply.github.com>
2025-06-16 14:28:00 +0200

    control and muscle activation deviation and value

---
+++ src/TreatmentOptimization/PathTerms/calcControllerDeviation.m
@@ -0,0 +1,39 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function calculates deviations in controls from reference values.
+%
+% (struct, struct, Array of number, Array of string) -> (Array of number)
+%
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Spencer Williams                                             %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function [pathTerm, constraintTerm] = calcControllerDeviation( ...
+    constraintTerm, inputs, values, time, controllerName)
+[controls, constraintTerm] = findControlsByLabels(constraintTerm, ...
+    inputs, values, time, controllerName);
+experimentalControls = findSplinedControlsByLabels(constraintTerm, ...
+    inputs, time);
+
+scaleFactor = valueOrAlternate(constraintTerm, "scale_factor", 1);
+pathTerm = controls - (experimentalControls * scaleFactor);
+end
