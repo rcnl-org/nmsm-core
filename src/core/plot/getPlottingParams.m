7bf1875554baa99e3a95e575e1318e38599fcfe1
RobSalati <rs200@rice.edu>
2025-08-20 17:43:49 -0500

    revert plotting params to default

--- src/core/plot/getPlottingParams.m
+++ src/core/plot/getPlottingParams.m
@@ -2,17 +2,16 @@ function params = getPlottingParams()
 % Figure size params
 params.units = 'normalized'; % pixels, normalized, inches, centimeters, points
 params.figureSize = [0.05 0.05 0.9 0.85];
-params.axisLabelFontSize = 25;
-params.subplotTitleFontSize = 20;
-params.legendFontSize = 25;
-params.tickLabelFontSize = 25;
+params.axisLabelFontSize = 15;
+params.subplotTitleFontSize = 12;
+params.legendFontSize = 8;
+params.tickLabelFontSize = 10;
 
 % Color params
-params.plotBackgroundColor = "#D8D8D8";
-params.subplotBackgroundColor = "#D8D8D8";
+params.plotBackgroundColor = "#F0F0F0";
+params.subplotBackgroundColor = "#FFFFFF";
 
 % Line params
 params.lineColors = ["#4477AA", "#EE6677", "#228833", "#CCBB44", "#66CCEE", "AA3377", "BBBBBB"];
-% params.lineColors = ["#4477AA", "#66CCEE", "#CCBB44", "#228833", "#EE6677", "AA3377", "BBBBBB"];
-params.linewidth = 5;
+params.linewidth = 2;
 end
