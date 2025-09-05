08b87c77ae492485e954817f2b00bf467da02246
stwilliams333 <stwilliamsmail@gmail.com>
2025-09-04 21:00:57 -0500

    acceleration terminal constraints

---
+++ src/TreatmentOptimization/TerminalTerms/calcFinalGeneralizedAccelerationValue.m
@@ -0,0 +1,40 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function calculates the value of modeled coordinate positions. 
+%
+% (2D matrix, Cell, Array of string) -> (Number)
+% 
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Spencer Williams                                             %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function [pathTerm, constraintTerm] = ...
+    calcFinalGeneralizedAccelerationValue( ...
+    constraintTerm, inputs, time, accelerations)
+assert(inputs.useJerk, "The final_generalized_acceleration_value " + ...
+    "constraint requires enabling joint jerk controls with " + ...
+    "<use_jerk>true</use_jerk>.")
+[acceleration, constraintTerm] = findDataByLabels(constraintTerm, ...
+    accelerations, inputs.coordinateNames, constraintTerm.coordinate);
+
+pathTerm = acceleration(end);
+end
---
+++ src/TreatmentOptimization/TerminalTerms/calcInitialGeneralizedAccelerationDeviation.m
@@ -0,0 +1,43 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function calculates the difference between experimental and
+% modeled coordinate accelerations. 
+%
+% (2D matrix, Cell, Array of string) -> (Number)
+% 
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Spencer Williams                                             %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function [pathTerm, constraintTerm] = ...
+    calcInitialGeneralizedAccelerationDeviation( ...
+    constraintTerm, inputs, time, accelerations)
+assert(inputs.useJerk, "The initial_generalized_acceleration_deviation " + ...
+    "constraint requires enabling joint jerk controls with " + ...
+    "<use_jerk>true</use_jerk>.")
+[acceleration, constraintTerm] = findDataByLabels(constraintTerm, ...
+    accelerations, inputs.coordinateNames, constraintTerm.coordinate);
+experimentalAcceleration = findSplinedJointAccelerationsByLabels( ...
+    constraintTerm, inputs, time);
+
+pathTerm = acceleration(1) - experimentalAcceleration(1);
+end
