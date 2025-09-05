d7556580425d7a5e26d75d5073d862cdfb097cec
stwilliams333 <stwilliamsmail@gmail.com>
2025-09-04 22:00:42 -0500

    controller terminal constraints

---
+++ src/TreatmentOptimization/TerminalTerms/calcInitialControllerDeviation.m
@@ -0,0 +1,42 @@
+% This function is part of the NMSM Pipeline, see file for full license.
+%
+% This function calculates the difference between experimental and
+% modeled controls. 
+%
+% (2D matrix, Cell, Array of string) -> (Number)
+% 
+
+% ----------------------------------------------------------------------- %
+% The NMSM Pipeline is a toolkit for model personalization and treatment  %
+% optimization of neuromusculoskeletal models through OpenSim. See        %
+% nmsm.rice.edu and the NOTICE file for more information. The             %
+% NMSM Pipeline is developed at Rice University and supported by the US   %
+% National Institutes of Health (R01 EB030520).                           %
+%                                                                         %
+% Copyright (c) 2021 Rice University and the Authors                      %
+% Author(s): Spencer Williams                                             %
+%                                                                         %
+% Licensed under the Apache License, Version 2.0 (the "License");         %
+% you may not use this file except in compliance with the License.        %
+% You may obtain a copy of the License at                                 %
+% http://www.apache.org/licenses/LICENSE-2.0.                             %
+%                                                                         %
+% Unless required by applicable law or agreed to in writing, software     %
+% distributed under the License is distributed on an "AS IS" BASIS,       %
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or         %
+% implied. See the License for the specific language governing            %
+% permissions and limitations under the License.                          %
+% ----------------------------------------------------------------------- %
+
+function [pathTerm, constraintTerm] = ...
+    calcInitialControllerDeviation( ...
+    constraintTerm, inputs, time, values)
+assert(inputs.useControlDerivatives, "The initial_controller_deviation " + ...
+    "constraint requires enabling control derivatives with " + ...
+    "<use_control_derivatives>true</use_control_derivatives>.")
+[control, constraintTerm] = findControlsByLabels(constraintTerm, inputs, values, ...
+    time, constraintTerm.controller);
+experimentalControl = findSplinedControlsByLabels(constraintTerm, inputs, time);
+
+pathTerm = control(1) - experimentalControl(1);
+end
