2fd6721aec94befc4a9c270c18a1d26b6bd02ac7
stwilliams333 <stwilliamsmail@gmail.com>
2025-10-06 19:47:41 -0500

    set up spherical contact

--- src/GroundContactPersonalization/Optimizations/ModelCalculation/calcModeledVerticalGroundReactionForce.m
+++ src/GroundContactPersonalization/Optimizations/ModelCalculation/calcModeledVerticalGroundReactionForce.m
@@ -53,6 +53,7 @@ for i=1:length(springConstants)
     ymax = 1e-2;
     Kval = springConstants(i);
     height = height - springRestingLength;
+    height = sign(height) .* abs(height) .^ 1.5;
     % When the height variable reaches a critical value of 0.70947586,
     % the calculated vertical ground reaction force goes to fininity. Thus,
     % it is essential to keep the height variable below this value.
