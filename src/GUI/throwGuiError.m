2ba11fc14b2dbb474c01db0097e7b234c19f1216
RobSalati <rs200@rice.edu>
2026-03-08 22:45:09 -0500

    JMP redundant joints and body messages

--- src/GUI/throwGuiError.m
+++ src/GUI/throwGuiError.m
@@ -1,5 +1,9 @@
 function throwGuiError(errorMessage, fieldObject, iconObject)
+    if ~isempty(fieldObject)
         fieldObject.BackgroundColor = [1.00,0.67,0.67];
+    end
+    if ~isempty(iconObject)
         iconObject.Visible = 'on';
         iconObject.Tooltip = errorMessage;
+    end
 end
