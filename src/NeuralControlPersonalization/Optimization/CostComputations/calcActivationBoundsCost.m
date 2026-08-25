44a1d25533fc7b80862d06d5872a259643e9def5
Xuanning Liu <xuanning.lucy.liu@gmail.com>
2026-08-25 16:31:40 -0500

    bug fix

--- src/NeuralControlPersonalization/Optimization/CostComputations/calcActivationBoundsCost.m
+++ src/NeuralControlPersonalization/Optimization/CostComputations/calcActivationBoundsCost.m
@@ -1,11 +1,12 @@
 function rawCost = calcActivationBoundsCost(activations, costTerm)
-% Smooth penalty for activations outside [0, 1]
+% Smooth penalty for activations below 0
 % Only needed when allow_negative_synergy_vector_weights is true
 % casADi safe
 sharpness = valueOrAlternate(costTerm, 'sharpness', 76);
 lowerViolation = ((-activations) + sqrt(activations.^2 + ...
     (1 / sharpness)^2)) / 2 - 1 / (2 * sharpness);
-upperViolation = ((activations - 1) + sqrt((activations - 1).^2 + ...
-    (1 / sharpness)^2)) / 2 - 1 / (2 * sharpness);
-rawCost = [lowerViolation(:); upperViolation(:)];
+% upperViolation = ((activations - 1) + sqrt((activations - 1).^2 + ...
+%     (1 / sharpness)^2)) / 2 - 1 / (2 * sharpness);
+% rawCost = [lowerViolation(:); upperViolation(:)];
+rawCost = lowerViolation(:);
 end
